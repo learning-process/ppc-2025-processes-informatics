cf70778c9c988d90407c0586c3d67a98ec56ab96
GssvvAl <gssvval@yandex.ru>
2025-10-22 10:39:59 +0000

    [refactor]

--- tasks/guseva_a_matrix_sums/tests/performance/main.cpp
+++ tasks/guseva_a_matrix_sums/tests/performance/main.cpp
@@ -1,40 +1,40 @@
-#include <gtest/gtest.h>
+// #include <gtest/gtest.h>
 
-#include "example_processes/common/include/common.hpp"
-#include "example_processes/mpi/include/ops_mpi.hpp"
-#include "example_processes/seq/include/ops_seq.hpp"
-#include "util/include/perf_test_util.hpp"
+// #include "example_processes/common/include/common.hpp"
+// #include "example_processes/mpi/include/ops_mpi.hpp"
+// #include "example_processes/seq/include/ops_seq.hpp"
+// #include "util/include/perf_test_util.hpp"
 
-namespace nesterov_a_test_task_processes {
+// namespace nesterov_a_test_task_processes {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
-  InType input_data_{};
+// class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+//   const int kCount_ = 100;
+//   InType input_data_{};
 
-  void SetUp() override {
-    input_data_ = kCount_;
-  }
+//   void SetUp() override {
+//     input_data_ = kCount_;
+//   }
 
-  bool CheckTestOutputData(OutType &output_data) const final {
-    return input_data_ == output_data;
-  }
+//   bool CheckTestOutputData(OutType &output_data) const final {
+//     return input_data_ == output_data;
+//   }
 
-  [[nodiscard]] InType GetTestInputData() const final {
-    return input_data_;
-  }
-};
+//   [[nodiscard]] InType GetTestInputData() const final {
+//     return input_data_;
+//   }
+// };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
-  ExecuteTest(GetParam());
-}
+// TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+//   ExecuteTest(GetParam());
+// }
 
-const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, NesterovATestTaskMPI, NesterovATestTaskSEQ>(PPC_SETTINGS_example_processes);
+// const auto kAllPerfTasks =
+//     ppc::util::MakeAllPerfTasks<InType, NesterovATestTaskMPI, NesterovATestTaskSEQ>(PPC_SETTINGS_example_processes);
 
-const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+// const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+// const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+// INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
 
-}  // namespace nesterov_a_test_task_processes
+// }  // namespace nesterov_a_test_task_processes
