58089241b1b532b2558f35896a282cc75caa0256
GssvvAl <gssvval@yandex.ru>
2025-10-22 12:05:14 +0000

    [alter] Alter algo for AppleClang not supporting C++20 features

--- tasks/guseva_a_matrix_sums/seq/src/ops_seq.cpp
+++ tasks/guseva_a_matrix_sums/seq/src/ops_seq.cpp
@@ -16,17 +16,26 @@ bool GusevaAMatrixSumsSEQ::ValidationImpl() {
 }
 
 bool GusevaAMatrixSumsSEQ::PreProcessingImpl() {
-  GetOutput().resize(std::get<1>(GetInput()));
+  GetOutput().resize(std::get<1>(GetInput()), 0.0);
   return true;
 }
 
 bool GusevaAMatrixSumsSEQ::RunImpl() {
   auto &matrix = std::get<2>(GetInput());
   auto &columns = std::get<1>(GetInput());
+#if defined(__APPLE__) && defined(__clang__)
+  auto &rows = std::get<0>(GetInput()) std::vector<double> expected(columns, 0);
+  for (uint32_t i = 0; i < rows; i++) {
+    for (uint32_t j = 0; j < columns; j++) {
+      GetOutput()[j] += matrix[(i * columns) + j];
+    }
+  }
+#else
   for (const auto &[column, x] : GetOutput() | std::views::enumerate) {
     const auto &arr = matrix | std::ranges::views::drop(column) | std::views::stride(columns);
     x = std::reduce(arr.begin(), arr.end(), 0.0);
   }
+#endif
   return true;
 }
 
