e91bb9486d41f779ac19449a740a5e8d09797263
fartekool <levonychev2006@gmail.com>
2025-10-24 01:36:46 +0300

    Update class name in perf tests

--- tasks/levonychev_i_min_val_rows_matrix/tests/performance/main.cpp
+++ tasks/levonychev_i_min_val_rows_matrix/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace levonychev_i_min_val_rows_matrix {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class LevonychevIMinValRowsMatrixPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(LevonychevIMinValRowsMatrixPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = LevonychevIMinValRowsMatrixPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, LevonychevIMinValRowsMatrixPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace levonychev_i_min_val_rows_matrix
