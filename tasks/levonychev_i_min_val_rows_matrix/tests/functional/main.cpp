57051d05432054d2237ba6139b7763a87f7ab16e
fartekool <levonychev2006@gmail.com>
2025-10-24 01:21:40 +0300

    Rename in functional/main.cpp

--- tasks/levonychev_i_min_val_rows_matrix/tests/functional/main.cpp
+++ tasks/levonychev_i_min_val_rows_matrix/tests/functional/main.cpp
@@ -20,7 +20,7 @@
 
 namespace levonychev_i_min_val_rows_matrix {
 
-class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+class LevonychevIMinValRowsMatrixFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
     return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
@@ -64,7 +64,7 @@ class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType
 
 namespace {
 
-TEST_P(NesterovARunFuncTestsProcesses, MatmulFromPic) {
+TEST_P(LevonychevIMinValRowsMatrixFuncTests, MatmulFromPic) {
   ExecuteTest(GetParam());
 }
 
@@ -76,9 +76,9 @@ const auto kTestTasksList =
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
-const auto kPerfTestName = NesterovARunFuncTestsProcesses::PrintFuncTestName<NesterovARunFuncTestsProcesses>;
+const auto kPerfTestName = LevonychevIMinValRowsMatrixFuncTests::PrintFuncTestName<LevonychevIMinValRowsMatrixFuncTests>;
 
-INSTANTIATE_TEST_SUITE_P(PicMatrixTests, NesterovARunFuncTestsProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(PicMatrixTests, LevonychevIMinValRowsMatrixFuncTests, kGtestValues, kPerfTestName);
 
 }  // namespace
 
