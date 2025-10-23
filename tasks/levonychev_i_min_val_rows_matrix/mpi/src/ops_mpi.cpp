2a057ccb11b3a97f13bf126df44136fbf32dc7eb
fartekool <levonychev2006@gmail.com>
2025-10-24 01:07:21 +0300

    First commit. Created dir for first task

---
+++ tasks/levonychev_i_min_val_rows_matrix/mpi/src/ops_mpi.cpp
@@ -0,0 +1,72 @@
+#include "levonychev_i_min_val_rows_matrix/mpi/include/ops_mpi.hpp"
+
+#include <mpi.h>
+
+#include <numeric>
+#include <vector>
+
+#include "levonychev_i_min_val_rows_matrix/common/include/common.hpp"
+#include "util/include/util.hpp"
+
+namespace levonychev_i_min_val_rows_matrix {
+
+LevonychevIMinValRowsMatrixMPI::LevonychevIMinValRowsMatrixMPI(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = 0;
+}
+
+bool LevonychevIMinValRowsMatrixMPI::ValidationImpl() {
+  return (GetInput() > 0) && (GetOutput() == 0);
+}
+
+bool LevonychevIMinValRowsMatrixMPI::PreProcessingImpl() {
+  GetOutput() = 2 * GetInput();
+  return GetOutput() > 0;
+}
+
+bool LevonychevIMinValRowsMatrixMPI::RunImpl() {
+  auto input = GetInput();
+  if (input == 0) {
+    return false;
+  }
+
+  for (InType i = 0; i < GetInput(); i++) {
+    for (InType j = 0; j < GetInput(); j++) {
+      for (InType k = 0; k < GetInput(); k++) {
+        std::vector<InType> tmp(i + j + k, 1);
+        GetOutput() += std::accumulate(tmp.begin(), tmp.end(), 0);
+        GetOutput() -= i + j + k;
+      }
+    }
+  }
+
+  const int num_threads = ppc::util::GetNumThreads();
+  GetOutput() *= num_threads;
+
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if (rank == 0) {
+    GetOutput() /= num_threads;
+  } else {
+    int counter = 0;
+    for (int i = 0; i < num_threads; i++) {
+      counter++;
+    }
+
+    if (counter != 0) {
+      GetOutput() /= counter;
+    }
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  return GetOutput() > 0;
+}
+
+bool LevonychevIMinValRowsMatrixMPI::PostProcessingImpl() {
+  GetOutput() -= GetInput();
+  return GetOutput() > 0;
+}
+
+}  // namespace levonychev_i_min_val_rows_matrix
---
+++ tasks/levonychev_i_min_val_rows_matrix/seq/src/ops_seq.cpp
@@ -0,0 +1,60 @@
+#include "levonychev_i_min_val_rows_matrix/seq/include/ops_seq.hpp"
+
+#include <numeric>
+#include <vector>
+
+#include "levonychev_i_min_val_rows_matrix/common/include/common.hpp"
+#include "util/include/util.hpp"
+
+namespace levonychev_i_min_val_rows_matrix {
+
+LevonychevIMinValRowsMatrixSEQ::LevonychevIMinValRowsMatrixSEQ(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = 0;
+}
+
+bool LevonychevIMinValRowsMatrixSEQ::ValidationImpl() {
+  return (GetInput() > 0) && (GetOutput() == 0);
+}
+
+bool LevonychevIMinValRowsMatrixSEQ::PreProcessingImpl() {
+  GetOutput() = 2 * GetInput();
+  return GetOutput() > 0;
+}
+
+bool LevonychevIMinValRowsMatrixSEQ::RunImpl() {
+  if (GetInput() == 0) {
+    return false;
+  }
+
+  for (InType i = 0; i < GetInput(); i++) {
+    for (InType j = 0; j < GetInput(); j++) {
+      for (InType k = 0; k < GetInput(); k++) {
+        std::vector<InType> tmp(i + j + k, 1);
+        GetOutput() += std::accumulate(tmp.begin(), tmp.end(), 0);
+        GetOutput() -= i + j + k;
+      }
+    }
+  }
+
+  const int num_threads = ppc::util::GetNumThreads();
+  GetOutput() *= num_threads;
+
+  int counter = 0;
+  for (int i = 0; i < num_threads; i++) {
+    counter++;
+  }
+
+  if (counter != 0) {
+    GetOutput() /= counter;
+  }
+  return GetOutput() > 0;
+}
+
+bool LevonychevIMinValRowsMatrixSEQ::PostProcessingImpl() {
+  GetOutput() -= GetInput();
+  return GetOutput() > 0;
+}
+
+}  // namespace levonychev_i_min_val_rows_matrix
