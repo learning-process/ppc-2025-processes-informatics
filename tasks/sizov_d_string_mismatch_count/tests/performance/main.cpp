2a37efb9bf8e9b1e8144fdf10f09f249679113de
SizovDmitry <184sizov9v@mail.ru>
2025-10-24 20:42:33 +0000

    Fix formatting issues [ci skip]

--- tasks/sizov_d_string_mismatch_count/tests/performance/main.cpp
+++ tasks/sizov_d_string_mismatch_count/tests/performance/main.cpp
@@ -11,8 +11,7 @@
 
 namespace sizov_d_string_mismatch_count {
 
-class SizovDRunPerfTestsStringMismatchCount
-    : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class SizovDRunPerfTestsStringMismatchCount : public ppc::util::BaseRunPerfTests<InType, OutType> {
   void SetUp() override {
     std::string a(1'000'000, 'a');
     std::string b = a;
@@ -25,31 +24,30 @@ class SizovDRunPerfTestsStringMismatchCount
     input_data_ = std::make_tuple(std::move(a), std::move(b));
   }
 
-  InType GetTestInputData() final { return input_data_; }
+  InType GetTestInputData() final {
+    return input_data_;
+  }
 
   bool CheckTestOutputData(OutType &output_data) final {
     return output_data == expected_result_;
   }
 
-private:
+ private:
   InType input_data_;
   OutType expected_result_ = 0;
 };
 
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, SizovDStringMismatchCountMPI,
-                                SizovDStringMismatchCountSEQ>(
+    ppc::util::MakeAllPerfTasks<InType, SizovDStringMismatchCountMPI, SizovDStringMismatchCountSEQ>(
         PPC_SETTINGS_sizov_d_string_mismatch_count);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
-const auto kPerfTestName =
-    SizovDRunPerfTestsStringMismatchCount::CustomPerfTestName;
+const auto kPerfTestName = SizovDRunPerfTestsStringMismatchCount::CustomPerfTestName;
 
 TEST_P(SizovDRunPerfTestsStringMismatchCount, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
-INSTANTIATE_TEST_SUITE_P(RunPerf, SizovDRunPerfTestsStringMismatchCount,
-                         kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunPerf, SizovDRunPerfTestsStringMismatchCount, kGtestValues, kPerfTestName);
 
-} // namespace sizov_d_string_mismatch_count
+}  // namespace sizov_d_string_mismatch_count
