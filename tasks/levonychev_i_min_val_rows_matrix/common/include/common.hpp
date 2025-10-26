50b0ce658987414840434ab1c843066743f49bf7
fartekool <levonychev2006@gmail.com>
2025-10-26 20:46:23 +0300

    Changed InType and OutType in common.hpp

--- tasks/levonychev_i_min_val_rows_matrix/common/include/common.hpp
+++ tasks/levonychev_i_min_val_rows_matrix/common/include/common.hpp
@@ -2,13 +2,13 @@
 
 #include <string>
 #include <tuple>
-
+#include <vector>
 #include "task/include/task.hpp"
 
 namespace levonychev_i_min_val_rows_matrix {
 
-using InType = int;
-using OutType = int;
+using InType = std::vector<std::vector<double>>;
+using OutType = std::vector<double>;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
