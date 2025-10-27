54c57c3235a985b2021424c2574f94e2ed16aad3
Offsidetax <dspichek@bk.ru>
2025-10-27 22:22:51 +0300

    New mpi logic

--- tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
+++ tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
@@ -27,19 +27,33 @@ bool SpichekDDotProductOfVectorsMPI::PreProcessingImpl() {
 }
 
 bool SpichekDDotProductOfVectorsMPI::RunImpl() {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
   const auto &[vector1, vector2] = GetInput();
 
   if (vector1.size() != vector2.size() || vector1.empty()) {
     return false;
   }
 
-  // Просто вычисляем скалярное произведение
-  int dot_product = 0;
-  for (size_t i = 0; i < vector1.size(); ++i) {
-    dot_product += vector1[i] * vector2[i];
+  size_t n = vector1.size();
+
+  // Каждый процесс вычисляет СВОЮ часть
+  size_t chunk_size = n / size;
+  size_t start = rank * chunk_size;
+  size_t end = (rank == size - 1) ? n : start + chunk_size;
+
+  int local_dot_product = 0;
+  for (size_t i = start; i < end; ++i) {
+    local_dot_product += vector1[i] * vector2[i];
   }
 
-  GetOutput() = dot_product;
+  // Аккуратно собираем результаты
+  int global_dot_product = 0;
+  MPI_Allreduce(&local_dot_product, &global_dot_product, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+  GetOutput() = global_dot_product;
   return true;
 }
 
