79f5e7870ffad6ee60a5ceff9d8a1a32f612ebb1
Offsidetax <dspichek@bk.ru>
2025-10-27 20:18:16 +0300

    Broadcast vectors data to all MPI processes

--- tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
+++ tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
@@ -39,19 +39,45 @@ bool SpichekDDotProductOfVectorsMPI::RunImpl() {
 
   size_t n = vector1.size();
 
+  // Распространяем размер векторов на все процессы
+  size_t vector_size = n;
+  MPI_Bcast(&vector_size, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
+
+  // Создаем локальные копии векторов
+  std::vector<int> local_vec1, local_vec2;
+
+  if (rank == 0) {
+    // Root процесс уже имеет данные
+    local_vec1 = vector1;
+    local_vec2 = vector2;
+  } else {
+    // Другие процессы выделяют память
+    local_vec1.resize(vector_size);
+    local_vec2.resize(vector_size);
+  }
+
+  // Распространяем данные векторов на все процессы
+  MPI_Bcast(local_vec1.data(), vector_size, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(local_vec2.data(), vector_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+  // Теперь все процессы имеют полные копии векторов
+  // Вычисляем свою часть скалярного произведения
   size_t chunk_size = n / size;
   size_t start = rank * chunk_size;
   size_t end = (rank == size - 1) ? n : start + chunk_size;
 
   int local_dot_product = 0;
   for (size_t i = start; i < end; ++i) {
-    local_dot_product += vector1[i] * vector2[i];
+    local_dot_product += local_vec1[i] * local_vec2[i];
   }
 
   int global_dot_product = 0;
-  MPI_Allreduce(&local_dot_product, &global_dot_product, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Reduce(&local_dot_product, &global_dot_product, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-  GetOutput() = global_dot_product;
+  // Только root процесс устанавливает результат
+  if (rank == 0) {
+    GetOutput() = global_dot_product;
+  }
 
   return true;
 }
