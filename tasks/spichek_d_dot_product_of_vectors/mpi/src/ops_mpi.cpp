82e1d737d3752661f385d992439b67e5afbd3608
Offsidetax <dspichek@bk.ru>
2025-10-27 23:21:04 +0300

    Updated data distribution in mpi

--- tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
+++ tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
@@ -27,37 +27,44 @@ bool SpichekDDotProductOfVectorsMPI::PreProcessingImpl() {
 }
 
 bool SpichekDDotProductOfVectorsMPI::RunImpl() {
-  int rank;
+  int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   const auto &[vector1, vector2] = GetInput();
 
-  // Сначала проверяем валидность данных на всех процессах
-  int is_valid = 1;
-  if (rank == 0) {
-    if (vector1.size() != vector2.size() || vector1.empty()) {
-      is_valid = 0;
-    }
+  // Определяем, кто имеет данные
+  int data_size = 0;
+  if (!vector1.empty() && !vector2.empty() && vector1.size() == vector2.size()) {
+    data_size = static_cast<int>(vector1.size());
   }
-  MPI_Bcast(&is_valid, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  if (!is_valid) {
+  // Находим максимальный размер данных среди всех процессов
+  int max_data_size;
+  MPI_Allreduce(&data_size, &max_data_size, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
+  if (max_data_size == 0) {
     GetOutput() = 0;
     return false;
   }
 
-  // Вычисляем на root процессе
-  int dot_product = 0;
-  if (rank == 0) {
-    for (size_t i = 0; i < vector1.size(); ++i) {
-      dot_product += vector1[i] * vector2[i];
+  // Если у процесса нет данных, он вычисляет 0
+  int local_dot_product = 0;
+  if (data_size > 0) {
+    size_t n = vector1.size();
+    size_t chunk_size = n / size;
+    size_t start = rank * chunk_size;
+    size_t end = (rank == size - 1) ? n : start + chunk_size;
+
+    for (size_t i = start; i < end; ++i) {
+      local_dot_product += vector1[i] * vector2[i];
     }
   }
 
-  // Распространяем результат
-  MPI_Bcast(&dot_product, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  GetOutput() = dot_product;
+  int global_dot_product = 0;
+  MPI_Allreduce(&local_dot_product, &global_dot_product, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
+  GetOutput() = global_dot_product;
   return true;
 }
 
