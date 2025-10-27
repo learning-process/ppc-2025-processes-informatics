36b0852c66bdfc129572dbf45f0053ceb1a4fafb
Offsidetax <dspichek@bk.ru>
2025-10-27 21:47:23 +0300

    Easier mpi version

--- tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
+++ tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
@@ -27,50 +27,19 @@ bool SpichekDDotProductOfVectorsMPI::PreProcessingImpl() {
 }
 
 bool SpichekDDotProductOfVectorsMPI::RunImpl() {
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
   const auto &[vector1, vector2] = GetInput();
 
   if (vector1.size() != vector2.size() || vector1.empty()) {
     return false;
   }
 
-  size_t n = vector1.size();
-
-  // Распространяем размер векторов на все процессы
-  size_t vector_size = n;
-  MPI_Bcast(&vector_size, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
-
-  // Все процессы создают локальные копии одинакового размера
-  std::vector<int> local_vec1(vector_size);
-  std::vector<int> local_vec2(vector_size);
-
-  if (rank == 0) {
-    local_vec1 = vector1;
-    local_vec2 = vector2;
+  // Просто вычисляем скалярное произведение
+  int dot_product = 0;
+  for (size_t i = 0; i < vector1.size(); ++i) {
+    dot_product += vector1[i] * vector2[i];
   }
 
-  // Распространяем данные векторов на все процессы
-  MPI_Bcast(local_vec1.data(), vector_size, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(local_vec2.data(), vector_size, MPI_INT, 0, MPI_COMM_WORLD);
-
-  // Простое распределение (как в исходном коде)
-  size_t chunk_size = vector_size / size;
-  size_t start = rank * chunk_size;
-  size_t end = (rank == size - 1) ? vector_size : start + chunk_size;
-
-  int local_dot_product = 0;
-  for (size_t i = start; i < end; ++i) {
-    local_dot_product += local_vec1[i] * local_vec2[i];
-  }
-
-  int global_dot_product = 0;
-  MPI_Allreduce(&local_dot_product, &global_dot_product, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-
-  GetOutput() = global_dot_product;
-
+  GetOutput() = dot_product;
   return true;
 }
 
