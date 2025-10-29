4be148eb8d47f3e29d5c8c0872bce42cd6104910
Offsidetax <dspichek@bk.ru>
2025-10-29 17:43:13 +0300

    Fixed deadlock

--- tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
+++ tasks/spichek_d_dot_product_of_vectors/mpi/src/ops_mpi.cpp
@@ -33,38 +33,29 @@ bool SpichekDDotProductOfVectorsMPI::RunImpl() {
 
   const auto &[vector1, vector2] = GetInput();
 
-  // Определяем, кто имеет данные
-  int data_size = 0;
-  if (!vector1.empty() && !vector2.empty() && vector1.size() == vector2.size()) {
-    data_size = static_cast<int>(vector1.size());
-  }
+  int local_size = (!vector1.empty() && vector1.size() == vector2.size()) ? static_cast<int>(vector1.size()) : 0;
 
-  // Находим максимальный размер данных среди всех процессов
-  int max_data_size;
-  MPI_Allreduce(&data_size, &max_data_size, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+  int max_size = 0;
+  MPI_Allreduce(&local_size, &max_size, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
 
-  if (max_data_size == 0) {
-    GetOutput() = 0;
-    return false;
-  }
-
-  // Если у процесса нет данных, он вычисляет 0
-  int local_dot_product = 0;
-  if (data_size > 0) {
+  long long local_dot = 0;
+  if (local_size > 0) {
     size_t n = vector1.size();
-    size_t chunk_size = n / size;
-    size_t start = rank * chunk_size;
-    size_t end = (rank == size - 1) ? n : start + chunk_size;
+    size_t chunk = n / size;
+    size_t start = rank * chunk;
+    size_t end = (rank == size - 1) ? n : start + chunk;
 
     for (size_t i = start; i < end; ++i) {
-      local_dot_product += vector1[i] * vector2[i];
+      local_dot += static_cast<long long>(vector1[i]) * vector2[i];
     }
   }
 
-  int global_dot_product = 0;
-  MPI_Allreduce(&local_dot_product, &global_dot_product, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  long long global_dot = 0;
+  MPI_Allreduce(&local_dot, &global_dot, 1, MPI_LONG_LONG, MPI_SUM, MPI_COMM_WORLD);
+  GetOutput() = static_cast<int>(global_dot);
 
-  GetOutput() = global_dot_product;
+  // гарантируем, что все процессы закончат вместе
+  MPI_Barrier(MPI_COMM_WORLD);
   return true;
 }
 
