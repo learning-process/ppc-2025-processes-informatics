5970b450da05ea18d9a5485adb70010ff1a71e02
root <abaldin05@mail.ru>
2025-10-29 19:27:19 +0000

    Define types

--- tasks/baldin_a_word_count/common/include/common.hpp
+++ tasks/baldin_a_word_count/common/include/common.hpp
@@ -7,9 +7,9 @@
 
 namespace baldin_a_word_count {
 
-using InType = int;
+using InType = std::string;
 using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using TestType = std::tuple<std::string, int>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace baldin_a_word_count
