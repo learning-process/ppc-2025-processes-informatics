5fded4f166f278432ea4ac47e5bc256dce1cb936
root <abaldin05@mail.ru>
2025-10-29 19:32:59 +0000

    some func and perf tests

--- tasks/baldin_a_word_count/tests/functional/main.cpp
+++ tasks/baldin_a_word_count/tests/functional/main.cpp
@@ -20,55 +20,72 @@
 
 namespace baldin_a_word_count {
 
-class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+class BaldinAWordCountFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
-    return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
+    std::string text = std::get<0>(test_param);
+
+    for (char &c : text) {
+      if (!std::isalnum(static_cast<unsigned char>(c))) {
+        c = '_';
+      }
+    }
+    return text + "_" + std::to_string(std::get<1>(test_param));
   }
 
  protected:
   void SetUp() override {
-    int width = -1;
-    int height = -1;
-    int channels = -1;
-    std::vector<uint8_t> img;
-    // Read image
-    {
-      std::string abs_path = ppc::util::GetAbsoluteTaskPath(PPC_ID_baldin_a_word_count, "pic.jpg");
-      auto *data = stbi_load(abs_path.c_str(), &width, &height, &channels, 0);
-      if (data == nullptr) {
-        throw std::runtime_error("Failed to load image: " + std::string(stbi_failure_reason()));
-      }
-      img = std::vector<uint8_t>(data, data + (static_cast<ptrdiff_t>(width * height * channels)));
-      stbi_image_free(data);
-      if (std::cmp_not_equal(width, height)) {
-        throw std::runtime_error("width != height: ");
-      }
-    }
-
     TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    input_data_ = width - height + std::min(std::accumulate(img.begin(), img.end(), 0), channels);
+    input_data = std::get<0>(params);
+    expected_output = std::get<1>(params);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return (input_data_ == output_data);
+    return output_data == expected_output;
   }
 
   InType GetTestInputData() final {
-    return input_data_;
+    return input_data;
   }
 
  private:
-  InType input_data_ = 0;
+  InType input_data;
+  OutType expected_output;
 };
 
 namespace {
 
-TEST_P(NesterovARunFuncTestsProcesses, MatmulFromPic) {
+TEST_P(BaldinAWordCountFuncTests, MatmulFromPic) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 3> kTestParam = {std::make_tuple(3, "3"), std::make_tuple(5, "5"), std::make_tuple(7, "7")};
+const std::array<TestType, 16> kTestParam = {
+  // 1. Базовые случаи
+  std::make_tuple(std::string("Hello world"), 2),
+  std::make_tuple(std::string("One two  three   four"), 4),
+  std::make_tuple(std::string("   Leading and trailing spaces   "), 4),
+  std::make_tuple(std::string("Word-with-hyphen counted-as-one"), 2),
+  std::make_tuple(std::string(""), 0),
+  std::make_tuple(std::string("a"), 1),
+
+  // 2. Пунктуация и символы
+  std::make_tuple(std::string("Hello, world!"), 2),
+  std::make_tuple(std::string("Wait... what?! Really?!"), 3),
+  std::make_tuple(std::string("end-of-line; another_word, and.one and , one ; and ,one"), 8),
+  std::make_tuple(std::string("Hello-world"), 1),
+
+  // 3. Пробелы и переносы строк
+  std::make_tuple(std::string("Line1\nLine2\n\nLine3"), 3),
+  std::make_tuple(std::string("Tabs\tand spaces   mixed"), 4),
+  std::make_tuple(std::string("   Multiple\n\twhitespace\nwords   "), 3),
+
+  // 4. Большие буквы, цифры, смешанный регистр
+  std::make_tuple(std::string("Hello WORLD mixed CASE"), 4),
+  std::make_tuple(std::string("Numbers 123 and letters ABC123xyz"), 5),
+
+  // 5. Много предложений
+  std::make_tuple(std::string("This is sentence one. Here is two! And three? Yes."), 10)
+};
 
 const auto kTestTasksList =
     std::tuple_cat(ppc::util::AddFuncTask<BaldinAWordCountMPI, InType>(kTestParam, PPC_SETTINGS_baldin_a_word_count),
@@ -76,9 +93,9 @@ const auto kTestTasksList =
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
-const auto kPerfTestName = NesterovARunFuncTestsProcesses::PrintFuncTestName<NesterovARunFuncTestsProcesses>;
+const auto kPerfTestName = BaldinAWordCountFuncTests::PrintFuncTestName<BaldinAWordCountFuncTests>;
 
-INSTANTIATE_TEST_SUITE_P(PicMatrixTests, NesterovARunFuncTestsProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(WordCountTests, BaldinAWordCountFuncTests, kGtestValues, kPerfTestName);
 
 }  // namespace
 
--- tasks/baldin_a_word_count/tests/performance/main.cpp
+++ tasks/baldin_a_word_count/tests/performance/main.cpp
@@ -1,4 +1,8 @@
+
 #include <gtest/gtest.h>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 #include "baldin_a_word_count/common/include/common.hpp"
 #include "baldin_a_word_count/mpi/include/ops_mpi.hpp"
@@ -7,24 +11,49 @@
 
 namespace baldin_a_word_count {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
-  InType input_data_{};
+class BaldinAWordCountPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
+private:
+  InType input_data;
+  OutType expected_output{};
 
   void SetUp() override {
-    input_data_ = kCount_;
+    std::string abs_path = ppc::util::GetAbsoluteTaskPath(PPC_ID_baldin_a_word_count, "book-war-and-peace.txt");
+    
+    std::ifstream file(abs_path);
+    if (!file.is_open()) {
+      throw std::runtime_error("File reading error");
+    }
+
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    input_data = buffer.str();
+    file.close();
+
+    int count = 0;
+    bool in_word = false;
+    for (char c : input_data) {
+        if (std::isalnum(c) || c == '-' || c == '_') {
+            if (!in_word) {
+                in_word = true;
+                count++;
+            }
+        } else {
+            in_word = false;
+        }
+    }
+    expected_output = count;
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return input_data_ == output_data;
+    return output_data == expected_output;
   }
 
   InType GetTestInputData() final {
-    return input_data_;
+    return input_data;
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(BaldinAWordCountPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +62,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = BaldinAWordCountPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, BaldinAWordCountPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace baldin_a_word_count
