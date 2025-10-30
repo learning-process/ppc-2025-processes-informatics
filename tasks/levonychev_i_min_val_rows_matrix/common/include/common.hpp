65f1c1008f2e1918fbbb0a6aa19fb01ca898bc83
fartekool <levonychev2006@gmail.com>
2025-10-30 14:52:49 +0300

    int -> size_t in common.hpp

--- tasks/levonychev_i_min_val_rows_matrix/common/include/common.hpp
+++ tasks/levonychev_i_min_val_rows_matrix/common/include/common.hpp
@@ -8,9 +8,9 @@
 
 namespace levonychev_i_min_val_rows_matrix {
 
-using InType = std::tuple<std::vector<double>, int, int>;
+using InType = std::tuple<std::vector<double>, size_t, size_t>;
 using OutType = std::vector<double>;
-using TestType = std::tuple<int, int>;
+using TestType = std::tuple<size_t, size_t>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace levonychev_i_min_val_rows_matrix
