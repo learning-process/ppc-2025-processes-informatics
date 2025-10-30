2c32510fc9d59ed7051007d57e7960df880a816c
root <abaldin05@mail.ru>
2025-10-30 19:09:35 +0000

    Update mpi version

--- tasks/baldin_a_word_count/mpi/src/ops_mpi.cpp
+++ tasks/baldin_a_word_count/mpi/src/ops_mpi.cpp
@@ -61,23 +61,45 @@ bool BaldinAWordCountMPI::RunImpl() {
     return true;
   }
 
+  int total_len = static_cast<int>(input.size());
+  int part = total_len / world_size;
+
   std::vector<int> send_counts(world_size), displs(world_size);
-  int part = (input.size() + world_size - 1) / world_size;
 
   if (rank == 0) {
+    int offset = 0;
     for (int i = 0; i < world_size; i++) {
-      displs[i] = i * part;
-      send_counts[i] = (i == world_size - 1) ? (input.size() - displs[i]) : part + 1;
+      if (i < world_size - 1) {
+        send_counts[i] = part + 1;
+      }
+      else {
+        send_counts[i] = total_len - offset;
+      }
+      displs[i] = offset;
+      offset += part;
     }
   }
 
-  std::vector<char> local_buf(part + 1);
-  MPI_Scatterv(input.data(), send_counts.data(), displs.data(), MPI_CHAR, local_buf.data(), part + 1, MPI_CHAR, 0,
-               MPI_COMM_WORLD);
+  // if (rank == 0) {
+  //   std::cout << total_len << '\n';
+  //   for (int i = 0; i < world_size; i++) {
+  //     std::cout << send_counts[i] << ' ';
+  //   }
+  //   std::cout << '\n';
+  // }
+
+  int recv_count = (rank == world_size - 1)
+                   ? total_len - part * (world_size - 1)
+                   : part + 1;
+  
+  std::vector<char> local_buf(recv_count);
+
+  MPI_Scatterv(input.data(), send_counts.data(), displs.data(), MPI_CHAR, local_buf.data(), recv_count, MPI_CHAR, 0,
+                MPI_COMM_WORLD);
 
   size_t local_cnt = 0;
   bool in_word = false;
-  for (int i = 0; i < part; i++) {
+  for (int i = 0; i < static_cast<int>(local_buf.size()); i++) {
     if (std::isalnum(local_buf[i]) || local_buf[i] == '-' || local_buf[i] == '_') {
       if (!in_word) {
         in_word = true;
@@ -88,29 +110,18 @@ bool BaldinAWordCountMPI::RunImpl() {
     }
   }
   if (rank != world_size - 1) {
-    if ((std::isalnum(local_buf[part - 1]) || local_buf[part - 1] == '-' || local_buf[part - 1] == '_') &&
+    if ((std::isalnum(local_buf[part - 1]) || local_buf[part - 1] == '-' || local_buf[part - 1] == '_' || std::isspace(local_buf[part - 1])) &&
         (std::isalnum(local_buf[part]) || local_buf[part] == '-' || local_buf[part] == '_')) {
       local_cnt--;
     }
   }
-
-  /*
-  if (rank == 0) {
-    std::cout << "SIZE: " << input.size() << '\n';
-    std::cout << "PART: " << part + 1 << '\n';
-  }
-  std::cout << "RANK: " << rank << ' ' << "STRING: ";
-  for (int i = 0; i < part + 1; i++) {
-   std:: cout << local_buf[i];
-  }
-  std::cout << "..." <<'\n';
-  */
-
+  
   size_t global_cnt = 0;
   MPI_Reduce(&local_cnt, &global_cnt, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   if (rank == 0) {
     GetOutput() = global_cnt;
+    //std::cout << "ASWERRRRR: " << global_cnt << '\n';
   }
 
   MPI_Bcast(static_cast<void *>(&GetOutput()), 1, MPI_INT, 0, MPI_COMM_WORLD);
