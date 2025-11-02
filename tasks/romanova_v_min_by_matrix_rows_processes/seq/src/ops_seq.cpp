79cfb2bea48495d40ba545141c595ad79c28d7aa
vassyusha <romanoffmiss@yandex.ru>
2025-11-03 00:31:32 +0300

    fixes in seq version

--- tasks/romanova_v_min_by_matrix_rows_processes/seq/src/ops_seq.cpp
+++ tasks/romanova_v_min_by_matrix_rows_processes/seq/src/ops_seq.cpp
@@ -14,7 +14,7 @@ namespace romanova_v_min_by_matrix_rows_processes {
 RomanovaVMinByMatrixRowsSEQ::RomanovaVMinByMatrixRowsSEQ(const InType &in) {
   SetTypeOfTask(GetStaticTypeOfTask());
   GetInput() = in;
-  GetOutput() = std::vector<int>(in.size());
+  GetOutput() = OutType(in.size());
 }
 
 bool RomanovaVMinByMatrixRowsSEQ::ValidationImpl() {
@@ -25,15 +25,15 @@ bool RomanovaVMinByMatrixRowsSEQ::PreProcessingImpl() {
   in_data_ = GetInput();
   n_ = in_data_.size();
   m_ = in_data_[0].size();
-  res_ = std::vector<int>(n_);
+  res_ = OutType(n_);
   return true;
 }
 
 bool RomanovaVMinByMatrixRowsSEQ::RunImpl() {
   int min_val;
   for(int i = 0; i < n_; i++){
-    min_val = INT_MAX;
-    for(int j = 0; j < m_; j++){
+    min_val = in_data_[i][0];
+    for(int j = 1; j < m_; j++){
       if(in_data_[i][j] < min_val) min_val = in_data_[i][j];
     }
     res_[i] = min_val;
