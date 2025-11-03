11a389e1ce601627d27f9cfeb70ad14d135106d0
root <abaldin05@mail.ru>
2025-11-03 16:45:40 +0000

    Add back the tests

--- tasks/baldin_a_word_count/tests/functional/main.cpp
+++ tasks/baldin_a_word_count/tests/functional/main.cpp
@@ -59,46 +59,45 @@ TEST_P(BaldinAWordCountFuncTests, MatmulFromPic) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 1> kTestParam = {std::make_tuple(std::string("Hello world"), 2)};
-// std::make_tuple(std::string("One two  three   four"), 4),
-// std::make_tuple(std::string("   Leading and trailing spaces   "), 4),
-// std::make_tuple(std::string("Word-with-hyphen counted-as-one"), 2),
-// std::make_tuple(std::string(""), 0),
-// std::make_tuple(std::string("a"), 1),
+const std::array<TestType, 30> kTestParam = {
+    std::make_tuple(std::string("Hello world"), 2),
+    std::make_tuple(std::string("One two  three   four"), 4),
+    std::make_tuple(std::string("   Leading and trailing spaces   "), 4),
+    std::make_tuple(std::string("Word-with-hyphen counted-as-one"), 2),
+    std::make_tuple(std::string(""), 0),
+    std::make_tuple(std::string("a"), 1),
 
-//     std::make_tuple(std::string(" "), 0),
-//     std::make_tuple(std::string("  _ "), 1),
-//     std::make_tuple(std::string("_A_"), 1),
-//     std::make_tuple(std::string("-_-_-"), 1),
-//     std::make_tuple(std::string("a-b_c"), 1),
-//     std::make_tuple(std::string("!@#$%^&*()"), 0),
+    std::make_tuple(std::string(" "), 0),
+    std::make_tuple(std::string("  _ "), 1),
+    std::make_tuple(std::string("_A_"), 1),
+    std::make_tuple(std::string("-_-_-"), 1),
+    std::make_tuple(std::string("a-b_c"), 1),
+    std::make_tuple(std::string("!@#$%^&*()"), 0),
 
-//     std::make_tuple(std::string("Hello, world!"), 2),
-//     std::make_tuple(std::string("Wait... what?! Really?!"), 3),
-//     std::make_tuple(std::string("end-of-line; another_word, and.one and , one ; and ,one"), 8),
-//     std::make_tuple(std::string("Hello-world"), 1),
+    std::make_tuple(std::string("Hello, world!"), 2),
+    std::make_tuple(std::string("Wait... what?! Really?!"), 3),
+    std::make_tuple(std::string("end-of-line; another_word, and.one and , one ; and ,one"), 8),
+    std::make_tuple(std::string("Hello-world"), 1),
 
-//     std::make_tuple(std::string("Line1\nLine2\n\nLine3"), 3),
-//     std::make_tuple(std::string("Tabs\tand spaces   mixed"), 4),
-//     std::make_tuple(std::string("   Multiple\n\twhitespace\nwords   "), 3),
+    std::make_tuple(std::string("Line1\nLine2\n\nLine3"), 3),
+    std::make_tuple(std::string("Tabs\tand spaces   mixed"), 4),
+    std::make_tuple(std::string("   Multiple\n\twhitespace\nwords   "), 3),
 
-//     std::make_tuple(std::string("Hello WORLD mixed CASE"), 4),
-//     std::make_tuple(std::string("Numbers 123 and letters ABC123xyz"), 5),
+    std::make_tuple(std::string("Hello WORLD mixed CASE"), 4),
+    std::make_tuple(std::string("Numbers 123 and letters ABC123xyz"), 5),
 
-//     std::make_tuple(std::string("This is sentence one. Here is two! And three? Yes."), 10),
+    std::make_tuple(std::string("This is sentence one. Here is two! And three? Yes."), 10),
 
-//     std::make_tuple(std::string("one,two;three.four"), 4),
-//     std::make_tuple(std::string("word1\tword2\nword3\rword4"), 4),
-//     std::make_tuple(std::string("mix of-different_kinds words"), 3),
+    std::make_tuple(std::string("one,two;three.four"), 4),
+    std::make_tuple(std::string("word1\tword2\nword3\rword4"), 4),
+    std::make_tuple(std::string("mix of-different_kinds words"), 3),
 
-//     std::make_tuple(std::string("abcdefgh ijklmnop qrstuv wx yz"), 5),
-//     std::make_tuple(std::string("aaaaa bbbbb ccccc ddddd eeeee fffff ggggg hhhhh"), 8),
-//     std::make_tuple(std::string("12345 67890 abcde fghij klmno pqrst uvwxyz"), 7),
+    std::make_tuple(std::string("abcdefgh ijklmnop qrstuv wx yz"), 5),
+    std::make_tuple(std::string("aaaaa bbbbb ccccc ddddd eeeee fffff ggggg hhhhh"), 8),
+    std::make_tuple(std::string("12345 67890 abcde fghij klmno pqrst uvwxyz"), 7),
 
-//     std::make_tuple(std::string("  a   b   c   "), 3),
-//     std::make_tuple(std::string("   A    "), 1)
-
-// };
+    std::make_tuple(std::string("  a   b   c   "), 3),
+    std::make_tuple(std::string("   A    "), 1)};
 
 const auto kTestTasksList =
     std::tuple_cat(ppc::util::AddFuncTask<BaldinAWordCountMPI, InType>(kTestParam, PPC_SETTINGS_baldin_a_word_count),
