7f09273e80889f4ccd346f3de1fa475e71ff4a84
brb484ul <pppphp2800@gmail.com>
2025-11-04 12:07:25 +0300

    add task, build

---
+++ tasks/khruev_a_min_elem_vec/tests/performance/main.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+
+#include "khruev_a_min_elem_vec/common/include/common.hpp"
+#include "khruev_a_min_elem_vec/mpi/include/ops_mpi.hpp"
+#include "khruev_a_min_elem_vec/seq/include/ops_seq.hpp"
+#include "util/include/perf_test_util.hpp"
+
+namespace khruev_a_min_elem_vec {
+
+class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+  const int kCount_ = 100;
+  InType input_data_{};
+
+  void SetUp() override {
+    input_data_ = kCount_;
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return input_data_ == output_data;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+};
+
+TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+  ExecuteTest(GetParam());
+}
+
+const auto kAllPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, KhruevAMinElemVecMPI, KhruevAMinElemVecSEQ>(PPC_SETTINGS_khruev_a_min_elem_vec);
+
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
+const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+
+}  // namespace khruev_a_min_elem_vec
