fb107ade735ae0a5eaf9f134623251aad317d5a5
Pylaeva Svetlana <pl.svetlanaa.a@gmail.com>
2025-11-13 13:51:04 +0300

    add mpi version

--- tasks/pylaeva_s_max_elem_matrix/mpi/src/ops_mpi.cpp
+++ tasks/pylaeva_s_max_elem_matrix/mpi/src/ops_mpi.cpp
@@ -17,56 +17,61 @@ PylaevaSMaxElemMatrixMPI::PylaevaSMaxElemMatrixMPI(const InType &in) {
 }
 
 bool PylaevaSMaxElemMatrixMPI::ValidationImpl() {
-  return (GetInput() > 0) && (GetOutput() == 0);
+  return (static_cast<size_t>(std::get<0>(GetInput()))==std::get<1>(GetInput()).size()) && (static_cast<size_t>(std::get<0>(GetInput()))>0) && (GetOutput() == 0);
 }
 
 bool PylaevaSMaxElemMatrixMPI::PreProcessingImpl() {
-  GetOutput() = 2 * GetInput();
-  return GetOutput() > 0;
+  GetOutput() = std::numeric_limits<int>::min();
+  return true;
 }
 
 bool PylaevaSMaxElemMatrixMPI::RunImpl() {
-  auto input = GetInput();
-  if (input == 0) {
-    return false;
-  }
-
-  for (InType i = 0; i < GetInput(); i++) {
-    for (InType j = 0; j < GetInput(); j++) {
-      for (InType k = 0; k < GetInput(); k++) {
-        std::vector<InType> tmp(i + j + k, 1);
-        GetOutput() += std::accumulate(tmp.begin(), tmp.end(), 0);
-        GetOutput() -= i + j + k;
-      }
-    }
-  }
-
-  const int num_threads = ppc::util::GetNumThreads();
-  GetOutput() *= num_threads;
+  const auto& matrix_data = std::get<1>(GetInput());
+  const auto& matrix_size = static_cast<int>(std::get<0>(GetInput()));
 
   int rank = 0;
+  int size = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  if (rank == 0) {
-    GetOutput() /= num_threads;
+  MPI_Bcast(&matrix_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+  int local_size = matrix_size / size;
+  int remainder = matrix_size % size;
+
+  int start_idx = 0;
+  int end_idx = 0;
+
+  if (rank < remainder) {
+    start_idx = rank * (local_size + 1);
+    end_idx = start_idx + local_size + 1;
   } else {
-    int counter = 0;
-    for (int i = 0; i < num_threads; i++) {
-      counter++;
-    }
+    start_idx = (remainder * (local_size + 1)) + ((rank - remainder) * local_size);
+    end_idx = start_idx + local_size;
+  }
 
-    if (counter != 0) {
-      GetOutput() /= counter;
+  int local_max = INT_MIN;
+  
+  for (int i = start_idx; i < end_idx && i < matrix_size; i++) {
+    if (matrix_data[i] > local_max) {
+      local_max = matrix_data[i];
     }
   }
 
-  MPI_Barrier(MPI_COMM_WORLD);
-  return GetOutput() > 0;
+  if (local_size == 0 && rank >= matrix_size) {
+    local_max = INT_MIN;
+  }
+
+  int global_max = 0;
+  MPI_Allreduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
+  GetOutput() = global_max;
+
+  return true;
 }
 
 bool PylaevaSMaxElemMatrixMPI::PostProcessingImpl() {
-  GetOutput() -= GetInput();
-  return GetOutput() > 0;
+  return true;
 }
 
 }  // namespace pylaeva_s_max_elem_matrix
