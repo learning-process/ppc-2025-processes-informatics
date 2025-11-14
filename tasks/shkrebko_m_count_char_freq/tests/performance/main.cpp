d2594339ab66655af921d235042f508a82c37c72
Shkrebko-Misha <michael.shkrebko@gmail.com>
2025-11-14 19:56:17 +0300

    clang-format

--- tasks/shkrebko_m_count_char_freq/tests/performance/main.cpp
+++ tasks/shkrebko_m_count_char_freq/tests/performance/main.cpp
@@ -1,60 +1,59 @@
-  #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-  #include "shkrebko_m_count_char_freq/common/include/common.hpp"
-  #include "shkrebko_m_count_char_freq/mpi/include/ops_mpi.hpp"
-  #include "shkrebko_m_count_char_freq/seq/include/ops_seq.hpp"
-  #include "util/include/perf_test_util.hpp"
+#include "shkrebko_m_count_char_freq/common/include/common.hpp"
+#include "shkrebko_m_count_char_freq/mpi/include/ops_mpi.hpp"
+#include "shkrebko_m_count_char_freq/seq/include/ops_seq.hpp"
+#include "util/include/perf_test_util.hpp"
 
-  namespace shkrebko_m_count_char_freq {
+namespace shkrebko_m_count_char_freq {
 
-  class ShkrebkoMCountCharFreqPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-    InType input_data_;
-    OutType expected_data_;
+class ShkrebkoMCountCharFreqPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
+  InType input_data_;
+  OutType expected_data_;
 
-    void SetUp() override {
-      std::string large_text;
-      const int target_size = 100000000;
-      const std::string base_text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  void SetUp() override {
+    std::string large_text;
+    const int target_size = 100000000;
+    const std::string base_text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-      while (large_text.size() < target_size) {
-        large_text += base_text;
+    while (large_text.size() < target_size) {
+      large_text += base_text;
+    }
+
+    large_text = large_text.substr(0, target_size);
+
+    std::string target_char = "e";
+
+    expected_data_ = 0;
+    for (char c : large_text) {
+      if (c == target_char[0]) {
+        expected_data_++;
       }
-
-      large_text = large_text.substr(0, target_size);
-
-      std::string target_char = "e";  
-
-      expected_data_ = 0;
-      for (char c : large_text) {
-        if (c == target_char[0]) {
-          expected_data_++;
-        }
-      }
-
-      input_data_ = std::make_tuple(large_text, target_char);
     }
 
-    bool CheckTestOutputData(OutType &output_data) final {
-      return output_data == expected_data_;
-    }
-
-    InType GetTestInputData() final {
-      return input_data_;
-    }
-  };
-
-  TEST_P(ShkrebkoMCountCharFreqPerfTests, RunPerfModes) {
-    ExecuteTest(GetParam());
+    input_data_ = std::make_tuple(large_text, target_char);
   }
 
-  const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, ShkrebkoMCountCharFreqMPI, ShkrebkoMCountCharFreqSEQ>(
-      PPC_SETTINGS_shkrebko_m_count_char_freq);
+  bool CheckTestOutputData(OutType &output_data) final {
+    return output_data == expected_data_;
+  }
 
-  const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+};
 
-  const auto kPerfTestName = ShkrebkoMCountCharFreqPerfTests::CustomPerfTestName;
+TEST_P(ShkrebkoMCountCharFreqPerfTests, RunPerfModes) {
+  ExecuteTest(GetParam());
+}
 
-  INSTANTIATE_TEST_SUITE_P(RunModeTests, ShkrebkoMCountCharFreqPerfTests, kGtestValues, kPerfTestName);
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, ShkrebkoMCountCharFreqMPI, ShkrebkoMCountCharFreqSEQ>(
+    PPC_SETTINGS_shkrebko_m_count_char_freq);
 
-  }  // namespace shkrebko_m_count_char_freq
-  
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
+const auto kPerfTestName = ShkrebkoMCountCharFreqPerfTests::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests, ShkrebkoMCountCharFreqPerfTests, kGtestValues, kPerfTestName);
+
+}  // namespace shkrebko_m_count_char_freq
