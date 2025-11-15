0f68dfde3762c5c51e6e64f80cd60a0ab1cfcebf
fartekool <levonychev2006@gmail.com>
2025-11-15 20:51:43 +0300

    Added static (clang-tidy fix)

--- tasks/levonychev_i_min_val_rows_matrix/mpi/include/ops_mpi.hpp
+++ tasks/levonychev_i_min_val_rows_matrix/mpi/include/ops_mpi.hpp
@@ -17,7 +17,7 @@ class LevonychevIMinValRowsMatrixMPI : public BaseTask {
   bool PreProcessingImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
-  void SetLocalRows(int rows, int cols, int proc_rank, int proc_num, int *local_rows, int *start_id);
+  static void SetLocalRows(int rows, int cols, int proc_rank, int proc_num, int *local_rows, int *start_id);
 };
 
 }  // namespace levonychev_i_min_val_rows_matrix
