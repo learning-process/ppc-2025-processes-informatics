4b0f50dc269eb00cdfed87e467633e3b6b416f87
Makoi66 <makoi199563@gmail.com>
2025-11-15 12:49:03 +0000

    fix_tests_6

--- tasks/makovskiy_i_min_value_in_matrix_rows/tests/functional/main.cpp
+++ tasks/makovskiy_i_min_value_in_matrix_rows/tests/functional/main.cpp
@@ -1,96 +1,104 @@
 #include <gtest/gtest.h>
 #include <mpi.h>
 
-#include <array>
+#include <algorithm>
 #include <stdexcept>
 #include <string>
-#include <tuple>
 #include <vector>
 
 #include "makovskiy_i_min_value_in_matrix_rows/common/include/common.hpp"
 #include "makovskiy_i_min_value_in_matrix_rows/mpi/include/ops_mpi.hpp"
 #include "makovskiy_i_min_value_in_matrix_rows/seq/include/ops_seq.hpp"
-#include "util/include/func_test_util.hpp"
 #include "util/include/util.hpp"
 
 namespace makovskiy_i_min_value_in_matrix_rows {
 
-class MinValueFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
- public:
-  static std::string PrintTestParam(const TestType &test_param) {
-    return std::get<2>(test_param);
-  }
+enum class TaskType { SEQ, MPI };
+enum class TestExpectation { SUCCESS, FAIL_VALIDATION, THROW_CONSTRUCTION };
 
- protected:
-  InType input_data_{};
-  OutType expected_output_{};
+using TestParam = std::tuple<TaskType, std::string, InType, OutType, TestExpectation>;
 
-  void SetUp() override {
-    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    input_data_ = std::get<0>(params);
-    expected_output_ = std::get<1>(params);
-  }
+class MinValueFunctionalTests : public ::testing::TestWithParam<TestParam> {};
 
-  InType GetTestInputData() final {
-    return input_data_;
-  }
+TEST_P(MinValueFunctionalTests, AllCases) {
+  const auto &[task_type, test_name, input_data, expected_output, expectation] = GetParam();
 
-  bool CheckTestOutputData(OutType &output_data) final {
-    int rank = 0;
-    if (ppc::util::IsUnderMpirun()) {
-      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  switch (expectation) {
+    case TestExpectation::SUCCESS: {
+      std::shared_ptr<BaseTask> task;
+      if (task_type == TaskType::SEQ) {
+        task = std::make_shared<MinValueSEQ>(input_data);
+      } else {
+        task = std::make_shared<MinValueMPI>(input_data);
+      }
+
+      ASSERT_TRUE(task->Validation());
+      ASSERT_TRUE(task->PreProcessing());
+      ASSERT_TRUE(task->Run());
+      ASSERT_TRUE(task->PostProcessing());
+
+      if (ppc::util::IsUnderMpirun()) {
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        if (rank == 0) {
+          ASSERT_EQ(task->GetOutput(), expected_output);
+        }
+      } else {
+        ASSERT_EQ(task->GetOutput(), expected_output);
+      }
+      break;
     }
-    if (rank == 0) {
-      return output_data == expected_output_;
-    }
-    return true;
-  }
-};
+    case TestExpectation::FAIL_VALIDATION: {
+      std::shared_ptr<BaseTask> task;
+      if (task_type == TaskType::SEQ) {
+        task = std::make_shared<MinValueSEQ>(input_data);
+      } else {
+        task = std::make_shared<MinValueMPI>(input_data);
+      }
 
-namespace {
-
-TEST_P(MinValueFuncTests, MinValueTestAllCases) {
-  auto task_getter = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTaskGetter)>(GetParam());
-  const auto &test_params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-  const auto &input_data = std::get<0>(test_params);
-  const auto &test_name = std::get<2>(test_params);
-
-  bool expect_validation_fail = (test_name.find("Invalid") != std::string::npos);
-
-  if (expect_validation_fail) {
-    if (test_name == "Invalid_Empty_Matrix" && !ppc::util::IsUnderMpirun()) {
-      ASSERT_THROW(task_getter(input_data), std::invalid_argument);
-    } else {
-      auto task = task_getter(input_data);
       ASSERT_FALSE(task->Validation());
+      task->PreProcessing();
+      task->Run();
+      task->PostProcessing();
+      break;
     }
-  } else {
-    auto task = task_getter(input_data);
-    ASSERT_TRUE(task->Validation());
-    ASSERT_TRUE(task->PreProcessing());
-    ASSERT_TRUE(task->Run());
-    ASSERT_TRUE(task->PostProcessing());
-    ASSERT_TRUE(CheckTestOutputData(task->GetOutput()));
+    case TestExpectation::THROW_CONSTRUCTION: {
+      if (task_type == TaskType::SEQ && !ppc::util::IsUnderMpirun()) {
+        ASSERT_THROW((MinValueSEQ(input_data)), std::invalid_argument);
+      }
+      break;
+    }
   }
 }
 
-const std::array<TestType, 5> kTestCases = {
-    // Positive
-    std::make_tuple(InType{{1, 2, 3}, {4, 5, 6}}, OutType{1, 4}, "Small_Matrix_2x3"),
-    std::make_tuple(InType{{-1, 0}, {10, 2}, {7}}, OutType{-1, 2, 7}, "Matrix_With_Negative_3x2"),
-    std::make_tuple(InType{{8}}, OutType{8}, "Single_Element_Matrix_1x1"),
-    // Negative
-    std::make_tuple(InType{}, OutType{}, "Invalid_Empty_Matrix"),
-    std::make_tuple(InType{{1, 2}, {}}, OutType{}, "Invalid_Empty_Row")};
+auto GenerateTestName = [](const ::testing::TestParamInfo<TestParam> &info) {
+  std::string name = std::get<1>(info.param);
+  TaskType type = std::get<0>(info.param);
+  std::string type_str = (type == TaskType::SEQ) ? "SEQ_" : "MPI_";
+  std::replace(name.begin(), name.end(), '/', '_');
+  return type_str + name;
+};
 
-const auto kTasks = std::tuple_cat(
-    ppc::util::AddFuncTask<MinValueSEQ, InType>(kTestCases, PPC_SETTINGS_makovskiy_i_min_value_in_matrix_rows),
-    ppc::util::AddFuncTask<MinValueMPI, InType>(kTestCases, PPC_SETTINGS_makovskiy_i_min_value_in_matrix_rows));
+INSTANTIATE_TEST_SUITE_P(
+    MinValueTests, MinValueFunctionalTests,
+    ::testing::Values(
+        // Positive
+        TestParam{TaskType::SEQ, "Positive_2x3", {{1, 2, 3}, {4, 5, 6}}, {1, 4}, TestExpectation::SUCCESS},
+        TestParam{TaskType::MPI, "Positive_2x3", {{1, 2, 3}, {4, 5, 6}}, {1, 4}, TestExpectation::SUCCESS},
+        TestParam{
+            TaskType::SEQ, "Positive_NegativeVals", {{-1, 0}, {10, 2}, {7}}, {-1, 2, 7}, TestExpectation::SUCCESS},
+        TestParam{
+            TaskType::MPI, "Positive_NegativeVals", {{-1, 0}, {10, 2}, {7}}, {-1, 2, 7}, TestExpectation::SUCCESS},
+        TestParam{TaskType::SEQ, "Positive_Single", {{8}}, {8}, TestExpectation::SUCCESS},
+        TestParam{TaskType::MPI, "Positive_Single", {{8}}, {8}, TestExpectation::SUCCESS},
 
-const auto kGtestValues = ppc::util::ExpandToValues(kTasks);
-const auto kTestName = MinValueFuncTests::PrintFuncTestName<MinValueFuncTests>;
+        // Negative: THROW_CONSTRUCTION
+        TestParam{TaskType::SEQ, "Invalid_EmptyMatrix", {}, {}, TestExpectation::THROW_CONSTRUCTION},
 
-INSTANTIATE_TEST_SUITE_P(MinValueAllTests, MinValueFuncTests, kGtestValues, kTestName);
+        // Negative: FAIL_VALIDATION
+        TestParam{TaskType::MPI, "Invalid_EmptyMatrix", {}, {}, TestExpectation::FAIL_VALIDATION},
+        TestParam{TaskType::SEQ, "Invalid_EmptyRow", {{1, 2}, {}}, {}, TestExpectation::FAIL_VALIDATION},
+        TestParam{TaskType::MPI, "Invalid_EmptyRow", {{1, 2}, {}}, {}, TestExpectation::FAIL_VALIDATION}),
+    GenerateTestName);
 
-}  // namespace
 }  // namespace makovskiy_i_min_value_in_matrix_rows
