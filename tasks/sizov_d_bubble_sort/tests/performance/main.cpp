5d5ff6958a50321a8e7b99f9f0bb6014b86b6e33
SizovDmitry <you@example.com>
2025-11-16 20:47:23 +0000

    Changed the size of n

--- tasks/sizov_d_bubble_sort/tests/performance/main.cpp
+++ tasks/sizov_d_bubble_sort/tests/performance/main.cpp
@@ -20,7 +20,7 @@ class SizovDRunPerfTestsBubbleSort : public ppc::util::BaseRunPerfTests<InType, 
     const std::string &test_name = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kNameTest)>(param);
     const bool is_mpi = test_name.find("_mpi_") != std::string::npos;
 
-    const std::size_t n = is_mpi ? 450000 : 185000;
+    const std::size_t n = is_mpi ? 250000 : 180000;
 
     std::vector<int> data(n);
     for (std::size_t i = 0; i < n; ++i) {
