12bf865da6f193a4a6fef53d4cbda72947f6966a
P_Alex_Tr <aleksandr.smyshlaev@yandex.ru>
2025-11-16 14:39:11 +0300

    fix_func_tests

--- tasks/smyshlaev_a_str_order_check/common/include/common.hpp
+++ tasks/smyshlaev_a_str_order_check/common/include/common.hpp
@@ -10,7 +10,7 @@ namespace smyshlaev_a_str_order_check {
 
 using InType = std::pair<std::string, std::string>;
 using OutType = int;
-using TestType = std::tuple<std::string, std::string, int>;
+using TestType = std::tuple<std::string, std::string, int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace smyshlaev_a_str_order_check
--- tasks/smyshlaev_a_str_order_check/tests/functional/main.cpp
+++ tasks/smyshlaev_a_str_order_check/tests/functional/main.cpp
@@ -18,17 +18,7 @@ namespace smyshlaev_a_str_order_check {
 class SmyshlaevAStrOrderCheckRunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
-    const auto &str_a = std::get<0>(test_param);
-    const auto &str_b = std::get<1>(test_param);
-    const auto &expected = std::get<2>(test_param);
-
-    std::string expected_str;
-    if (expected < 0) {
-      expected_str = "neg_" + std::to_string(std::abs(expected));
-    } else {
-      expected_str = std::to_string(expected);
-    }
-    return str_a + "_" + str_b + "_expect_" + expected_str;
+    return std::get<3>(test_param);
   }
 
  protected:
@@ -58,12 +48,18 @@ TEST_P(SmyshlaevAStrOrderCheckRunFuncTestsProcesses, StringOrderCheckTest) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 6> kTestParam = {std::make_tuple(std::string("apple"), std::string("apple"), 0),
-                                            std::make_tuple(std::string("apple"), std::string("banana"), -1),
-                                            std::make_tuple(std::string("zebra"), std::string("yak"), 1),
-                                            std::make_tuple(std::string("cat"), std::string("caterpillar"), -1),
-                                            std::make_tuple(std::string("caterpillar"), std::string("cat"), 1),
-                                            std::make_tuple(std::string("Zebra"), std::string("zebra"), -1)};
+const std::array<TestType, 11> kTestParam = {
+    std::make_tuple(std::string("car"), std::string("car"), 0, std::string("equal_not_empty_strings")),
+    std::make_tuple(std::string("air"), std::string("bike"), -1, std::string("first_string_less_than_second")),
+    std::make_tuple(std::string("plane"), std::string("material"), 1, std::string("first_string_greater_than_second")),
+    std::make_tuple(std::string("abc"), std::string("abcdefg"), -1, std::string("first_string_prefix_of_second")),
+    std::make_tuple(std::string("gdefg"), std::string("gde"), 1, std::string("second_string_prefix_of_first")),
+    std::make_tuple(std::string("Abc"), std::string("abc"), -1, std::string("case_sensitive_comparison")),
+    std::make_tuple(std::string(""), std::string(""), 0, std::string("both_strings_empty")),
+    std::make_tuple(std::string("a"), std::string(""), 1, std::string("right_string_empty")),
+    std::make_tuple(std::string(""), std::string("a"), -1, std::string("left_string_empty")),
+    std::make_tuple(std::string("a"), std::string("a"), 0, std::string("characters_equal")),
+    std::make_tuple(std::string("a"), std::string("b"), -1, std::string("left_character_less"))};
 
 const auto kTestTasksList = std::tuple_cat(
     ppc::util::AddFuncTask<SmyshlaevAStrOrderCheckMPI, InType>(kTestParam, PPC_SETTINGS_smyshlaev_a_str_order_check),
