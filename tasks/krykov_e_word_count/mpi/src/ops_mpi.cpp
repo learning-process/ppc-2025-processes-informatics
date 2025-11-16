f2cbe735528aca397c3ff07f0c9cdced4a55eb29
Gizmo43 <egarus2005@gmail.com>
2025-11-16 22:28:09 +0300

    try19

--- tasks/krykov_e_word_count/mpi/src/ops_mpi.cpp
+++ tasks/krykov_e_word_count/mpi/src/ops_mpi.cpp
@@ -38,32 +38,9 @@ bool KrykovEWordCountMPI::RunImpl() {
     return true;
   }
 
-  // Если процессов больше чем символов, считаем на процессе 0
-  if (static_cast<size_t>(world_size) > text.size()) {
-    if (world_rank == 0) {
-      size_t count = 0;
-      bool in_word = false;
-      for (char c : text) {
-        if (std::isspace(static_cast<unsigned char>(c))) {
-          in_word = false;
-        } else {
-          if (!in_word) {
-            in_word = true;
-            count++;
-          }
-        }
-      }
-      GetOutput() = static_cast<int>(count);
-    }
-    MPI_Bcast(&GetOutput(), 1, MPI_INT, 0, MPI_COMM_WORLD);
-    return true;
-  }
-
-  // Размер текста и его передача всем процессам
   int text_size = static_cast<int>(text.size());
   MPI_Bcast(&text_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  // Вычисление размеров чанков
   int base_size = text_size / world_size;
   int remainder = text_size % world_size;
 
@@ -77,14 +54,12 @@ bool KrykovEWordCountMPI::RunImpl() {
     offset += chunk_sizes[i];
   }
 
-  // Каждый процесс получает свой чанк
   int local_size = chunk_sizes[world_rank];
   std::vector<char> local_chunk(local_size);
 
   MPI_Scatterv(world_rank == 0 ? text.data() : nullptr, chunk_sizes.data(), displs.data(), MPI_CHAR, local_chunk.data(),
                local_size, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-  // Локальный подсчет слов
   size_t local_count = 0;
   bool in_word = false;
   for (char c : local_chunk) {
@@ -98,64 +73,36 @@ bool KrykovEWordCountMPI::RunImpl() {
     }
   }
 
-  // Сбор информации о границах для коррекции
-  // Важно: анализируем реальные символы на границах
-  bool starts_with_letter = false;
-  bool ends_with_letter = false;
-
-  if (local_size > 0) {
-    starts_with_letter = !std::isspace(static_cast<unsigned char>(local_chunk[0]));
-    ends_with_letter = !std::isspace(static_cast<unsigned char>(local_chunk[local_size - 1]));
-  }
-
-  // Преобразуем bool в int для MPI
-  int starts_int = starts_with_letter ? 1 : 0;
-  int ends_int = ends_with_letter ? 1 : 0;
+  int starts_with_space = local_size > 0 ? (std::isspace(static_cast<unsigned char>(local_chunk[0])) ? 1 : 0) : 1;
+  int ends_with_space =
+      local_size > 0 ? (std::isspace(static_cast<unsigned char>(local_chunk[local_size - 1])) ? 1 : 0) : 1;
 
   std::vector<int> all_starts(world_size);
   std::vector<int> all_ends(world_size);
 
-  MPI_Gather(&starts_int, 1, MPI_INT, world_rank == 0 ? all_starts.data() : nullptr, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Gather(&ends_int, 1, MPI_INT, world_rank == 0 ? all_ends.data() : nullptr, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Gather(&starts_with_space, 1, MPI_INT, world_rank == 0 ? all_starts.data() : nullptr, 1, MPI_INT, 0,
+             MPI_COMM_WORLD);
+  MPI_Gather(&ends_with_space, 1, MPI_INT, world_rank == 0 ? all_ends.data() : nullptr, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  // Сбор локальных счетчиков
   std::vector<size_t> all_counts(world_size);
   MPI_Gather(&local_count, 1, MPI_UNSIGNED_LONG_LONG, world_rank == 0 ? all_counts.data() : nullptr, 1,
              MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
 
-  // Коррекция на процессе 0
-  size_t total_count = 0;
   if (world_rank == 0) {
-    // Суммируем все локальные счетчики
+    size_t total_count = 0;
     for (size_t count : all_counts) {
       total_count += count;
     }
 
-    // Коррекция разделенных слов
-    // Если предыдущий чанк заканчивается буквой И текущий начинается с буквы,
-    // значит слово было разделено и посчитано дважды
     for (int i = 1; i < world_size; ++i) {
-      if (all_ends[i - 1] == 1 && all_starts[i] == 1) {
+      if (all_ends[i - 1] == 0 && all_starts[i] == 0) {
         total_count--;
       }
     }
+
+    GetOutput() = static_cast<int>(total_count);
   }
 
-  // Распространение результата на все процессы
-  size_t global_count = 0;
-  MPI_Reduce(&local_count, &global_count, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-
-  if (world_rank == 0) {
-    // Применяем коррекцию к глобальному счетчику
-    for (int i = 1; i < world_size; ++i) {
-      if (all_ends[i - 1] == 1 && all_starts[i] == 1) {
-        global_count--;
-      }
-    }
-    GetOutput() = static_cast<int>(global_count);
-  }
-
-  // Распространяем финальный результат
   int result = 0;
   if (world_rank == 0) {
     result = GetOutput();
