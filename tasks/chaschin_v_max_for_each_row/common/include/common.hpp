f951da2223e7737e13ab876f08a7fff21108f67f
kodt2 <kodt2005@yandex.ru>
2025-11-17 15:41:21 +0300

    testingbuilding

--- tasks/chaschin_v_max_for_each_row/common/include/common.hpp
+++ tasks/chaschin_v_max_for_each_row/common/include/common.hpp
@@ -9,6 +9,7 @@ namespace chaschin_v_max_for_each_row {
 
 using InType = int;
 using OutType = int;
+int f = 1;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
