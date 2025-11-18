79f839cbae0da9c27bd09f68ee28a44f2b024276
kodt2 <kodt2005@yandex.ru>
2025-11-19 01:42:58 +0300

    fix for ubuntu / gcc-build (ubuntu-24.04-arm, Debug)

--- tasks/chaschin_v_max_for_each_row/mpi/include/ops_mpi.hpp
+++ tasks/chaschin_v_max_for_each_row/mpi/include/ops_mpi.hpp
@@ -17,6 +17,19 @@ class ChaschinVMaxForEachRow : public BaseTask {
   bool PreProcessingImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
+
+  struct RowRange {
+    int start;
+    int count;
+  };
+
+  RowRange ComputeRange(int nrows, int rank, int size);
+  std::vector<std::vector<float>> DistributeRows(const std::vector<std::vector<float>> &mat, int rank, int size,
+                                                 const RowRange &range);
+  std::vector<float> ComputeLocalMax(const std::vector<std::vector<float>> &local_mat);
+
+  void GatherResults(std::vector<float> &out, const std::vector<float> &local_out, int rank, int size,
+                     const RowRange &range);
 };
 
 }  // namespace chaschin_v_max_for_each_row
