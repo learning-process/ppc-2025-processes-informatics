557e6f3205ef1f9da608fe3c2eb7173704616913
AlenaK13 <kruglovatm@mail.ru>
2025-11-18 15:34:10 +0300

    Fix tests name

--- tasks/kruglova_max_diff_adjacent/tests/functional/main.cpp
+++ tasks/kruglova_max_diff_adjacent/tests/functional/main.cpp
@@ -1,3 +1,4 @@
+
 #include <gtest/gtest.h>
 #include <stb/stb_image.h>
 
@@ -20,7 +21,7 @@
 
 namespace kruglova_max_diff_adjacent {
 
-class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+class KruglovaAMaxDiffAdjacentTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
     return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
@@ -65,7 +66,7 @@ class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType
 
 namespace {
 
-TEST_P(NesterovARunFuncTestsProcesses, MatmulFromPic) {
+TEST_P(KruglovaAMaxDiffAdjacentTests, MatmulFromPic) {
   ExecuteTest(GetParam());
 }
 
@@ -77,9 +78,9 @@ const auto kTestTasksList =
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
-const auto kPerfTestName = NesterovARunFuncTestsProcesses::PrintFuncTestName<NesterovARunFuncTestsProcesses>;
+const auto kPerfTestName = KruglovaAMaxDiffAdjacentTests::PrintFuncTestName<KruglovaAMaxDiffAdjacentTests>;
 
-INSTANTIATE_TEST_SUITE_P(PicMatrixTests, NesterovARunFuncTestsProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(PicMatrixTests, KruglovaAMaxDiffAdjacentTests, kGtestValues, kPerfTestName);
 
 }  // namespace
 
--- tasks/kruglova_max_diff_adjacent/tests/performance/main.cpp
+++ tasks/kruglova_max_diff_adjacent/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace kruglova_max_diff_adjacent {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class KruglovaAMaxDiffAdjacentPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(KruglovaAMaxDiffAdjacentPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = KruglovaAMaxDiffAdjacentPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, KruglovaAMaxDiffAdjacentPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace kruglova_max_diff_adjacent
