14230adf48a8edf64a38d1aecfcdf46777a3ff0d
ulianamaslova25 <uliana-maslova@mail.ru>
2025-11-18 23:54:19 +0300

    fix TestType

--- tasks/maslova_u_char_frequency_count/common/include/common.hpp
+++ tasks/maslova_u_char_frequency_count/common/include/common.hpp
@@ -10,7 +10,7 @@ namespace maslova_u_char_frequency_count {
 
 using InType = std::pair<std::string, char>;
 using OutType = size_t;
-using TestType = std::tuple<std::pair<std::string, char>, size_t>;
+using TestType = std::tuple<InType, OutType, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace maslova_u_char_frequency_count
