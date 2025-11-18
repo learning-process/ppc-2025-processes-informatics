435b48a44dcc29ca6441a3bd28db1d11c3d952dd
AntonZenin <zenin.anton.2005@gmail.com>
2025-11-18 21:48:15 +0300

    trial fix

--- tasks/zenin_a_sum_values_by_columns_matrix/mpi/include/ops_mpi.hpp
+++ tasks/zenin_a_sum_values_by_columns_matrix/mpi/include/ops_mpi.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstddef>
-#include <tuple>
 #include <vector>
 
 #include "task/include/task.hpp"
@@ -22,8 +21,6 @@ class ZeninASumValuesByColumnsMatrixMPI : public BaseTask {
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
-  static void CalculateLocalSums(const std::vector<double> &matrix_data, size_t columns, size_t total_rows,
-                                 size_t start_column, size_t cols_this_process, std::vector<double> &local_sums);
   static void PrepareGathervParameters(int world_size, size_t base_cols_per_process, size_t remain,
                                        std::vector<int> &recv_counts, std::vector<int> &displacements);
 };
