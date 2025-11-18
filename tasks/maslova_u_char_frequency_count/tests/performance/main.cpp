41a127e865446bb7fc28b261a683e2f10b41c30f
ulianamaslova25 <uliana-maslova@mail.ru>
2025-11-18 23:57:24 +0300

    Add performance tests

--- tasks/maslova_u_char_frequency_count/tests/performance/main.cpp
+++ tasks/maslova_u_char_frequency_count/tests/performance/main.cpp
@@ -1,30 +1,51 @@
 #include <gtest/gtest.h>
 
+#include <string>
+#include <vector>
+
 #include "maslova_u_char_frequency_count/common/include/common.hpp"
 #include "maslova_u_char_frequency_count/mpi/include/ops_mpi.hpp"
 #include "maslova_u_char_frequency_count/seq/include/ops_seq.hpp"
 #include "util/include/perf_test_util.hpp"
+#include "util/include/util.hpp"
 
 namespace maslova_u_char_frequency_count {
 
-class MaslovaURunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
-  InType input_data_{};
+class MaslovaUPerfTests
+    : public ppc::util::BaseRunPerfTests<InType, OutType> {
+ private:
+  InType input_data_;
+  OutType expected_output_{};
 
+ protected:
   void SetUp() override {
-    input_data_ = kCount_;
-  }
+    const size_t str_size = 5000000;
+    const char input_char = 'y';
 
-  bool CheckTestOutputData(OutType &output_data) final {
-    return input_data_ == output_data;
+    std::string generated_string(str_size, 'a');
+    size_t expected_count = 0;
+
+    if constexpr (str_size > 100) {
+      for (size_t i = 0; i < str_size / 100; ++i) {
+        generated_string[i * 50] = input_char;
+        expected_count++;
+      }
+    }
+
+    input_data_ = {generated_string, input_char};
+    expected_output_ = expected_count;
   }
 
   InType GetTestInputData() final {
     return input_data_;
   }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return output_data == expected_output_;
+  }
 };
 
-TEST_P(MaslovaURunPerfTestProcesses, RunPerfModes) {
+TEST_P(MaslovaUPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +54,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = MaslovaURunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = MaslovaUPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, MaslovaURunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, MaslovaUPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace maslova_u_char_frequency_count
