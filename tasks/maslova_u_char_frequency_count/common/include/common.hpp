61be904ce6fd50d6d48df304a0a983fdbeb827ee
ulianamaslova25 <uliana-maslova@mail.ru>
2025-11-18 13:09:48 +0300

    Add InType and OutType

--- tasks/maslova_u_char_frequency_count/common/include/common.hpp
+++ tasks/maslova_u_char_frequency_count/common/include/common.hpp
@@ -2,14 +2,15 @@
 
 #include <string>
 #include <tuple>
+#include <utility>
 
 #include "task/include/task.hpp"
 
 namespace maslova_u_char_frequency_count {
 
-using InType = int;
-using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using InType = std::pair<std::string, char>;
+using OutType = size_t;
+using TestType = std::tuple<std::pair<std::string, char>, size_t>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace maslova_u_char_frequency_count
