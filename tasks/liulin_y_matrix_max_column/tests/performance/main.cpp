6c41653ed6f304824e5e35b8079590970f923ad7
naitora69 <aominedaikiii192@gmail.com>
2025-11-19 23:09:13 +0300

    initial_fix_tests

--- tasks/liulin_y_matrix_max_column/tests/performance/main.cpp
+++ tasks/liulin_y_matrix_max_column/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace liulin_y_matrix_max_column {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class LiulinYMatrixMaxColumnPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(LiulinYMatrixMaxColumnPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = LiulinYMatrixMaxColumnPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, LiulinYMatrixMaxColumnPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace liulin_y_matrix_max_column
