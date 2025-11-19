1e48841b25f8c16d9787e34c6f11acaabb4b999f
smallAbyss <eal1105@yandex.ru>
2025-11-19 12:58:41 +0000

    add func tests

--- tasks/luzan_e_matrix_rows_sum/tests/functional/main.cpp
+++ tasks/luzan_e_matrix_rows_sum/tests/functional/main.cpp
@@ -64,7 +64,8 @@ TEST_P(LuzanEMatrixRowsSumFuncTests, MatmulFromPic) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 3> kTestParam = {std::make_tuple(3, 3), std::make_tuple(2, 5), std::make_tuple(10, 70)};
+const std::array<TestType, 6> kTestParam = {std::make_tuple(3, 3), std::make_tuple(2, 5), std::make_tuple(10, 70), 
+                                            std::make_tuple(2000, 5), std::make_tuple(5, 2000), std::make_tuple(1, 1)}; 
 
 const auto kTestTasksList = std::tuple_cat(
     ppc::util::AddFuncTask<LuzanEMatrixRowsSumMPI, InType>(kTestParam, PPC_SETTINGS_luzan_e_matrix_rows_sum),
