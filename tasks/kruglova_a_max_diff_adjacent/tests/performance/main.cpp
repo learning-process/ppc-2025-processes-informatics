e110ab5359289b03289ee71d6a0d0c4eeec350d0
AlenaK13 <kruglovatm@mail.ru>
2025-11-20 14:57:25 +0300

    getting started passing functional tests done3

--- tasks/kruglova_a_max_diff_adjacent/tests/performance/main.cpp
+++ tasks/kruglova_a_max_diff_adjacent/tests/performance/main.cpp
@@ -11,7 +11,7 @@ namespace kruglova_a_max_diff_adjacent {
 
 class KruglovaAMaxDiffAdjacentPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
-  static constexpr int k_count = 2000000;
+  static constexpr int k_count = 4000000;
   InType input_data_;
   OutType expected_output_;
 
