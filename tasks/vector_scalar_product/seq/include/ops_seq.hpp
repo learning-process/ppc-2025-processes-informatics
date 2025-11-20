3efe44017f1c21dc293a385d2479a7cff86b603f
Mariya <MariyShelenkova@yandex.ru>
2025-11-20 19:21:04 +0300

    Shelenkova

---
+++ tasks/vector_scalar_product/seq/include/ops_seq.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "vector_scalar_product/common/include/common.hpp"
+
+namespace vector_scalar_product {
+
+class VectorScalarProductSeq : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kSEQ;
+  }
+
+  explicit VectorScalarProductSeq(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+  double partial_sum_ = 0.0;
+};
+
+}  // namespace vector_scalar_product
