576d434b74b9f7a2ddeb66bf377a895c39597786
Projectius_student <sprojectiustech@gmail.com>
2025-11-22 22:17:18 +0300

    types configuration

--- tasks/konstantinov_s_elem_vec_sign_change_count/common/include/common.hpp
+++ tasks/konstantinov_s_elem_vec_sign_change_count/common/include/common.hpp
@@ -2,12 +2,13 @@
 
 #include <string>
 #include <tuple>
+#include <vector>
 
 #include "task/include/task.hpp"
 
 namespace konstantinov_s_elem_vec_sign_change_count {
 
-using InType = int;
+using InType = std::vector<int>;
 using OutType = int;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
