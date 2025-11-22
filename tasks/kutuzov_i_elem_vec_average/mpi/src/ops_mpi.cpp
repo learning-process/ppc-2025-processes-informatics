9a39a06318a965bd76cc05d30d6a79ef3e61806d
K <Akarfire@gmail.com>
2025-11-22 21:10:33 +0300

    [DEV] MPI brought scatter back

--- tasks/kutuzov_i_elem_vec_average/mpi/src/ops_mpi.cpp
+++ tasks/kutuzov_i_elem_vec_average/mpi/src/ops_mpi.cpp
@@ -38,24 +38,35 @@ bool KutuzovIElemVecAverageMPI::RunImpl() {
   int num_processes = 0;
   MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
 
+  int total_elements_num = 0;
+  if (rank == 0) {
+    total_elements_num = static_cast<int>(GetInput().size());
+  }
+  MPI_Bcast(&total_elements_num, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
   int batch_size = 0;
-  batch_size = static_cast<int>(input.size()) / num_processes;
+  batch_size = total_elements_num / num_processes;
 
   if (batch_size > 0) {
+    double* local_buffer = new double[batch_size];
+    MPI_Scatter(GetInput().data(), batch_size, MPI_DOUBLE, local_buffer, batch_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
     double sum = 0.0;
     for (int i = 0; i < batch_size; i++) {
       sum += input[rank * batch_size + i];
     }
+
     MPI_Reduce(&sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    delete[] local_buffer;
   }
 
   if (rank == 0) {
-    if (num_processes * batch_size < static_cast<int>(input.size())) {
-      for (int i = num_processes * batch_size; i < static_cast<int>(input.size()); i++) {
+    if (num_processes * batch_size < total_elements_num) {
+      for (int i = num_processes * batch_size; i < total_elements_num; i++) {
         global_sum += input[i];
       }
     }
-    result = global_sum / static_cast<double>(input.size());
+    result = global_sum / static_cast<double>(total_elements_num);
   }
 
   MPI_Bcast(&result, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
