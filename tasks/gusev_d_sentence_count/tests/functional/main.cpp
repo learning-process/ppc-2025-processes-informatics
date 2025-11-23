5aa0cc40e4e6f4f3ac3fbee0e30669b51ca38076
userskins <userskins@gmail.com>
2025-11-24 00:47:23 +0300

    Правка работы тестов + реализация тестов

--- tasks/gusev_d_sentence_count/tests/functional/main.cpp
+++ tasks/gusev_d_sentence_count/tests/functional/main.cpp
@@ -1,16 +1,10 @@
 #include <gtest/gtest.h>
-#include <stb/stb_image.h>
 
 #include <algorithm>
 #include <array>
 #include <cstddef>
-#include <cstdint>
-#include <numeric>
-#include <stdexcept>
 #include <string>
 #include <tuple>
-#include <utility>
-#include <vector>
 
 #include "gusev_d_sentence_count/common/include/common.hpp"
 #include "gusev_d_sentence_count/mpi/include/ops_mpi.hpp"
@@ -22,37 +16,23 @@ namespace gusev_d_sentence_count {
 
 class GusevDSentenceCountFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
+  GusevDSentenceCountFuncTests() = default;
+
   static std::string PrintTestParam(const TestType &test_param) {
-    return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
+    std::string text = std::get<0>(test_param);
+    std::replace(text.begin(), text.end(), ' ', '_');
+    return text + "_Expected_" + std::to_string(std::get<1>(test_param));
   }
 
  protected:
   void SetUp() override {
-    int width = -1;
-    int height = -1;
-    int channels = -1;
-    std::vector<uint8_t> img;
-    // Read image in RGB to ensure consistent channel count
-    {
-      std::string abs_path = ppc::util::GetAbsoluteTaskPath("gusev_d_sentence_count", "pic.jpg");
-      auto *data = stbi_load(abs_path.c_str(), &width, &height, &channels, STBI_rgb);
-      if (data == nullptr) {
-        throw std::runtime_error("Failed to load image: " + std::string(stbi_failure_reason()));
-      }
-      channels = STBI_rgb;
-      img = std::vector<uint8_t>(data, data + (static_cast<ptrdiff_t>(width * height * channels)));
-      stbi_image_free(data);
-      if (width != height) {
-        throw std::runtime_error("width != height: ");
-      }
-    }
-
     TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    input_data_ = width - height + std::min(std::accumulate(img.begin(), img.end(), 0), channels);
+    input_data_ = std::get<0>(params);
+    expected_output_ = std::get<1>(params);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return (input_data_ == output_data);
+    return output_data == expected_output_;
   }
 
   InType GetTestInputData() final {
@@ -60,16 +40,38 @@ class GusevDSentenceCountFuncTests : public ppc::util::BaseRunFuncTests<InType, 
   }
 
  private:
-  InType input_data_ = 0;
+  InType input_data_;
+  OutType expected_output_ = 0;
 };
 
 namespace {
 
-TEST_P(GusevDSentenceCountFuncTests, MatmulFromPic) {
+TEST_P(GusevDSentenceCountFuncTests, SentenceBoundaryTests) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 3> kTestParam = {std::make_tuple(3, "3"), std::make_tuple(5, "5"), std::make_tuple(7, "7")};
+const std::array<TestType, 16> kTestParam = {
+    std::make_tuple(std::string(""), 0),                                     
+    std::make_tuple(std::string("No terminators here"), 0),                 
+    std::make_tuple(std::string("Sentence one. Sentence two! Sentence three?"), 3),
+    std::make_tuple(std::string("Is this a question?"), 1),                  
+
+    std::make_tuple(std::string("This is the end."), 1),                     
+    std::make_tuple(std::string("Wow!"), 1),                                
+    std::make_tuple(std::string("Wait, what?"), 1),                         
+
+    std::make_tuple(std::string("Wait... What?! Stop!"), 2),
+    std::make_tuple(std::string("Really?? No!"), 2),              
+
+    std::make_tuple(std::string("Test. Another one! Last?"), 3),
+    std::make_tuple(std::string("Symbols? @ # $ %"), 1),                     
+    std::make_tuple(std::string("!Start with terminator. End."), 2),          
+    
+    std::make_tuple(std::string("The end is near..."), 1),                    
+    std::make_tuple(std::string("A!B.C?"), 3),                          
+    std::make_tuple(std::string("Only terminators???!!!"), 1),               
+    std::make_tuple(std::string("A..B.C"), 2)                               
+};
 
 const auto kTestTasksList =
     std::tuple_cat(ppc::util::AddFuncTask<GusevDSentenceCountMPI, InType>(kTestParam, PPC_SETTINGS_gusev_d_sentence_count),
@@ -79,7 +81,7 @@ const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
 const auto kPerfTestName = GusevDSentenceCountFuncTests::PrintFuncTestName<GusevDSentenceCountFuncTests>;
 
-INSTANTIATE_TEST_SUITE_P(PicMatrixTests, GusevDSentenceCountFuncTests, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(SentenceCountBoundaryTests, GusevDSentenceCountFuncTests, kGtestValues, kPerfTestName);
 
 }  // namespace
 
--- tasks/gusev_d_sentence_count/tests/performance/main.cpp
+++ tasks/gusev_d_sentence_count/tests/performance/main.cpp
@@ -1,30 +1,68 @@
 #include <gtest/gtest.h>
 
+#include <cstddef>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
 #include "gusev_d_sentence_count/common/include/common.hpp"
 #include "gusev_d_sentence_count/mpi/include/ops_mpi.hpp"
 #include "gusev_d_sentence_count/seq/include/ops_seq.hpp"
 #include "util/include/perf_test_util.hpp"
+#include "util/include/util.hpp"
 
 namespace gusev_d_sentence_count {
 
-class GusevDSentenceCountPerfTest : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
-  InType input_data_{};
+static bool IsTerminator(char c) {
+  return c == '.' || c == '!' || c == '?';
+}
+
+class GusevDSentenceCountPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
+ private:
+  InType input_data_;
+  OutType expected_output_ = 0;
 
   void SetUp() override {
-    input_data_ = kCount_;
+    std::string abs_path = ppc::util::GetAbsoluteTaskPath("gusev_d_sentence_count", "holy-bible.txt");
+
+    std::ifstream file(abs_path);
+    if (!file.is_open()) {
+      throw std::runtime_error("File reading error");
+    }
+
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    input_data_ = buffer.str();
+    file.close();
+
+    size_t count = 0;
+    size_t len = input_data_.length();
+
+    for (size_t i = 0; i < len; ++i) {
+      if (IsTerminator(input_data_[i])) {
+        bool is_next_not_term = (i + 1 == len) || !IsTerminator(input_data_[i + 1]);
+        if (is_next_not_term) {
+          count++;
+        }
+      }
+    }
+    expected_output_ = count;
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return input_data_ == output_data;
+    return output_data == expected_output_;
   }
 
   InType GetTestInputData() final {
     return input_data_;
   }
+
+ public:
+  GusevDSentenceCountPerfTests() = default;
 };
 
-TEST_P(GusevDSentenceCountPerfTest, RunPerfModes) {
+TEST_P(GusevDSentenceCountPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +71,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = GusevDSentenceCountPerfTest::CustomPerfTestName;
+const auto kPerfTestName = GusevDSentenceCountPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, GusevDSentenceCountPerfTest, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, GusevDSentenceCountPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace gusev_d_sentence_count
