b732b41ac3b66a396d5e7dc1cdaaec54f50098d2
Uzema <uzema_2k16@mail.ru>
2025-11-24 21:25:35 +0300

    Changed the InType, OutType, etc

--- tasks/nikolaev_d_most_dif_vec_neighbors/common/include/common.hpp
+++ tasks/nikolaev_d_most_dif_vec_neighbors/common/include/common.hpp
@@ -7,9 +7,9 @@
 
 namespace nikolaev_d_most_dif_vec_neighbors {
 
-using InType = int;
-using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using InType = std::vector<int>;
+using OutType = std::pair<int, int>;
+using TestType = std::tuple<std::pair<int, int>, std::vector<int>>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace nikolaev_d_most_dif_vec_neighbors
