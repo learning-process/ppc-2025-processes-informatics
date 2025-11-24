7b058fa2748b910249544821de8931707d4c0085
Uzema <uzema_2k16@mail.ru>
2025-11-24 21:27:57 +0300

    Added func and perf tests

--- tasks/nikolaev_d_most_dif_vec_neighbors/tests/functional/main.cpp
+++ tasks/nikolaev_d_most_dif_vec_neighbors/tests/functional/main.cpp
@@ -23,35 +23,27 @@ namespace nikolaev_d_most_dif_vec_neighbors {
 class NikolaevDMostDifVecNeighborsFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
-    return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
+    auto expected_ans = std::get<0>(test_param);
+    int first = expected_ans.first;
+    int second = expected_ans.second;
+
+    std::string str1 = (first >= 0) ? std::to_string(first) : "neg" + std::to_string(-first);
+    std::string str2 = (second >= 0) ? std::to_string(second) : "neg" + std::to_string(-second);
+    //return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
+    return "expected_ans_" + str1 + "_" + str2;
   }
 
  protected:
   void SetUp() override {
-    int width = -1;
-    int height = -1;
-    int channels = -1;
-    std::vector<uint8_t> img;
-    // Read image
-    {
-      std::string abs_path = ppc::util::GetAbsoluteTaskPath(PPC_ID_nikolaev_d_most_dif_vec_neighbors, "pic.jpg");
-      auto *data = stbi_load(abs_path.c_str(), &width, &height, &channels, 0);
-      if (data == nullptr) {
-        throw std::runtime_error("Failed to load image: " + std::string(stbi_failure_reason()));
-      }
-      img = std::vector<uint8_t>(data, data + (static_cast<ptrdiff_t>(width * height * channels)));
-      stbi_image_free(data);
-      if (std::cmp_not_equal(width, height)) {
-        throw std::runtime_error("width != height: ");
-      }
-    }
-
+    //std::string abs_path = ppc::util::GetAbsoluteTaskPath(PPC_ID_nikolaev_d_most_dif_vec_neighbors, "pic.jpg");
     TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    input_data_ = width - height + std::min(std::accumulate(img.begin(), img.end(), 0), channels);
+    
+    input_data_ = std::get<1>(params);
+    expected_output_ = std::get<0>(params);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return (input_data_ == output_data);
+    return (expected_output_ == output_data);
   }
 
   InType GetTestInputData() final {
@@ -59,16 +51,23 @@ class NikolaevDMostDifVecNeighborsFuncTests : public ppc::util::BaseRunFuncTests
   }
 
  private:
-  InType input_data_ = 0;
+  InType input_data_;
+  OutType expected_output_;
 };
 
 namespace {
 
-TEST_P(NikolaevDMostDifVecNeighborsFuncTests, MatmulFromPic) {
+TEST_P(NikolaevDMostDifVecNeighborsFuncTests, FuncTestsRun) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 3> kTestParam = {std::make_tuple(3, "3"), std::make_tuple(5, "5"), std::make_tuple(7, "7")};
+const std::array<TestType, 6> kTestParam = {
+    std::make_tuple(std::make_pair(1, 3), std::vector<int>{1, 3}),
+    std::make_tuple(std::make_pair(3, 15), std::vector<int>{1, 3, 15}),
+    std::make_tuple(std::make_pair(1, 32), std::vector<int>{2, 1, 1, 32, 12, 16, 5, -2, -1, -3}),
+    std::make_tuple(std::make_pair(-1, 5), std::vector<int>{1, 3, 0, -1, 5, 2}),
+    std::make_tuple(std::make_pair(1, 50), std::vector<int>{1, 1, 1, 1, 1, 50, 30, 1, 1}),
+    std::make_tuple(std::make_pair(-1, -6), std::vector<int>{1, 2, -1, -6, -2, 2})};
 
 const auto kTestTasksList =
     std::tuple_cat(ppc::util::AddFuncTask<NikolaevDMostDifVecNeighborsMPI, InType>(kTestParam, PPC_SETTINGS_nikolaev_d_most_dif_vec_neighbors),
@@ -78,7 +77,9 @@ const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
 const auto kPerfTestName = NikolaevDMostDifVecNeighborsFuncTests::PrintFuncTestName<NikolaevDMostDifVecNeighborsFuncTests>;
 
-INSTANTIATE_TEST_SUITE_P(PicMatrixTests, NikolaevDMostDifVecNeighborsFuncTests, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(MostDifVecNeighbors, NikolaevDMostDifVecNeighborsFuncTests, kGtestValues, kPerfTestName);
+
+
 
 }  // namespace
 
--- tasks/nikolaev_d_most_dif_vec_neighbors/tests/performance/main.cpp
+++ tasks/nikolaev_d_most_dif_vec_neighbors/tests/performance/main.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+  #include <random>
+
 #include "nikolaev_d_most_dif_vec_neighbors/common/include/common.hpp"
 #include "nikolaev_d_most_dif_vec_neighbors/mpi/include/ops_mpi.hpp"
 #include "nikolaev_d_most_dif_vec_neighbors/seq/include/ops_seq.hpp"
@@ -8,15 +10,36 @@
 namespace nikolaev_d_most_dif_vec_neighbors {
 
 class NikolaevDMostDifVecNeighborsPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
-  InType input_data_{};
+  const size_t kCount_ = 10'000'000;
+  InType input_data_;
+  OutType expected_output_;
 
   void SetUp() override {
-    input_data_ = kCount_;
+    input_data_.resize(kCount_);
+
+    //std::random_device dev;
+    std::mt19937 rng(42); //dev() 42
+    std::uniform_int_distribution<int> dist(-10'000, 10'000);
+
+    for (size_t i = 0; i < kCount_; ++i) {
+      input_data_[i] = dist(rng);
+    }
+
+    std::uniform_int_distribution<size_t> pos_dist(0, kCount_ - 2);
+    size_t max_dif_pos = pos_dist(rng);
+
+    input_data_[max_dif_pos] = -15'000;
+    input_data_[max_dif_pos + 1] = 15'000;
+    expected_output_ = std::make_pair(-15'000, 15'000);
+
+    // for (int i=0; i < 10; i++) {
+    //   std::cout << "input_data_[" << i << "] = " << input_data_[i] << "\n";
+    // }
+    // std::cout << "max_dif_pos=" << max_dif_pos << "\n";
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return input_data_ == output_data;
+    return (expected_output_ == output_data);
   }
 
   InType GetTestInputData() final {
