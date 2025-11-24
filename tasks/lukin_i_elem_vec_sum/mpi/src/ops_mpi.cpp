0fc7c5d9dc9ba07da308a69f2dc883dbb0f2ae4f
elus10n <dizbvalkyre@gmail.com>
2025-11-24 18:51:10 +0300

    back to scatterv realization + GetInput() is now available only on root

--- tasks/lukin_i_elem_vec_sum/mpi/src/ops_mpi.cpp
+++ tasks/lukin_i_elem_vec_sum/mpi/src/ops_mpi.cpp
@@ -11,16 +11,33 @@ namespace lukin_i_elem_vec_sum {
 
 LukinIElemVecSumMPI::LukinIElemVecSumMPI(const InType &in) {
   SetTypeOfTask(GetStaticTypeOfTask());
-  GetInput() = in;
+
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0) {
+    GetInput() = in;
+  }
+
   GetOutput() = 0;
 }
 
 bool LukinIElemVecSumMPI::ValidationImpl() {
-  return (static_cast<int>(GetInput().size()) != 0);
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if (rank == 0) {
+    return (static_cast<int>(GetInput().size()) != 0);
+  }
+  return true;
 }
 
 bool LukinIElemVecSumMPI::PreProcessingImpl() {
-  vec_size_ = static_cast<int>(GetInput().size());
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if (rank == 0) {
+    vec_size_ = static_cast<int>(GetInput().size());
+  }
   return true;
 }
 
@@ -29,6 +46,8 @@ bool LukinIElemVecSumMPI::RunImpl() {
   int rank = 0;
   MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  MPI_Bcast(&vec_size_, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   if (proc_count > vec_size_) {
     if (rank == 0) {
@@ -42,13 +61,26 @@ bool LukinIElemVecSumMPI::RunImpl() {
     return true;
   }
 
+  std::vector<int> sendcounts(proc_count);
+  std::vector<int> offsets(proc_count);
+
   const int part = vec_size_ / proc_count;
   const int reminder = vec_size_ % proc_count;
 
-  int start = (part * rank) + ((rank < reminder) ? rank : reminder);
-  int end = start + part + ((rank < reminder) ? 1 : 0);
+  int offset = 0;
+  for (int i = 0; i < proc_count; i++) {
+    sendcounts[i] = part + (i < reminder ? 1 : 0);
+    offsets[i] = offset;
+    offset += sendcounts[i];
+  }
 
-  OutType local_sum = std::accumulate(GetInput().begin() + start, GetInput().begin() + end, 0LL);
+  int local_size = sendcounts[rank];
+  std::vector<int> local_vec(local_size);
+
+  MPI_Scatterv(rank == 0 ? GetInput().data() : nullptr, sendcounts.data(), offsets.data(), MPI_INT, local_vec.data(),
+               local_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+  OutType local_sum = std::accumulate(local_vec.begin(), local_vec.end(), 0LL);
 
   OutType global_sum = 0LL;
 
