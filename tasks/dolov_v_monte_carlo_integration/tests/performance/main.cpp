8cd51338c4cd6b21bd0479631bfe577354097191
slaver07 <slavutich05@gmail.com>
2025-11-24 23:19:29 +0000

    refactor(tests): Fix tests name

--- tasks/dolov_v_monte_carlo_integration/tests/performance/main.cpp
+++ tasks/dolov_v_monte_carlo_integration/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace dolov_v_monte_carlo_integration {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class DolovVMonteCarloIntegrationPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(DolovVMonteCarloIntegrationPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = DolovVMonteCarloIntegrationPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, DolovVMonteCarloIntegrationPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace dolov_v_monte_carlo_integration
