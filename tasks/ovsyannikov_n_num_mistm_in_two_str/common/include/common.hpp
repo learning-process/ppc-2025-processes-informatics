4e38b022b376197ed25e76bc4803325444687c80
unknown <nik.ovsyannikov2090@gmail.com>
2025-11-26 23:11:33 +0300

    some change

--- tasks/ovsyannikov_n_num_mistm_in_two_str/common/include/common.hpp
+++ tasks/ovsyannikov_n_num_mistm_in_two_str/common/include/common.hpp
@@ -9,4 +9,5 @@ namespace ovsyannikov_n_num_mistm_in_two_str {
 using InType = std::pair<std::string, std::string>;  // Входные данные: пара строк
 using OutType = int;                                 // Выходные данные: количество несовпадений
 using BaseTask = ppc::task::Task<InType, OutType>;
+using TestType = std::tuple<InType, OutType, std::string>;
 }  // namespace ovsyannikov_n_num_mistm_in_two_str
