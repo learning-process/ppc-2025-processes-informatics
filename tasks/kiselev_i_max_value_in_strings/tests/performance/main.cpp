548d47e14401d3758acd7ea84ba34f6fd7a805ee
Korobokkk <bbb754748@gmail.com>
2025-11-27 19:12:00 +0300

    alpfa v. 1.0

--- tasks/kiselev_i_max_value_in_strings/tests/performance/main.cpp
+++ tasks/kiselev_i_max_value_in_strings/tests/performance/main.cpp
@@ -10,16 +10,17 @@
 namespace kiselev_i_max_value_in_strings {
 
 class KiselevIMaxValueInStringsRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kMatrixSize_ = 1000;  // Большая матрица для тестов производительности
-
+  const int k_count = 10000; // количество строк
+  const int k_max_cols = 10000; // макс длина строки
  protected:
   void SetUp() override {
-    // Создаем большую матрицу для performance тестов
-    input_data_.resize(kMatrixSize_);
-    for (int i = 0; i < kMatrixSize_; ++i) {
-      input_data_[i].resize(kMatrixSize_);
-      for (int j = 0; j < kMatrixSize_; ++j) {
-        input_data_[i][j] = (i + j) % 100;  // Заполняем значениями
+    // big matrix
+    input_data_.resize(k_count);
+    for (int i = 0; i < k_count; ++i) {
+      int len_row = 1+ (int)(i%k_max_cols);
+      input_data_[i].resize(len_row);
+      for (int j = 0; j < len_row; ++j) {
+        input_data_[i][j] = (i+2 + j+7) % 100; 
       }
     }
   }
@@ -50,48 +51,4 @@ const auto kPerfTestName = KiselevIMaxValueInStringsRunPerfTestProcesses::Custom
 
 INSTANTIATE_TEST_SUITE_P(RunModeTests, KiselevIMaxValueInStringsRunPerfTestProcesses, kGtestValues, kPerfTestName);
 
-}  // namespace kiselev_i_max_value_in_strings
-
-/*
-#include <gtest/gtest.h>
-
-#include "kiselev_i_max_value_in_strings/common/include/common.hpp"
-#include "kiselev_i_max_value_in_strings/mpi/include/ops_mpi.hpp"
-#include "kiselev_i_max_value_in_strings/seq/include/ops_seq.hpp"
-#include "util/include/perf_test_util.hpp"
-
-namespace kiselev_i_max_value_in_strings {
-
-class KiselevIMaxValueInStringsRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
-  InType input_data_{};
-
-  void SetUp() override {
-    // input_data_ = kCount_;
-  }
-
-  bool CheckTestOutputData(OutType &output_data) final {
-    // return input_data_ == output_data;
-    return true;
-  }
-
-  InType GetTestInputData() final {
-    return input_data_;
-  }
-};
-
-TEST_P(KiselevIMaxValueInStringsRunPerfTestProcesses, RunPerfModes) {
-  ExecuteTest(GetParam());
-}
-
-const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, KiselevITestTaskMPI, KiselevITestTaskSEQ>(PPC_SETTINGS_kiselev_i_max_value_in_strings);
-
-const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
-
-const auto kPerfTestName = KiselevIMaxValueInStringsRunPerfTestProcesses::CustomPerfTestName;
-
-INSTANTIATE_TEST_SUITE_P(RunModeTests, KiselevIMaxValueInStringsRunPerfTestProcesses, kGtestValues, kPerfTestName);
-
-}  // namespace kiselev_i_max_value_in_strings
-*/
+} 
