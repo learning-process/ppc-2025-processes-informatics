5c85ea11b26c1b055c0db345b3b385650de36122
DimaVotincev <dvotinccev@gmail.com>
2025-11-30 21:05:21 +0300

    ok

--- tasks/votincev_d_matrix_mult/tests/functional/main.cpp
+++ tasks/votincev_d_matrix_mult/tests/functional/main.cpp
@@ -87,7 +87,6 @@ TEST_P(VotincevDMatrixMultRunFuncTestsProcesses, MatrixMultiplicationTests) {
 const std::array<TestType, 10> kTestParam = {"test1", "test2", "test3", "test4", "test5",
                                              "test6", "test7", "test8", "test9", "test10"};
 
-
 const auto kTestTasksList = std::tuple_cat(
     ppc::util::AddFuncTask<VotincevDMatrixMultMPI, InType>(kTestParam, PPC_SETTINGS_votincev_d_matrix_mult),
     ppc::util::AddFuncTask<VotincevDMatrixMultSEQ, InType>(kTestParam, PPC_SETTINGS_votincev_d_matrix_mult));
