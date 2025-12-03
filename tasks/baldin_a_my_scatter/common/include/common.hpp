d8121463ba64d7149988b512f1ed280f60c42cae
root <abaldin05@mail.ru>
2025-12-03 16:40:39 +0000

    Define types

--- tasks/baldin_a_my_scatter/common/include/common.hpp
+++ tasks/baldin_a_my_scatter/common/include/common.hpp
@@ -2,14 +2,16 @@
 
 #include <string>
 #include <tuple>
+#include <vector>
+#include <mpi.h>
 
 #include "task/include/task.hpp"
 
 namespace baldin_a_my_scatter {
 
-using InType = int;
-using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using InType = std::tuple<const void*, int, MPI_Datatype, void *, int, MPI_Datatype, int, MPI_Comm>;
+using OutType = void *;
+using TestType = InType;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace baldin_a_my_scatter
