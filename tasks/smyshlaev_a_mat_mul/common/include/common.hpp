1bdb63437df2b8ac24dc373ee8ff59f986d34278
P_Alex_Tr <aleksandr.smyshlaev@yandex.ru>
2025-12-04 22:22:56 +0300

    set types

--- tasks/smyshlaev_a_mat_mul/common/include/common.hpp
+++ tasks/smyshlaev_a_mat_mul/common/include/common.hpp
@@ -7,9 +7,9 @@
 
 namespace smyshlaev_a_mat_mul {
 
-using InType = int;
-using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using InType = std::tuple<int, std::vector<double>, int, std::vector<double>>;
+using OutType = std::vector<double>;
+using TestType = std::tuple<int, std::vector<double>, int, std::vector<double>, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace smyshlaev_a_mat_mul
