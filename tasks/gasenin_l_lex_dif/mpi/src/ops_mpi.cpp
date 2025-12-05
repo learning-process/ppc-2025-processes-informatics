8cfbacad15eab80a9388f3fab8c7f0c9a1f66996
Leontin16 <gasenidze@mail.ru>
2025-12-05 09:35:29 +0300

    mpi fix

--- tasks/gasenin_l_lex_dif/mpi/src/ops_mpi.cpp
+++ tasks/gasenin_l_lex_dif/mpi/src/ops_mpi.cpp
@@ -27,6 +27,28 @@ LocalDiff FindLocalDifference(const std::vector<char> &s1_chunk, const std::vect
   return {.diff_pos = std::string::npos, .result = 0};
 }
 
+void CalculateDistribution(size_t min_len, int size, std::vector<int> &sendcounts, std::vector<int> &displs) {
+  int remainder = static_cast<int>(min_len % size);
+  int chunk_size = static_cast<int>(min_len / size);
+  int prefix_sum = 0;
+
+  for (int i = 0; i < size; ++i) {
+    sendcounts[i] = chunk_size + (i < remainder ? 1 : 0);
+    displs[i] = prefix_sum;
+    prefix_sum += sendcounts[i];
+  }
+}
+
+int GetFinalResultByLength(size_t len1, size_t len2) {
+  if (len1 < len2) {
+    return -1;
+  }
+  if (len1 > len2) {
+    return 1;
+  }
+  return 0;
+}
+
 }  // namespace
 
 namespace gasenin_l_lex_dif {
@@ -62,16 +84,16 @@ bool GaseninLLexDifMPI::RunImpl() {
   const std::string &str1 = input_data.first;
   const std::string &str2 = input_data.second;
 
-  std::vector<uint64_t> lengths(2, 0);
+  std::vector<unsigned long long> lengths(2, 0);
   if (rank == 0) {
     lengths[0] = str1.length();
     lengths[1] = str2.length();
   }
-  MPI_Bcast(lengths.data(), 2, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(lengths.data(), 2, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
 
-  uint64_t len1 = lengths[0];
-  uint64_t len2 = lengths[1];
-  uint64_t min_len = std::min(len1, len2);
+  auto len1 = static_cast<size_t>(lengths[0]);
+  auto len2 = static_cast<size_t>(lengths[1]);
+  size_t min_len = std::min(len1, len2);
 
   if (min_len == 0 && len1 == len2) {
     GetOutput() = 0;
@@ -80,19 +102,10 @@ bool GaseninLLexDifMPI::RunImpl() {
 
   std::vector<int> sendcounts(size);
   std::vector<int> displs(size);
-
-  int remainder = static_cast<int>(min_len % size);
-  int chunk_size = static_cast<int>(min_len / size);
-  int prefix_sum = 0;
-
-  for (int i = 0; i < size; ++i) {
-    sendcounts[i] = chunk_size + (i < remainder ? 1 : 0);
-    displs[i] = prefix_sum;
-    prefix_sum += sendcounts[i];
-  }
+  CalculateDistribution(min_len, size, sendcounts, displs);
 
   int local_count = sendcounts[rank];
-  size_t global_start = static_cast<size_t>(displs[rank]);
+  auto global_start = static_cast<size_t>(displs[rank]);
 
   std::vector<char> local_str1(local_count);
   std::vector<char> local_str2(local_count);
@@ -109,31 +122,24 @@ bool GaseninLLexDifMPI::RunImpl() {
   LocalDiff local_diff = FindLocalDifference(local_str1, local_str2, global_start);
 
   size_t local_diff_pos = (local_diff.diff_pos == std::string::npos) ? min_len : local_diff.diff_pos;
-  int local_result = local_diff.result;
 
-  uint64_t local_pos_64 = local_diff_pos;
-  uint64_t global_min_pos_64 = min_len;
+  unsigned long long local_pos_64 = local_diff_pos;
+  unsigned long long global_min_pos_64 = min_len;
 
-  MPI_Allreduce(&local_pos_64, &global_min_pos_64, 1, MPI_UINT64_T, MPI_MIN, MPI_COMM_WORLD);
+  MPI_Allreduce(&local_pos_64, &global_min_pos_64, 1, MPI_UNSIGNED_LONG_LONG, MPI_MIN, MPI_COMM_WORLD);
 
-  size_t global_min_pos = static_cast<size_t>(global_min_pos_64);
+  auto global_min_pos = static_cast<size_t>(global_min_pos_64);
 
   int result_for_sum = 0;
   if (local_diff_pos == global_min_pos && local_diff_pos < min_len) {
-    result_for_sum = local_result;
+    result_for_sum = local_diff.result;
   }
 
   int final_result = 0;
   MPI_Allreduce(&result_for_sum, &final_result, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
   if (global_min_pos == min_len) {
-    if (len1 < len2) {
-      final_result = -1;
-    } else if (len1 > len2) {
-      final_result = 1;
-    } else {
-      final_result = 0;
-    }
+    final_result = GetFinalResultByLength(len1, len2);
   }
 
   GetOutput() = final_result;
