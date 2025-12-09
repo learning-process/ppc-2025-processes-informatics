1a2b681860af35717714bf284622c0283c7e4015
root <abaldin05@mail.ru>
2025-12-09 20:44:37 +0000

    Define new TestType

--- tasks/baldin_a_my_scatter/common/include/common.hpp
+++ tasks/baldin_a_my_scatter/common/include/common.hpp
@@ -11,7 +11,7 @@ namespace baldin_a_my_scatter {
 
 using InType = std::tuple<const void*, int, MPI_Datatype, void *, int, MPI_Datatype, int, MPI_Comm>;
 using OutType = void *;
-using TestType = InType;
+using TestType = std::tuple<int, int, MPI_Datatype>; // count, root, type
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace baldin_a_my_scatter
