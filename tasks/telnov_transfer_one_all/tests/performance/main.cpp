5667f046525391f35125257f4a7322bd1839755c
mollycooddle <toolya.telnoff@yandex.ru>
2025-12-09 18:48:57 +0300

    clang-tidy

--- tasks/telnov_transfer_one_all/tests/performance/main.cpp
+++ tasks/telnov_transfer_one_all/tests/performance/main.cpp
@@ -28,18 +28,12 @@ TEST_P(TelnovTransferOneAllPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
-const auto kAllPerfTasks = std::make_tuple(
-    std::make_tuple(
-        ppc::task::TaskGetter<TelnovTransferOneAllMPI<int>, InType>,
-        "MPI",
-        ppc::performance::PerfResults::TypeOfRunning::kMPI
-    ),
-    std::make_tuple(
-        ppc::task::TaskGetter<TelnovTransferOneAllSEQ<int>, InType>,
-        "SEQ",
-        ppc::performance::PerfResults::TypeOfRunning::kSEQ
-    )
-);
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks(
+  std::make_tuple(
+    std::make_tuple(ppc::task::TaskGetter<TelnovTransferOneAllMPI<int>, InType>, "MPI"),
+      std::make_tuple(ppc::task::TaskGetter<TelnovTransferOneAllSEQ<int>, InType>, "SEQ")
+  ),
+  PPC_SETTINGS_telnov_transfer_one_all);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
