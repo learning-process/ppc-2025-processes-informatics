92327aca830a013a59383c11d2ff9bb8dd7fb249
mollycooddle <toolya.telnoff@yandex.ru>
2025-12-10 06:42:00 +0300

    codecov_ops_mpi/seq

--- tasks/telnov_transfer_one_all/mpi/include/ops_mpi.hpp
+++ tasks/telnov_transfer_one_all/mpi/include/ops_mpi.hpp
@@ -11,7 +11,7 @@ class TelnovTransferOneAllMPI : public BaseTask<T> {
  public:
   using InType = std::vector<T>;
   using OutType = InType;
-  static ppc::task::TypeOfTask GetStaticTypeOfTask() {
+  ppc::task::TypeOfTask GetStaticTypeOfTask() const {
     return ppc::task::TypeOfTask::kMPI;
   }
   explicit TelnovTransferOneAllMPI(const InType &in);
