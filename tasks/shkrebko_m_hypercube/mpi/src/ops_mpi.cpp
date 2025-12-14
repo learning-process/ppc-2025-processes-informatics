8309ea9ecbd0ca91035fe1c83d2de5e478419d4e
Shkrebko-Misha <michael.shkrebko@gmail.com>
2025-12-14 06:33:30 +0300

    fix again clear mpi

--- tasks/shkrebko_m_hypercube/mpi/src/ops_mpi.cpp
+++ tasks/shkrebko_m_hypercube/mpi/src/ops_mpi.cpp
@@ -109,12 +109,14 @@ bool ShkrebkoMHypercubeMPI::RunImpl() {
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  for (int i = 0; i < 1000; i++) {
+
+  for (int i = 0; i < 100000; i++) {
     volatile int dummy = 0;
-    for (int j = 0; j < 1000; j++) {
+    for (int j = 0; j < 100000; j++) {
       dummy += i * j;
     }
   }
+  
 
   HypercubeData local_data;
 
