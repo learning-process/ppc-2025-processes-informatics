f752e9902b3dda75c14204f98499c9f809b5a62b
P_Alex_Tr <aleksandr.smyshlaev@yandex.ru>
2025-12-14 14:57:10 +0300

    clang format

--- tasks/smyshlaev_a_gauss_filt/tests/functional/main.cpp
+++ tasks/smyshlaev_a_gauss_filt/tests/functional/main.cpp
@@ -4,10 +4,10 @@
 #include <array>
 #include <cstddef>
 #include <cstdint>
+#include <memory>
 #include <string>
 #include <tuple>
 #include <vector>
-#include <memory>
 
 #include "smyshlaev_a_gauss_filt/common/include/common.hpp"
 #include "smyshlaev_a_gauss_filt/mpi/include/ops_mpi.hpp"
@@ -18,7 +18,6 @@ namespace smyshlaev_a_gauss_filt {
 
 class SmyshlaevAGaussFiltRunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
-  // Имя теста теперь находится под индексом 4
   static std::string PrintTestParam(const TestType &test_param) {
     return std::get<4>(test_param);
   }
@@ -26,8 +25,7 @@ class SmyshlaevAGaussFiltRunFuncTestsProcesses : public ppc::util::BaseRunFuncTe
  protected:
   void SetUp() override {
     TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    
-    // Собираем InType (ImageType) из компонентов кортежа
+
     input_data_.width = std::get<0>(params);
     input_data_.height = std::get<1>(params);
     input_data_.channels = std::get<2>(params);
@@ -69,7 +67,6 @@ TEST_P(SmyshlaevAGaussFiltRunFuncTestsProcesses, GaussFiltTest) {
   ExecuteTest(GetParam());
 }
 
-// Вспомогательная функция теперь возвращает кортеж (TestType)
 TestType CreateRandomTest(int w, int h, int c, const std::string &name) {
   std::vector<uint8_t> data(static_cast<size_t>(w) * h * c);
   for (size_t i = 0; i < data.size(); ++i) {
@@ -78,16 +75,14 @@ TestType CreateRandomTest(int w, int h, int c, const std::string &name) {
   return std::make_tuple(w, h, c, data, name);
 }
 
-const std::array<TestType, 8> kTestParam = {
-    CreateRandomTest(3, 3, 3, "3x3_RGB"),
-    CreateRandomTest(32, 32, 3, "32x32_RGB"),
-    CreateRandomTest(60, 20, 3, "60x20_RGB"),
-    CreateRandomTest(20, 60, 3, "20x60_RGB"),
-    CreateRandomTest(23, 31, 3, "23x31_RGB_PrimeDims"),
-    CreateRandomTest(40, 40, 1, "40x40_Grayscale"),
-    CreateRandomTest(24, 24, 4, "24x24_RGBA"),
-    CreateRandomTest(2, 2, 3, "2x2_ForceSequential")
-};
+const std::array<TestType, 8> kTestParam = {CreateRandomTest(3, 3, 3, "3x3_RGB"),
+                                            CreateRandomTest(32, 32, 3, "32x32_RGB"),
+                                            CreateRandomTest(60, 20, 3, "60x20_RGB"),
+                                            CreateRandomTest(20, 60, 3, "20x60_RGB"),
+                                            CreateRandomTest(23, 31, 3, "23x31_RGB_PrimeDims"),
+                                            CreateRandomTest(40, 40, 1, "40x40_Grayscale"),
+                                            CreateRandomTest(24, 24, 4, "24x24_RGBA"),
+                                            CreateRandomTest(2, 2, 3, "2x2_ForceSequential")};
 
 const auto kTestTasksList = std::tuple_cat(
     ppc::util::AddFuncTask<SmyshlaevAGaussFiltMPI, InType>(kTestParam, PPC_SETTINGS_smyshlaev_a_gauss_filt),
