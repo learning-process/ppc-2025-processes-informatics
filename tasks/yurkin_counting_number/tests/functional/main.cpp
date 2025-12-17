9e8c39a24707b76fe9cac188532971ff641adb2c
georghinho <yurkin23egor@yandex.ru>
2025-12-18 01:14:25 +0300

    update5

--- tasks/yurkin_counting_number/tests/functional/main.cpp
+++ tasks/yurkin_counting_number/tests/functional/main.cpp
@@ -1,54 +1,55 @@
 #include <gtest/gtest.h>
 
-#include <string>
-#include <tuple>
-#include <vector>
-
 #include "util/include/func_test_util.hpp"
-#include "util/include/util.hpp"
 #include "yurkin_counting_number/common/include/common.hpp"
 #include "yurkin_counting_number/mpi/include/ops_mpi.hpp"
 #include "yurkin_counting_number/seq/include/ops_seq.hpp"
 
 namespace yurkin_counting_number {
 
-class YurkinCountingNumberFuncTest : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
- protected:
-  void SetUp() override {
-    // Берём параметр теста
-    const auto &param = GetParam();
-    TestType test = std::get<2>(param);
+class YurkinCountingNumberFuncTest : public ppc::util::BaseFunctionalTest<InType, OutType> {
+ public:
+  InType input_data_{};
 
-    // В SetUp подготавливаем строку, буквы и цифры
-    const std::string &s = std::get<1>(test);
-    input_data_.assign(s.begin(), s.end());
+  void SetUp() override {
+    // формируем тестовую строку
+    GlobalData::g_data_string = "Abc123!!!deF#45";
+
+    // input_data_ не влияет, но должен существовать
+    input_data_ = 0;
   }
 
-  // Всё остальное не трогаем
- private:
-  InType input_data_{};
+  InType GetTestInputData() override {
+    return input_data_;
+  }
+
+  bool CheckTestOutputData(OutType &output_data) override {
+    int cnt = 0;
+
+    for (unsigned char c : GlobalData::g_data_string) {
+      if (std::isalpha(c)) {
+        cnt++;
+      }
+    }
+
+    return output_data == cnt;
+  }
 };
 
-namespace {
-
-TEST_P(YurkinCountingNumberFuncTest, CountLetters) {
-  ExecuteTest(GetParam());
+TEST_P(YurkinCountingNumberFuncTest, FunctionalRun) {
+  Run();
 }
 
-// Примеры тестовых строк: буквы + цифры
-const std::array<TestType, 3> kTestParam = {std::make_tuple(3, "abc123"), std::make_tuple(5, "A1B2C3D4E"),
-                                            std::make_tuple(7, "xyzXYZ1234567")};
+const auto kAllTasks = ppc::util::MakeAllTasks<InType, YurkinCountingNumberMPI, YurkinCountingNumberSEQ>(
+    PPC_SETTINGS_yurkin_counting_number);
 
-const auto kTestTasksList = std::tuple_cat(
-    ppc::util::AddFuncTask<YurkinCountingNumberMPI, InType>(kTestParam, PPC_SETTINGS_yurkin_counting_number),
-    ppc::util::AddFuncTask<YurkinCountingNumberSEQ, InType>(kTestParam, PPC_SETTINGS_yurkin_counting_number));
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllTasks);
 
-const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
+const char kFuncTestName[] = "FuncTest";
 
-const char *kPerfTestName = "YurkinCountLetters";
-
-INSTANTIATE_TEST_SUITE_P(CountLettersTests, YurkinCountingNumberFuncTest, kGtestValues, kPerfTestName);
-
-}  // namespace
+INSTANTIATE_TEST_SUITE_P(FunctionalTest, YurkinCountingNumberFuncTest, kGtestValues,
+                         [](const testing::TestParamInfo<YurkinCountingNumberFuncTest::ParamType> &info) {
+                           return std::string(kFuncTestName) + "_" + info.param.name;
+                         });
 
 }  // namespace yurkin_counting_number
