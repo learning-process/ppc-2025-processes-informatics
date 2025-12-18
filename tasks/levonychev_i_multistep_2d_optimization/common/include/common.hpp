53241d7d4ad7cb45e212fbc06a9c2e60a4b50dce
fartekool <levonychev2006@gmail.com>
2025-12-18 15:52:11 +0300

    input/output in common.gpp

--- tasks/levonychev_i_multistep_2d_optimization/common/include/common.hpp
+++ tasks/levonychev_i_multistep_2d_optimization/common/include/common.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <string>
 #include <tuple>
 
@@ -7,8 +8,33 @@
 
 namespace levonychev_i_multistep_2d_optimization {
 
-using InType = int;
-using OutType = int;
+struct OptimizationParams {
+  std::function<double(double, double)> func;
+
+  double x_min = 0.0;
+  double x_max = 1.0;
+  double y_min = 0.0;
+  double y_max = 1.0;
+  
+  int num_steps = 3;
+  
+  int grid_size_step1 = 10;
+
+  int candidates_per_step = 4;
+
+  bool use_local_optimization = true;
+};
+
+struct OptimizationResult {
+  double x_min = 0.0;
+  double y_min = 0.0;
+  double value = 0.0;
+  
+  int iterations = 0;
+};
+
+using InType = OptimizationParams;
+using OutType = OptimizationResult;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
