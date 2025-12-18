2d92d52f3cb8e477cd8c8e1187cc7985e3247bed
fartekool <levonychev2006@gmail.com>
2025-12-18 17:08:05 +0300

    first tests

--- tasks/levonychev_i_multistep_2d_optimization/tests/functional/main.cpp
+++ tasks/levonychev_i_multistep_2d_optimization/tests/functional/main.cpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <array>
+#include <cmath>
 #include <cstddef>
 #include <cstdint>
 #include <numeric>
@@ -20,6 +21,29 @@
 
 namespace levonychev_i_multistep_2d_optimization {
 
+inline double ParaboloidFunction(double x, double y, double x0, double y0) {
+  double dx = x - x0;
+  double dy = y - y0;
+  return dx * dx + dy * dy;
+}
+
+inline double RosenbrockFunction(double x, double y) {
+  double term1 = 1.0 - x;
+  double term2 = y - x * x;
+  return term1 * term1 + 100.0 * term2 * term2;
+}
+
+inline double RastriginFunction(double x, double y) {
+  const double pi = 3.14159265358979323846;
+  const double A = 10.0;
+  const double n = 2.0; 
+  
+  double term1 = x * x - A * std::cos(2.0 * pi * x);
+  double term2 = y * y - A * std::cos(2.0 * pi * y);
+  
+  return A * n + term1 + term2;
+}
+
 class LevonychevIMultistep2dOptimizationFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
@@ -28,11 +52,119 @@ class LevonychevIMultistep2dOptimizationFuncTests : public ppc::util::BaseRunFun
 
  protected:
   void SetUp() override {
+    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
+    int test_id = std::get<0>(params);
     
+    if (test_id == 1) {
+      const double x_min = 0.0;
+      const double x_max = 5.0;
+      const double y_min = 0.0;
+      const double y_max = 5.0;
+      
+      double x0 = 2.5;
+      double y0 = 2.5; 
+      
+      auto func = [x0, y0](double x, double y) -> double {
+        return ParaboloidFunction(x, y, x0, y0);
+      };
+      
+      input_data_.func = func;
+      input_data_.x_min = x_min;
+      input_data_.x_max = x_max;
+      input_data_.y_min = y_min;
+      input_data_.y_max = y_max;
+      input_data_.num_steps = 3;
+      input_data_.grid_size_step1 = 100;
+      input_data_.candidates_per_step = 4;
+      input_data_.use_local_optimization = true;
+      
+      expected_x_min_ = x0;
+      expected_y_min_ = y0;
+      expected_value_ = 0.0;
+      
+    } else if (test_id == 2) {
+      const double x_min = 0.0;
+      const double x_max = 2.0;
+      const double y_min = 0.0;
+      const double y_max = 2.0;
+      
+      auto func = [](double x, double y) -> double {
+        return RosenbrockFunction(x, y);
+      };
+      
+      input_data_.func = func;
+      input_data_.x_min = x_min;
+      input_data_.x_max = x_max;
+      input_data_.y_min = y_min;
+      input_data_.y_max = y_max;
+      input_data_.num_steps = 5; 
+      input_data_.grid_size_step1 = 100;
+      input_data_.candidates_per_step = 10;
+      input_data_.use_local_optimization = true;
+      
+      expected_x_min_ = 1.0;
+      expected_y_min_ = 1.0;
+      expected_value_ = 0.0;
+      
+    } else if (test_id == 3) {
+      const double x_min = -2.0;
+      const double x_max = 2.0;
+      const double y_min = -2.0;
+      const double y_max = 2.0;
+      
+      auto func = [](double x, double y) -> double {
+        return RastriginFunction(x, y);
+      };
+      
+      input_data_.func = func;
+      input_data_.x_min = x_min;
+      input_data_.x_max = x_max;
+      input_data_.y_min = y_min;
+      input_data_.y_max = y_max;
+      input_data_.num_steps = 5;
+      input_data_.grid_size_step1 = 100;
+      input_data_.candidates_per_step = 10;
+      input_data_.use_local_optimization = true;
+      
+      expected_x_min_ = 0.0;
+      expected_y_min_ = 0.0;
+      expected_value_ = 0.0;
+    }
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return (input_data_ == output_data);
+    if (output_data.iterations <= 0) {
+      return false;
+    }
+
+    if (output_data.x_min < input_data_.x_min || output_data.x_min > input_data_.x_max ||
+        output_data.y_min < input_data_.y_min || output_data.y_min > input_data_.y_max) {
+      return false;
+    }
+
+    if (!std::isfinite(output_data.value)) {
+      return false;
+    }
+    
+    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
+    int test_id = std::get<0>(params);
+    
+    double tolerance_coord = 0.1;
+    double tolerance_value = 0.01;
+
+    if (test_id == 2) { 
+      tolerance_coord = 2;
+      tolerance_value = 1;
+    } else if (test_id == 3) { 
+      tolerance_coord = 3;
+      tolerance_value = 5;
+    }
+    std::cout << test_id << " x_min: " << output_data.x_min << " y_min: " << output_data.y_min << " value: " << output_data.value << std::endl;
+    bool x_ok = std::abs(output_data.x_min - expected_x_min_) < tolerance_coord;
+    bool y_ok = std::abs(output_data.y_min - expected_y_min_) < tolerance_coord;
+    bool value_ok = std::abs(output_data.value - expected_value_) < tolerance_value;
+    
+    return x_ok && y_ok && value_ok;
   }
 
   InType GetTestInputData() final {
@@ -40,7 +172,10 @@ class LevonychevIMultistep2dOptimizationFuncTests : public ppc::util::BaseRunFun
   }
 
  private:
-  InType input_data_ = 0;
+  InType input_data_;
+  double expected_x_min_ = 0.0;
+  double expected_y_min_ = 0.0;
+  double expected_value_ = 0.0;
 };
 
 namespace {
@@ -49,7 +184,7 @@ TEST_P(LevonychevIMultistep2dOptimizationFuncTests, Multistep2dOptimization) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 3> kTestParam = {std::make_tuple(3, "3"), std::make_tuple(5, "5"), std::make_tuple(7, "7")};
+const std::array<TestType, 3> kTestParam = {std::make_tuple(1, "1"), std::make_tuple(2, "2"), std::make_tuple(3, "3")};
 
 const auto kTestTasksList =
     std::tuple_cat(ppc::util::AddFuncTask<LevonychevIMultistep2dOptimizationMPI, InType>(kTestParam, PPC_SETTINGS_levonychev_i_multistep_2d_optimization),
--- tasks/levonychev_i_multistep_2d_optimization/tests/performance/main.cpp
+++ tasks/levonychev_i_multistep_2d_optimization/tests/performance/main.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <cmath>
+
 #include "levonychev_i_multistep_2d_optimization/common/include/common.hpp"
 #include "levonychev_i_multistep_2d_optimization/mpi/include/ops_mpi.hpp"
 #include "levonychev_i_multistep_2d_optimization/seq/include/ops_seq.hpp"
@@ -7,16 +9,66 @@
 
 namespace levonychev_i_multistep_2d_optimization {
 
+
+
+inline double RosenbrockFunction(double x, double y) {
+  double term1 = 1.0 - x;
+  double term2 = y - x * x;
+  return term1 * term1 + 100.0 * term2 * term2;
+}
+
+
 class LevonychevIMultistep2dOptimizationPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
   InType input_data_{};
+  double expected_x_min_ = 0.0;
+  double expected_y_min_ = 0.0;
+  double expected_value_min_ = 0.0;
 
   void SetUp() override {
-    input_data_ = kCount_;
+    auto func = [](double x, double y) -> double {
+      return RosenbrockFunction(x, y);
+    };
+
+    input_data_.func = func;
+    input_data_.x_min = -2.0;
+    input_data_.x_max = 2.0;
+    input_data_.y_min = -2.0;
+    input_data_.y_max = 2.0;
+    input_data_.num_steps = 6; 
+    input_data_.grid_size_step1 = 100; 
+    input_data_.candidates_per_step = 10; 
+    input_data_.use_local_optimization = true;
+
+    expected_x_min_ = 1.0;
+    expected_y_min_ = 1.0;
+    expected_value_min_ = RosenbrockFunction(1.0, 1.0); 
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return input_data_ == output_data;
+    
+    if (output_data.iterations <= 0) {
+      return false;
+    }
+
+    if (output_data.x_min < input_data_.x_min || output_data.x_min > input_data_.x_max ||
+        output_data.y_min < input_data_.y_min || output_data.y_min > input_data_.y_max) {
+      return false;
+    }
+
+    if (!std::isfinite(output_data.value)) {
+      return false;
+    }
+
+    const double tolerance_coord = 0.5; 
+    const double tolerance_value = 2.0;
+    std::cout << " x_min: " << output_data.x_min << " y_min: " << output_data.y_min << " value: " << output_data.value << std::endl;
+    bool x_ok = std::abs(output_data.x_min - expected_x_min_) < tolerance_coord;
+    bool y_ok = std::abs(output_data.y_min - expected_y_min_) < tolerance_coord;
+    bool value_ok = std::abs(output_data.value - expected_value_min_) < tolerance_value;
+
+    bool value_reasonable = output_data.value < expected_value_min_ + 20.0;
+    
+    return x_ok && y_ok && value_ok && value_reasonable;
   }
 
   InType GetTestInputData() final {
