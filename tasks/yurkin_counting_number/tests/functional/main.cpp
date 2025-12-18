b1d1d0e8488db6e6f33c83a53de2257854ffa6ef
georghinho <yurkin23egor@yandex.ru>
2025-12-19 00:55:22 +0300

    update7

--- tasks/yurkin_counting_number/tests/functional/main.cpp
+++ tasks/yurkin_counting_number/tests/functional/main.cpp
@@ -1,28 +1,26 @@
 #include <gtest/gtest.h>
 
-#include "util/include/func_test_util.hpp"
+#include "core/include/task_test.hpp"
 #include "yurkin_counting_number/common/include/common.hpp"
 #include "yurkin_counting_number/mpi/include/ops_mpi.hpp"
 #include "yurkin_counting_number/seq/include/ops_seq.hpp"
 
 namespace yurkin_counting_number {
 
-class YurkinCountingNumberFuncTest : public ppc::util::TaskFunctionalTest<InType, OutType> {
+class YurkinCountingNumberFuncTest : public ppc::core::TaskTest<InType, OutType> {
  public:
   void SetUp() override {
-    test_input_ = {'A', 'b', 'c', '1', '2', '3', '!', '!', '!', 'd', 'e', 'F', '#', '4', '5'};
+    input_data_ = {'A', 'b', 'c', '1', '2', '3', 'd', 'e', 'F', '#'};
   }
 
-  bool CheckTestOutputData(const OutType &output) override {
+  bool CheckTestOutput(const OutType &output) override {
     int cnt = 0;
-
-    for (char c : test_input_) {
+    for (char c : input_data_) {
       if (std::isalpha(static_cast<unsigned char>(c))) {
         cnt++;
       }
     }
-
-    return output == cnt;
+    return cnt == output;
   }
 };
 
@@ -30,8 +28,8 @@ TEST_P(YurkinCountingNumberFuncTest, FunctionalRun) {
   Run();
 }
 
-INSTANTIATE_TEST_SUITE_P(FunctionalTest, YurkinCountingNumberFuncTest,
-                         ppc::util::CreateTasks<InType, YurkinCountingNumberMPI, YurkinCountingNumberSeq>(
-                             PPC_SETTINGS_yurkin_counting_number));
+INSTANTIATE_TEST_SUITE_P(Functional, YurkinCountingNumberFuncTest,
+                         testing::Values(std::make_shared<YurkinCountingNumberSeq>(),
+                                         std::make_shared<YurkinCountingNumberMPI>()));
 
 }  // namespace yurkin_counting_number
