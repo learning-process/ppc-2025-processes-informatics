9dc9ad72f674e4af96ef1b786b1bc59e91b94577
fartekool <levonychev2006@gmail.com>
2025-12-18 15:49:28 +0300

    local opt in optimization_common.hpp

---
+++ tasks/levonychev_i_multistep_2d_optimization/common/include/optimization_common.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <limits>
+#include <vector>
+
+namespace levonychev_i_multistep_2d_optimization {
+
+struct Point {
+  double x = 0.0;
+  double y = 0.0;
+  double value = std::numeric_limits<double>::max();
+  
+  Point() = default;
+  Point(double x_val, double y_val, double func_value) 
+      : x(x_val), y(y_val), value(func_value) {}
+  
+  bool operator<(const Point& other) const {
+    return value < other.value;
+  }
+};
+
+struct SearchRegion {
+  double x_min = 0.0;
+  double x_max = 0.0;
+  double y_min = 0.0;
+  double y_max = 0.0;
+  
+  SearchRegion() = default;
+  SearchRegion(double x_mn, double x_mx, double y_mn, double y_mx)
+      : x_min(x_mn), x_max(x_mx), y_min(y_mn), y_max(y_mx) {}
+};
+
+inline Point LocalOptimization(const std::function<double(double, double)>& func,
+                               double x_start, double y_start,
+                               double x_min, double x_max,
+                               double y_min, double y_max,
+                               int max_iterations = 100) {
+  const double epsilon = 1e-6;
+  const double step_size = 0.01;
+  
+  double x = x_start;
+  double y = y_start;
+  double current_value = func(x, y);
+  
+  for (int iter = 0; iter < max_iterations; ++iter) {
+    const double h = 1e-5;
+    double grad_x = (func(x + h, y) - func(x - h, y)) / (2.0 * h);
+    double grad_y = (func(x, y + h) - func(x, y - h)) / (2.0 * h);
+    
+    double new_x = x - step_size * grad_x;
+    double new_y = y - step_size * grad_y;
+    
+    new_x = std::max(x_min, std::min(x_max, new_x));
+    new_y = std::max(y_min, std::min(y_max, new_y));
+    
+    double new_value = func(new_x, new_y);
+    
+    if (std::abs(new_value - current_value) < epsilon) {
+      break;
+    }
+    
+    x = new_x;
+    y = new_y;
+    current_value = new_value;
+  }
+  
+  return Point(x, y, current_value);
+}
+
+inline std::vector<Point> SearchInRegion(const std::function<double(double, double)>& func,
+                                         const SearchRegion& region,
+                                         int grid_size) {
+  std::vector<Point> points;
+  
+  double step_x = (region.x_max - region.x_min) / (grid_size - 1);
+  double step_y = (region.y_max - region.y_min) / (grid_size - 1);
+  
+  for (int i = 0; i < grid_size; ++i) {
+    for (int j = 0; j < grid_size; ++j) {
+      double x = region.x_min + i * step_x;
+      double y = region.y_min + j * step_y;
+      
+      double value = func(x, y);
+      points.emplace_back(x, y, value);
+    }
+  }
+  
+  std::sort(points.begin(), points.end());
+  return points;
+}
+
+}  // namespace levonychev_i_multistep_2d_optimization
+
