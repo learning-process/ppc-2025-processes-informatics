aee95d4315caa934bca504e3e3db5360ae1ff155
georghinho <yurkin23egor@yandex.ru>
2025-12-20 04:54:52 +0300

    update27

--- tasks/yurkin_counting_number/tests/performance/main.cpp
+++ tasks/yurkin_counting_number/tests/performance/main.cpp
@@ -1,47 +1,20 @@
-#include <gtest/gtest.h>
-
-#include <cstddef>
-#include <string>
-
 #include "util/include/perf_test_util.hpp"
+#include "util/include/util.hpp"
 #include "yurkin_counting_number/common/include/common.hpp"
 #include "yurkin_counting_number/mpi/include/ops_mpi.hpp"
 #include "yurkin_counting_number/seq/include/ops_seq.hpp"
 
 namespace yurkin_counting_number {
 
-class YurkinCountingNumberPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  static constexpr int kCount = 5'000'000;
-  InType input_data_{};
-
-  void SetUp() override {
-    input_data_.assign(kCount, '5');
-
-    for (int i = 1; i < kCount; i += 2) {
-      input_data_[i] = 'a';
-    }
-  }
-
-  bool CheckTestOutputData(OutType &output_data) final {
-    return output_data == kCount / 2;
-  }
-
-  InType GetTestInputData() final {
-    return input_data_;
-  }
-};
+class YurkinCountingNumberPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {};
 
 TEST_P(YurkinCountingNumberPerfTests, RunPerfModes) {
-  ExecuteTest(GetParam());
+  RunAllPerf("yurkin_counting_number");
 }
 
-const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, YurkinCountingNumberMPI, YurkinCountingNumberSEQ>(
-    PPC_SETTINGS_yurkin_counting_number);
+static const std::array<InType, 3> kInputs = {InType(1000, 'a'), InType(2000, 'b'), InType(5000, 'c')};
 
-const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
-
-const auto kPerfTestName = YurkinCountingNumberPerfTests::CustomPerfTestName;
-
-INSTANTIATE_TEST_SUITE_P(RunModeTests, YurkinCountingNumberPerfTests, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(YurkinPerfTests, YurkinCountingNumberPerfTests, ::testing::ValuesIn(kInputs),
+                         ::testing::PrintToStringParamName());
 
 }  // namespace yurkin_counting_number
