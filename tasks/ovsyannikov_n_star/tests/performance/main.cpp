9a971db6525121be878e511c6f3de13be3981f35
unknown <nik.ovsyannikov2090@gmail.com>
2025-12-22 02:16:52 +0300

    fixed ct

--- tasks/ovsyannikov_n_star/tests/performance/main.cpp
+++ tasks/ovsyannikov_n_star/tests/performance/main.cpp
@@ -1,8 +1,5 @@
 #include <gtest/gtest.h>
 
-#include <string>
-#include <tuple>
-
 #include "ovsyannikov_n_star/common/include/common.hpp"
 #include "ovsyannikov_n_star/mpi/include/ops_mpi.hpp"
 #include "ovsyannikov_n_star/seq/include/ops_seq.hpp"
@@ -17,28 +14,22 @@ class OvsyannikovNRunPerfTestStar : public ppc::util::BaseRunPerfTests<InType, O
     expected_output_ = 1000;
   }
 
-  bool CheckTestOutputData(OutType &actual_res) final {
-    return actual_res == expected_output_;
-  }
+  bool CheckTestOutputData(OutType& actual_res) final { return actual_res == expected_output_; }
 
-  InType GetTestInputData() final {
-    return input_data_;
-  }
+  InType GetTestInputData() final { return input_data_; }
 
  private:
   InType input_data_;
   OutType expected_output_ = 0;
 };
 
-TEST_P(OvsyannikovNRunPerfTestStar, RunPerfModes) {
-  ExecuteTest(GetParam());
-}
+TEST_P(OvsyannikovNRunPerfTestStar, RunPerfModes) { ExecuteTest(GetParam()); }
 
-const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, OvsyannikovNStarMPI, OvsyannikovNStarSEQ>(PPC_SETTINGS_ovsyannikov_n_star);
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, OvsyannikovNStarMPI, OvsyannikovNStarSEQ>(
+    PPC_SETTINGS_ovsyannikov_n_star);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 const auto kPerfTestName = OvsyannikovNRunPerfTestStar::CustomPerfTestName;
 INSTANTIATE_TEST_SUITE_P(StarPerfTests, OvsyannikovNRunPerfTestStar, kGtestValues, kPerfTestName);
 
-}  // namespace ovsyannikov_n_star
+}
