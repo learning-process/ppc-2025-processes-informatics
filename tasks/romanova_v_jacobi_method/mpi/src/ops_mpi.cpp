596670a524c763a730d2fb1a2550b7f5e68736ac
vassyusha <romanoffmiss@yandex.ru>
2025-12-24 16:13:24 +0300

    fix:static_cast

--- tasks/romanova_v_jacobi_method/mpi/src/ops_mpi.cpp
+++ tasks/romanova_v_jacobi_method/mpi/src/ops_mpi.cpp
@@ -80,8 +80,8 @@ bool RomanovaVJacobiMethodMPI::PreProcessingImpl() {
   size_t delta = n_ / n;
   size_t extra = n_ % n;
 
-  local_n_ = delta + (std::static_cast<size_t>(rank) < extra ? 1 : 0);
-  st_row_ = rank * delta + (std::static_cast<size_t>(rank) ? rank : extra);
+  local_n_ = delta + (static_cast<size_t>(rank) < extra ? 1 : 0);
+  st_row_ = rank * delta + (static_cast<size_t>(rank) ? rank : extra);
 
   if (rank == 0) {
     send_countsA = std::vector<int>(n, static_cast<int>(delta * n_));
