5f18fb46158b7960a13ebd84555b5ca8522ccfc9
georghinho <yurkin23egor@yandex.ru>
2025-12-24 16:43:44 +0300

    add report + fix

--- tasks/yurkin_g_ruler/mpi/src/ops_mpi.cpp
+++ tasks/yurkin_g_ruler/mpi/src/ops_mpi.cpp
@@ -3,23 +3,25 @@
 #include <mpi.h>
 
 #include <algorithm>
+#include <cstddef>
+#include <cstdint>
 
 #include "yurkin_g_ruler/common/include/common.hpp"
 
 namespace yurkin_g_ruler {
 
 YurkinGRulerMPI::YurkinGRulerMPI(const InType &in) {
-  SetTypeOfTask(GetStaticTypeOfTask());
-  GetInput() = in;
-  GetOutput() = 0;
+  this->SetTypeOfTask(this->GetStaticTypeOfTask());
+  this->GetInput() = in;
+  this->GetOutput() = 0;
 }
 
 bool YurkinGRulerMPI::ValidationImpl() {
-  return (GetInput() >= 0) && (GetOutput() == 0);
+  return (this->GetInput() >= 0) && (this->GetOutput() == 0);
 }
 
 bool YurkinGRulerMPI::PreProcessingImpl() {
-  GetOutput() = 0;
+  this->GetOutput() = 0;
   return true;
 }
 
@@ -33,56 +35,40 @@ bool YurkinGRulerMPI::RunImpl() {
     return false;
   }
 
-  int src = 0;
-  int dst = size - 1;
-
+  int src = this->GetInput() % size;
+  int dst = (this->GetInput() / size) % size;
   src = std::clamp(src, 0, size - 1);
   dst = std::clamp(dst, 0, size - 1);
 
-  GetOutput() = GetInput();
-  int payload = GetInput();
+  int payload = this->GetInput();
 
   if (src == dst) {
+    this->GetOutput() = payload;
     MPI_Barrier(MPI_COMM_WORLD);
     return true;
   }
 
-  const int low = std::min(src, dst);
-  const int high = std::max(src, dst);
-  const int direction = (dst > src) ? +1 : -1;
-
-  if (rank < low || rank > high) {
-    MPI_Barrier(MPI_COMM_WORLD);
-    return true;
-  }
+  int next = (rank + 1) % size;
+  int prev = (rank - 1 + size) % size;
 
   if (rank == src) {
-    const int next = rank + direction;
     MPI_Send(&payload, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    return true;
+  } else if (rank == dst) {
+    int recv_val = 0;
+    MPI_Recv(&recv_val, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    this->GetOutput() = recv_val;
+  } else {
+    int recv_val = 0;
+    MPI_Recv(&recv_val, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Send(&recv_val, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
   }
-
-  const int prev = rank - direction;
-  int recv_val = 0;
-  MPI_Recv(&recv_val, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-  if (rank == dst) {
-    GetOutput() = recv_val;
-    MPI_Barrier(MPI_COMM_WORLD);
-    return true;
-  }
-
-  const int next = rank + direction;
-  MPI_Send(&recv_val, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
 
   MPI_Barrier(MPI_COMM_WORLD);
   return true;
 }
 
 bool YurkinGRulerMPI::PostProcessingImpl() {
-  return GetOutput() >= 0;
+  return this->GetOutput() >= 0;
 }
 
 }  // namespace yurkin_g_ruler
