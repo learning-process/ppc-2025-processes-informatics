422d9fd4587b08423f7a25a1cafb9cb697918527
SonyaMorozova <sonyasmorozova@gmail.com>
2025-12-24 16:36:10 +0300

    Update perf

--- tasks/morozova_s_broadcast/tests/performance/main.cpp
+++ tasks/morozova_s_broadcast/tests/performance/main.cpp
@@ -14,9 +14,10 @@ class MorozovaSRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType,
   InType input_data_;
 
   void SetUp() override {
+    auto test_info = GetParam();
+    int test_idx = std::get<0>(test_info);
     const int sizes[] = {100, 1000, 10000, 100000, 1000000};
-    int test_idx = GetTestIndex() % 5;
-    int size = sizes[test_idx];
+    int size = sizes[test_idx % 5];
 
     input_data_.resize(size);
     std::iota(input_data_.begin(), input_data_.end(), 0);
@@ -37,11 +38,8 @@ TEST_P(MorozovaSRunPerfTestProcesses, RunPerfModes) {
 
 const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, MorozovaSBroadcastMPI, MorozovaSBroadcastSEQ>(
     PPC_SETTINGS_morozova_s_broadcast);
-
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
-
 const auto kPerfTestName = MorozovaSRunPerfTestProcesses::CustomPerfTestName;
-
 INSTANTIATE_TEST_SUITE_P(RunModeTests, MorozovaSRunPerfTestProcesses, kGtestValues, kPerfTestName);
 
 }  // namespace morozova_s_broadcast
