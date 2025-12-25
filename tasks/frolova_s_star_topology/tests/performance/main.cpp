e1890cee268a4f67004cf0ebcf55db1cc06fed51
Andrew-Kop <144238394+cuttty@users.noreply.github.com>
2025-12-25 17:34:31 +0300

    fix perf test

--- tasks/frolova_s_star_topology/tests/performance/main.cpp
+++ tasks/frolova_s_star_topology/tests/performance/main.cpp
@@ -8,7 +8,7 @@
 namespace frolova_s_star_topology {
 
 class FrolovaSRunPerfTestsProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 10000;
+  const int kCount_ = 2147483645;
   InType input_data_{};
 
   void SetUp() override {
