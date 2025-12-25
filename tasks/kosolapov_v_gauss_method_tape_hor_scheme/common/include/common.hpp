0ac8bd284166a4b248476799f1dc67827e41ffcf
Kosolapov-Vitaliy <givit3324@gmail.com>
2025-12-25 12:21:56 +0300

    debugging 2

--- tasks/kosolapov_v_gauss_method_tape_hor_scheme/common/include/common.hpp
+++ tasks/kosolapov_v_gauss_method_tape_hor_scheme/common/include/common.hpp
@@ -12,7 +12,13 @@ struct LinSystem {
   std::vector<double> r_side;
   LinSystem() = default;
   LinSystem(const LinSystem &other) : matrix(other.matrix), r_side(other.r_side) {};
-  LinSystem &operator=(const LinSystem &other) = default;
+  LinSystem &operator=(const LinSystem &other) {
+    if (this != &other) {
+      matrix = std::vector<std::vector<double>>(other.matrix);
+      r_side = std::vector<double> r_side(other.r_side);
+    }
+    return *this;
+  }
 };
 
 using InType = LinSystem;
