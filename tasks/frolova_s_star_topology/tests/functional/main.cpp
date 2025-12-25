97782cf3aa388c44027ff75b6561a6ed1d18e493
Andrew-Kop <144238394+cuttty@users.noreply.github.com>
2025-12-25 14:29:06 +0300

    fix tests

--- tasks/frolova_s_star_topology/tests/functional/main.cpp
+++ tasks/frolova_s_star_topology/tests/functional/main.cpp
@@ -1,261 +1,61 @@
 #include <gtest/gtest.h>
-#include <mpi.h>
 
-#include <algorithm>
+#include <array>
 #include <cstddef>
-#include <random>
-#include <vector>
+#include <string>
+#include <tuple>
+#include <utility>
 
+#include "frolova_s_star_topology/common/include/common.hpp"
 #include "frolova_s_star_topology/mpi/include/ops_mpi.hpp"
+#include "frolova_s_star_topology/seq/include/ops_seq.hpp"
+#include "util/include/func_test_util.hpp"
+#include "util/include/util.hpp"
 
 namespace frolova_s_star_topology {
 
-static std::vector<int> MakeRandomVector(size_t sz) {
-  std::random_device dev;
-  std::mt19937 gen(dev());
-  std::vector<int> vec(sz);
-  for (size_t i = 0; i < sz; i++) {
-    vec[i] = static_cast<int>((gen() % 200) - 100);
+class FrolovaSRunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+ public:
+  static std::string PrintTestParam(const TestType &test_param) {
+    return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
   }
-  return vec;
+
+ protected:
+  void SetUp() override {
+    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
+    input_data_ = std::get<0>(params);
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return output_data >= 0;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+
+ private:
+  InType input_data_ = 1;
+};
+
+namespace {
+
+TEST_P(FrolovaSRunFuncTestsProcesses, StarTopologyTest) {
+  ExecuteTest(GetParam());
 }
+
+const std::array<TestType, 3> kTestParam = {std::make_tuple(1, "1"), std::make_tuple(2, "2"), std::make_tuple(3, "3")};
+
+const auto kTestTasksList = std::tuple_cat(
+    ppc::util::AddFuncTask<FrolovaSStarTopologyMPI, InType>(kTestParam, PPC_SETTINGS_frolova_s_star_topology),
+    ppc::util::AddFuncTask<FrolovaSStarTopologySEQ, InType>(kTestParam, PPC_SETTINGS_frolova_s_star_topology));
+
+const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
+
+const auto kPerfTestName = FrolovaSRunFuncTestsProcesses::PrintFuncTestName<FrolovaSRunFuncTestsProcesses>;
+
+INSTANTIATE_TEST_SUITE_P(StarTopologyTests, FrolovaSRunFuncTestsProcesses, kGtestValues, kPerfTestName);
+
+}  // namespace
 
 }  // namespace frolova_s_star_topology
-
-TEST(frolovaSStar, DataLength0) {
-  const size_t data_length = 0;
-  int rank = 0;
-  int size = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  if (size < 3) {
-    int dst = (rank == 0) ? 1 : rank;
-    frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-    task.ValidationImpl();
-    task.PreProcessingImpl();
-    task.RunImpl();
-    task.PostProcessingImpl();
-    return;
-  }
-
-  std::vector<int> destinations(size - 1);
-  std::vector<int> data((size - 1) * data_length);
-  std::vector<int> res(data_length);
-
-  if (rank == 0) {
-    std::random_device rd;
-    destinations.resize(size - 1);
-    for (size_t i = 0; i < destinations.size(); ++i) {
-      destinations[i] = static_cast<int>(i) + 1;
-    }
-    std::shuffle(destinations.begin(), destinations.end(), rd);
-    data = frolova_s_star_topology::MakeRandomVector((size - 1) * data_length);
-  }
-
-  MPI_Bcast(destinations.data(), size - 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(data.data(), static_cast<int>((size - 1) * data_length), MPI_INT, 0, MPI_COMM_WORLD);
-
-  // ВАЖНО: создаем задачу для ВСЕХ процессов
-  int dst = (rank == 0) ? 1 : destinations[rank - 1];
-  frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-
-  // Проверяем только для workers
-  if (rank != 0) {
-    ASSERT_EQ(task.ValidationImpl(), true);
-  }
-
-  // ВЫЗЫВАЕМ методы для ВСЕХ процессов
-  task.PreProcessingImpl();
-  task.RunImpl();
-  task.PostProcessingImpl();
-
-  int is_failed = 0;
-  if (rank != 0) {
-    res = data;
-  }
-
-  int failures = 0;
-  MPI_Reduce(&is_failed, &failures, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  if (rank == 0) {
-    EXPECT_EQ(failures, 0);
-  }
-}
-
-TEST(frolovaSStar, DataLength64) {
-  const size_t data_length = 64;
-  int rank = 0;
-  int size = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  if (size < 3) {
-    int dst = (rank == 0) ? 1 : rank;
-    frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-    task.ValidationImpl();
-    task.PreProcessingImpl();
-    task.RunImpl();
-    task.PostProcessingImpl();
-    return;
-  }
-
-  std::vector<int> destinations(size - 1);
-  std::vector<int> data((size - 1) * data_length);
-  std::vector<int> res(data_length);
-
-  if (rank == 0) {
-    std::random_device rd;
-    destinations.resize(size - 1);
-    for (size_t i = 0; i < destinations.size(); ++i) {
-      destinations[i] = static_cast<int>(i) + 1;
-    }
-    std::shuffle(destinations.begin(), destinations.end(), rd);
-    data = frolova_s_star_topology::MakeRandomVector((size - 1) * data_length);
-  }
-
-  MPI_Bcast(destinations.data(), size - 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(data.data(), static_cast<int>((size - 1) * data_length), MPI_INT, 0, MPI_COMM_WORLD);
-
-  // ВАЖНО: создаем задачу для ВСЕХ процессов
-  int dst = (rank == 0) ? 1 : destinations[rank - 1];
-  frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-
-  // Проверяем только для workers
-  if (rank != 0) {
-    ASSERT_EQ(task.ValidationImpl(), true);
-  }
-
-  // ВЫЗЫВАЕМ методы для ВСЕХ процессов
-  task.PreProcessingImpl();
-  task.RunImpl();
-  task.PostProcessingImpl();
-
-  int is_failed = 0;
-  if (rank != 0) {
-    res = data;
-  }
-
-  int failures = 0;
-  MPI_Reduce(&is_failed, &failures, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  if (rank == 0) {
-    EXPECT_EQ(failures, 0);
-  }
-}
-
-TEST(frolovaSStar, dataLength1024) {
-  const size_t data_length = 1024;
-  int rank = 0;
-  int size = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  if (size < 3) {
-    int dst = (rank == 0) ? 1 : rank;
-    frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-    task.ValidationImpl();
-    task.PreProcessingImpl();
-    task.RunImpl();
-    task.PostProcessingImpl();
-    return;
-  }
-
-  std::vector<int> destinations(size - 1);
-  std::vector<int> data((size - 1) * data_length);
-  std::vector<int> res(data_length);
-
-  if (rank == 0) {
-    std::random_device rd;
-    destinations.resize(size - 1);
-    for (size_t i = 0; i < destinations.size(); ++i) {
-      destinations[i] = static_cast<int>(i) + 1;
-    }
-    std::shuffle(destinations.begin(), destinations.end(), rd);
-    data = frolova_s_star_topology::MakeRandomVector((size - 1) * data_length);
-  }
-
-  MPI_Bcast(destinations.data(), size - 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(data.data(), static_cast<int>((size - 1) * data_length), MPI_INT, 0, MPI_COMM_WORLD);
-
-  // ВАЖНО: создаем задачу для ВСЕХ процессов
-  int dst = (rank == 0) ? 1 : destinations[rank - 1];
-  frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-
-  // Проверяем только для workers
-  if (rank != 0) {
-    ASSERT_EQ(task.ValidationImpl(), true);
-  }
-
-  // ВЫЗЫВАЕМ методы для ВСЕХ процессов
-  task.PreProcessingImpl();
-  task.RunImpl();
-  task.PostProcessingImpl();
-
-  int is_failed = 0;
-  if (rank != 0) {
-    res = data;
-  }
-
-  int failures = 0;
-  MPI_Reduce(&is_failed, &failures, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  if (rank == 0) {
-    EXPECT_EQ(failures, 0);
-  }
-}
-
-TEST(frolovaSStar, dataLength32768) {
-  const size_t data_length = 32768;
-  int rank = 0;
-  int size = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  if (size < 3) {
-    int dst = (rank == 0) ? 1 : rank;
-    frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-    task.ValidationImpl();
-    task.PreProcessingImpl();
-    task.RunImpl();
-    task.PostProcessingImpl();
-    return;
-  }
-
-  std::vector<int> destinations(size - 1);
-  std::vector<int> data((size - 1) * data_length);
-  std::vector<int> res(data_length);
-
-  if (rank == 0) {
-    std::random_device rd;
-    destinations.resize(size - 1);
-    for (size_t i = 0; i < destinations.size(); ++i) {
-      destinations[i] = static_cast<int>(i) + 1;
-    }
-    std::shuffle(destinations.begin(), destinations.end(), rd);
-    data = frolova_s_star_topology::MakeRandomVector((size - 1) * data_length);
-  }
-
-  MPI_Bcast(destinations.data(), size - 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(data.data(), static_cast<int>((size - 1) * data_length), MPI_INT, 0, MPI_COMM_WORLD);
-
-  // ВАЖНО: создаем задачу для ВСЕХ процессов
-  int dst = (rank == 0) ? 1 : destinations[rank - 1];
-  frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-
-  // Проверяем только для workers
-  if (rank != 0) {
-    ASSERT_EQ(task.ValidationImpl(), true);
-  }
-
-  // ВЫЗЫВАЕМ методы для ВСЕХ процессов
-  task.PreProcessingImpl();
-  task.RunImpl();
-  task.PostProcessingImpl();
-
-  int is_failed = 0;
-  if (rank != 0) {
-    res = data;
-  }
-
-  int failures = 0;
-  MPI_Reduce(&is_failed, &failures, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  if (rank == 0) {
-    EXPECT_EQ(failures, 0);
-  }
-}
--- tasks/frolova_s_star_topology/tests/performance/main.cpp
+++ tasks/frolova_s_star_topology/tests/performance/main.cpp
@@ -1,116 +1,40 @@
 #include <gtest/gtest.h>
-#include <mpi.h>
 
-#include <algorithm>
-#include <cstddef>
-#include <random>
-#include <vector>
-
+#include "frolova_s_star_topology/common/include/common.hpp"
 #include "frolova_s_star_topology/mpi/include/ops_mpi.hpp"
+#include "frolova_s_star_topology/seq/include/ops_seq.hpp"
+#include "util/include/perf_test_util.hpp"
 
 namespace frolova_s_star_topology {
 
-static std::vector<int> MakeRandomVector(size_t sz) {
-  std::random_device dev;
-  std::mt19937 gen(dev());
-  std::vector<int> vec(sz);
-  for (size_t i = 0; i < sz; i++) {
-    vec[i] = static_cast<int>((gen() % 200) - 100);
+class FrolovaSRunPerfTestsProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+  const int kCount_ = 1;
+  InType input_data_{};
+
+  void SetUp() override {
+    input_data_ = kCount_;
   }
-  return vec;
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return output_data >= 0;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+};
+
+TEST_P(FrolovaSRunPerfTestsProcesses, RunPerfModes) {
+  ExecuteTest(GetParam());
 }
+
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, FrolovaSStarTopologyMPI, FrolovaSStarTopologySEQ>(
+    PPC_SETTINGS_frolova_s_star_topology);
+
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
+const auto kPerfTestName = FrolovaSRunPerfTestsProcesses::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests, FrolovaSRunPerfTestsProcesses, kGtestValues, kPerfTestName);
 
 }  // namespace frolova_s_star_topology
-
-TEST(frolovaSStar, pipelineRun) {
-  int rank = 0;
-  int size = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  if (size < 3) {
-    int dst = (rank == 0) ? 1 : rank;
-    frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-    task.ValidationImpl();
-    task.PreProcessingImpl();
-    task.RunImpl();
-    task.PostProcessingImpl();
-    return;
-  }
-  const size_t data_length = 4096;
-  std::vector<int> destinations(size - 1);
-  std::vector<int> data((size - 1) * data_length);
-
-  if (rank == 0) {
-    std::random_device rd;
-    destinations.resize(size - 1);
-    for (size_t i = 0; i < destinations.size(); ++i) {
-      destinations[i] = static_cast<int>(i) + 1;
-    }
-    std::shuffle(destinations.begin(), destinations.end(), rd);
-    data = frolova_s_star_topology::MakeRandomVector((size - 1) * data_length);
-  }
-
-  MPI_Bcast(destinations.data(), size - 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(data.data(), static_cast<int>((size - 1) * data_length), MPI_INT, 0, MPI_COMM_WORLD);
-
-  // ВАЖНО: создаем задачу для ВСЕХ процессов
-  int dst = (rank == 0) ? 1 : destinations[rank - 1];
-  frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-
-  // Проверяем только для workers
-  if (rank != 0) {
-    ASSERT_EQ(task.ValidationImpl(), true);
-  }
-
-  // ВЫЗЫВАЕМ методы для ВСЕХ процессов
-  task.PreProcessingImpl();
-  task.RunImpl();
-  task.PostProcessingImpl();
-}
-
-TEST(frolovaSStar, taskRun) {
-  int rank = 0;
-  int size = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  if (size < 3) {
-    int dst = (rank == 0) ? 1 : rank;
-    frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-    task.ValidationImpl();
-    task.PreProcessingImpl();
-    task.RunImpl();
-    task.PostProcessingImpl();
-    return;
-  }
-
-  const size_t data_length = 4096;
-  std::vector<int> destinations(size - 1);
-  std::vector<int> data((size - 1) * data_length);
-
-  if (rank == 0) {
-    std::random_device rd;
-    destinations.resize(size - 1);
-    for (size_t i = 0; i < destinations.size(); ++i) {
-      destinations[i] = static_cast<int>(i) + 1;
-    }
-    std::shuffle(destinations.begin(), destinations.end(), rd);
-    data = frolova_s_star_topology::MakeRandomVector((size - 1) * data_length);
-  }
-
-  MPI_Bcast(destinations.data(), size - 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(data.data(), static_cast<int>((size - 1) * data_length), MPI_INT, 0, MPI_COMM_WORLD);
-
-  // ВАЖНО: создаем задачу для ВСЕХ процессов
-  int dst = (rank == 0) ? 1 : destinations[rank - 1];
-  frolova_s_star_topology::FrolovaSStarTopologyMPI task(dst);
-
-  // Проверяем только для workers
-  if (rank != 0) {
-    ASSERT_EQ(task.ValidationImpl(), true);
-  }
-
-  // ВЫЗЫВАЕМ методы для ВСЕХ процессов
-  task.PreProcessingImpl();
-  task.RunImpl();
-  task.PostProcessingImpl();
-}
