1031ed44ffccfb802453ca71e3a0830f1079718d
Andrew-Kop <144238394+cuttty@users.noreply.github.com>
2025-12-25 14:59:34 +0300

    fix bugs

--- tasks/frolova_s_star_topology/mpi/src/ops_mpi.cpp
+++ tasks/frolova_s_star_topology/mpi/src/ops_mpi.cpp
@@ -46,47 +46,75 @@ bool FrolovaSStarTopologyMPI::RunImpl() {
 
   if (rank == 0) {
     std::vector<int> buf;
+
+    // Получаем все запросы и маршрутируем
     for (int i = 0; i < nodes; i++) {
       int dst = 0;
       MPI_Recv(&dst, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
       const int src = status.MPI_SOURCE;
+
+      if (dst < 0 || dst >= size) {
+        int error = -1;
+        MPI_Send(&error, 1, MPI_INT, src, 0, MPI_COMM_WORLD);
+        continue;
+      }
+
       MPI_Probe(src, 0, MPI_COMM_WORLD, &status);
       int buf_size = 0;
       MPI_Get_count(&status, MPI_INT, &buf_size);
       buf.resize(buf_size);
       MPI_Recv(buf.data(), buf_size, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      // Отправляем получателю
       MPI_Send(&src, 1, MPI_INT, dst, 0, MPI_COMM_WORLD);
       MPI_Send(buf.data(), buf_size, MPI_INT, dst, 0, MPI_COMM_WORLD);
     }
+
+    // Отправляем завершающие сообщения
     for (int i = 0; i < nodes; i++) {
-      MPI_Send(&kTerm, 1, MPI_INT, i + 1, 0, MPI_COMM_WORLD);
+      int finish = -1;
+      MPI_Send(&finish, 1, MPI_INT, i + 1, 1, MPI_COMM_WORLD);
     }
 
   } else {
+    // Отправляем запрос центральному
     MPI_Send(&dest_, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+    // Отправляем данные (если есть)
     if (!data_.empty()) {
       MPI_Send(data_.data(), static_cast<int>(data_.size()), MPI_INT, 0, 0, MPI_COMM_WORLD);
     } else {
       int dummy = 0;
       MPI_Send(&dummy, 0, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
+
+    // Ждем сообщения от центрального
     while (true) {
-      int src = 0;
-      MPI_Recv(&src, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      if (src == kTerm) {
+      MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+      if (status.MPI_TAG == 1) {
+        int finish = 0;
+        MPI_Recv(&finish, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         break;
+      } else if (status.MPI_TAG == 0) {
+        int src = 0;
+        MPI_Recv(&src, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        if (src < 0) {
+          break;
+        }
+
+        MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
+        int buf_size = 0;
+        MPI_Get_count(&status, MPI_INT, &buf_size);
+        output_.resize(buf_size);
+        MPI_Recv(output_.data(), buf_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       }
-      MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
-      int buf_size = 0;
-      MPI_Get_count(&status, MPI_INT, &buf_size);
-      output_.resize(buf_size);
-      MPI_Recv(output_.data(), buf_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
   }
 
   return true;
 }
-
 bool FrolovaSStarTopologyMPI::PostProcessingImpl() {
   int rank = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
