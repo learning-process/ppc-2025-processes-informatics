ed91ee6288e962cf895eeb627ce1ab1af12d1235
unknown <nik.ovsyannikov2090@gmail.com>
2025-12-25 20:13:49 +0300

    cf

--- tasks/ovsyannikov_n_star/mpi/src/ops_mpi.cpp
+++ tasks/ovsyannikov_n_star/mpi/src/ops_mpi.cpp
@@ -1,81 +1,86 @@
 #include "ovsyannikov_n_star/mpi/include/ops_mpi.hpp"
+
 #include <mpi.h>
-#include <vector>
+
 #include <algorithm>
+#include <vector>
+
 #include "ovsyannikov_n_star/common/include/common.hpp"
 
 namespace ovsyannikov_n_star {
 
 OvsyannikovNStarMPI::OvsyannikovNStarMPI(const InType &in) {
-    SetTypeOfTask(GetStaticTypeOfTask());
-    GetInput() = in;
-    GetOutput() = 0;
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = 0;
 }
 
 bool OvsyannikovNStarMPI::ValidationImpl() {
-    // Проверка, что входных данных ровно 3 (src, dst, val)
-    return GetInput().size() == 3;
+  // Проверка, что входных данных ровно 3 (src, dst, val)
+  return GetInput().size() == 3;
 }
 
 bool OvsyannikovNStarMPI::PreProcessingImpl() {
-    GetOutput() = 0;
-    return true;
+  GetOutput() = 0;
+  return true;
 }
 
 bool OvsyannikovNStarMPI::RunImpl() {
-    int is_initialized = 0;
-    MPI_Initialized(&is_initialized);
-    if (!is_initialized) {
-        GetOutput() = GetInput()[2];
-        return true;
-    }
-
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    int src = GetInput()[0];
-    int dst = GetInput()[1];
-    int val = GetInput()[2];
-
-    // Если данные некорректны для текущего количества процессов
-    if (size <= 1 || src >= size || dst >= size || src < 0 || dst < 0) {
-        if (rank == 0) GetOutput() = val;
-        return true; 
-    }
-
-    int res = 0;
-    bool is_transit = (src != 0 && dst != 0);
-
-    if (rank == src) {
-        if (src == dst) {
-            res = val;
-        } else {
-            int next_hop = is_transit ? 0 : dst;
-            MPI_Send(&val, 1, MPI_INT, next_hop, 0, MPI_COMM_WORLD);
-        }
-    }
-
-    if (rank == 0 && is_transit) {
-        int tmp;
-        MPI_Recv(&tmp, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Send(&tmp, 1, MPI_INT, dst, 0, MPI_COMM_WORLD);
-    }
-
-    if (rank == dst && src != dst) {
-        int source_hop = is_transit ? 0 : src;
-        MPI_Recv(&res, 1, MPI_INT, source_hop, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-
-    GetOutput() = res;
-    // Рассылаем результат от dst всем остальным
-    MPI_Bcast(&GetOutput(), 1, MPI_INT, dst, MPI_COMM_WORLD);
-
+  int is_initialized = 0;
+  MPI_Initialized(&is_initialized);
+  if (!is_initialized) {
+    GetOutput() = GetInput()[2];
     return true;
+  }
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  int src = GetInput()[0];
+  int dst = GetInput()[1];
+  int val = GetInput()[2];
+
+  // Если данные некорректны для текущего количества процессов
+  if (size <= 1 || src >= size || dst >= size || src < 0 || dst < 0) {
+    if (rank == 0) {
+      GetOutput() = val;
+    }
+    return true;
+  }
+
+  int res = 0;
+  bool is_transit = (src != 0 && dst != 0);
+
+  if (rank == src) {
+    if (src == dst) {
+      res = val;
+    } else {
+      int next_hop = is_transit ? 0 : dst;
+      MPI_Send(&val, 1, MPI_INT, next_hop, 0, MPI_COMM_WORLD);
+    }
+  }
+
+  if (rank == 0 && is_transit) {
+    int tmp;
+    MPI_Recv(&tmp, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Send(&tmp, 1, MPI_INT, dst, 0, MPI_COMM_WORLD);
+  }
+
+  if (rank == dst && src != dst) {
+    int source_hop = is_transit ? 0 : src;
+    MPI_Recv(&res, 1, MPI_INT, source_hop, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+
+  GetOutput() = res;
+  // Рассылаем результат от dst всем остальным
+  MPI_Bcast(&GetOutput(), 1, MPI_INT, dst, MPI_COMM_WORLD);
+
+  return true;
 }
 
 bool OvsyannikovNStarMPI::PostProcessingImpl() {
-    return true;
+  return true;
 }
 
 }  // namespace ovsyannikov_n_star
--- tasks/ovsyannikov_n_star/tests/performance/main.cpp
+++ tasks/ovsyannikov_n_star/tests/performance/main.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
+
 #include <vector>
+
 #include "ovsyannikov_n_star/common/include/common.hpp"
 #include "ovsyannikov_n_star/mpi/include/ops_mpi.hpp"
 #include "ovsyannikov_n_star/seq/include/ops_seq.hpp"
@@ -10,7 +12,7 @@ namespace ovsyannikov_n_star {
 class OvsyannikovNRunPerfTestStar : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
   void SetUp() override {
-    input_data_ = {0, 0, 1000}; 
+    input_data_ = {0, 0, 1000};
     expected_output_ = 1000;
   }
 
@@ -36,7 +38,7 @@ const auto kAllPerfTasks =
     ppc::util::MakeAllPerfTasks<InType, OvsyannikovNStarMPI, OvsyannikovNStarSEQ>(PPC_SETTINGS_ovsyannikov_n_star);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
-INSTANTIATE_TEST_SUITE_P(StarPerfTests, OvsyannikovNRunPerfTestStar, kGtestValues, 
+INSTANTIATE_TEST_SUITE_P(StarPerfTests, OvsyannikovNRunPerfTestStar, kGtestValues,
                          OvsyannikovNRunPerfTestStar::CustomPerfTestName);
 
 }  // namespace ovsyannikov_n_star
