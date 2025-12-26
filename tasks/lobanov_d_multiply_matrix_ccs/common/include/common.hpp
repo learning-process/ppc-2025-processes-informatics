72da5a21236bd49443eed613bb137daf9ad5859e
yolage-stack <andr24367@mail.ru>
2025-12-26 09:39:11 +0300

    fix

--- tasks/lobanov_d_multiply_matrix_ccs/common/include/common.hpp
+++ tasks/lobanov_d_multiply_matrix_ccs/common/include/common.hpp
@@ -16,8 +16,54 @@ struct CompressedColumnMatrix {
   std::vector<int> row_index_data;
   std::vector<int> column_pointer_data;
 
-  CompressedColumnMatrix() : row_count(0), column_count(0), non_zero_count(0) {}
-  CompressedColumnMatrix(int r, int c, int nz) : row_count(r), column_count(c), non_zero_count(nz) {}
+  CompressedColumnMatrix() {
+    memset(this, 0, sizeof(CompressedColumnMatrix));
+    row_count = 0;
+    column_count = 0;
+    non_zero_count = 0;
+  }
+
+  CompressedColumnMatrix(const CompressedColumnMatrix &other) {
+    memset(this, 0, sizeof(CompressedColumnMatrix));
+    row_count = other.row_count;
+    column_count = other.column_count;
+    non_zero_count = other.non_zero_count;
+    value_data = other.value_data;
+    row_index_data = other.row_index_data;
+    column_pointer_data = other.column_pointer_data;
+  }
+
+  CompressedColumnMatrix &operator=(const CompressedColumnMatrix &other) {
+    if (this != &other) {
+      row_count = other.row_count;
+      column_count = other.column_count;
+      non_zero_count = other.non_zero_count;
+      value_data = other.value_data;
+      row_index_data = other.row_index_data;
+      column_pointer_data = other.column_pointer_data;
+    }
+    return *this;
+  }
+
+  ~CompressedColumnMatrix() = default;
+
+  bool IsValid() const {
+    if (row_count < 0 || column_count < 0 || non_zero_count < 0) {
+      return false;
+    }
+    if (non_zero_count > 0) {
+      if (value_data.size() != static_cast<size_t>(non_zero_count)) {
+        return false;
+      }
+      if (row_index_data.size() != static_cast<size_t>(non_zero_count)) {
+        return false;
+      }
+    }
+    if (column_pointer_data.size() != static_cast<size_t>(column_count + 1)) {
+      return false;
+    }
+    return true;
+  }
 };
 
 using InType = std::pair<CompressedColumnMatrix, CompressedColumnMatrix>;
