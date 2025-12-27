a35b7557907933ef9d8a3e6001400981a6ed69db
Pylaeva Svetlana <pl.svetlanaa.a@gmail.com>
2025-12-27 17:24:50 +0300

    add type in common

--- tasks/pylaeva_s_convex_hull_bin/common/include/common.hpp
+++ tasks/pylaeva_s_convex_hull_bin/common/include/common.hpp
@@ -1,14 +1,37 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <tuple>
+#include <vector>
 
 #include "task/include/task.hpp"
 
 namespace pylaeva_s_convex_hull_bin {
+struct Point {
+  int x, y;
+  explicit Point(int x = 0, int y = 0) : x(x), y(y) {}
+  bool operator==(const Point &other) const {
+    return x == other.x && y == other.y;
+  }
+  bool operator!=(const Point &other) const {
+    return !(*this == other);
+  }
+  bool operator<(const Point &other) const {
+    return std::tie(x, y) < std::tie(other.x, other.y);
+  }
+};
 
-using InType = int;
-using OutType = int;
+struct ImageData {
+  int width{0};
+  int height{0};
+  std::vector<uint8_t> pixels;
+  std::vector<std::vector<Point>> components;
+  std::vector<std::vector<Point>> convex_hulls;
+};
+
+using InType = ImageData;
+using OutType = ImageData;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
