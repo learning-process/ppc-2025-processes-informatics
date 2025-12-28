58dae242d88667f913fa43028d54515c287787c2
kodt2 <kodt2005@yandex.ru>
2025-12-28 21:09:13 +0300

    func

--- tasks/chaschin_v_sobel_operator/mpi/include/ops_mpi.hpp
+++ tasks/chaschin_v_sobel_operator/mpi/include/ops_mpi.hpp
@@ -22,10 +22,9 @@ class ChaschinVSobelOperatorMPI : public BaseTask {
   bool PostProcessingImpl() override;
 
   std::vector<float> PreprocessToGrayscaleWithOverlap(const std::vector<std::vector<Pixel>> &image, int n_procs,
-    std::vector<int> &sendcounts, std::vector<int> &displs);
+                                                      std::vector<int> &sendcounts, std::vector<int> &displs);
 
-    float sobel_at(const std::vector<float> &img, int i, int j, int stride)
-  std::vector<float> PreProcessGray;
+  float sobel_at(const std::vector<float> &img, int i, int j, int stride) std::vector<float> PreProcessGray;
   std::vector<float> PostProcessGray;
   std::tuple<int, int> Size;
   std::vector<int> ScatterSendCounts;
