e40b0c5a971dc2c6e66966efa70f899bd15335fb
slaver07 <slavutich05@gmail.com>
2025-12-28 16:24:41 +0000

    refactor: simplify SEQ implementation to a stub for 100% coverage

--- tasks/dolov_v_torus_topology/seq/include/ops_seq.hpp
+++ tasks/dolov_v_torus_topology/seq/include/ops_seq.hpp
@@ -12,17 +12,14 @@ class DolovVTorusTopologySEQ : public BaseTask {
   }
   explicit DolovVTorusTopologySEQ(InType in);
 
- private:
   bool ValidationImpl() override;
   bool PreProcessingImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
-  static void DefineGridDimensions(int total_procs, int &r, int &c);
-  static int GetNextNode(int current, int target, int r, int c);
-
-  InputData internal_input_;
-  OutputData internal_output_;
+ private:
+  InType internal_input_;
+  OutType internal_output_;
 };
 
 }  // namespace dolov_v_torus_topology
