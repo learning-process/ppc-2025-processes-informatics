5adde796c0c3566000a268702241ce9273cac56b
kodt2 <kodt2005@yandex.ru>
2025-12-28 19:40:32 +0300

    func tests

--- tasks/chaschin_v_sobel_operator/tests/performance/main.cpp
+++ tasks/chaschin_v_sobel_operator/tests/performance/main.cpp
@@ -16,37 +16,35 @@ class ChaschinVRunPerfTestProcessesSO : public ppc::util::BaseRunPerfTests<InTyp
   InType input_data;
 
   void SetUp() override {
+     // Создаем квадратное изображение размером k_count x k_count
     input_data.resize(k_count);
     for (int i = 0; i < k_count; ++i) {
-      input_data[i].resize(k_count);
-    }
-
-    for (int i = 0; i < k_count; i++) {
-      for (int j = 0; j < k_count; j++) {
-        input_data[i][j] = static_cast<float>((i + 1) * (j + 2));
-      }
+        input_data[i].resize(k_count);
+        for (int j = 0; j < k_count; ++j) {
+            // Детерминированное значение
+            input_data[i][j] = static_cast<float>((i + 1) * (j + 2));
+        }
     }
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
+    // Проверка размера
     if (output_data.size() != input_data.size()) {
-      return false;
-    }
-
-    for (size_t i = 0; i < output_data.size(); i++) {
-      if (output_data[i] != *std::max_element(input_data[i].begin(), input_data[i].end())) {
         return false;
-      }
     }
-    return true;
-  }
 
-  InType GetTestInputData() final {
-    return input_data;
-  }
+    // Проверка значений: каждая строка должна содержать максимум строки
+    for (size_t i = 0; i < output_data.size(); ++i) {
+        float expected_max = *std::max_element(input_data[i].begin(), input_data[i].end());
+        if (output_data[i] != expected_max) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
-TEST_P(ChaschinVRunPerfTestProcessesSO, RunPerfModes) {
+TEST_P(ChaschinVRunPerfTestProcessesSO, RunPerfModesSO) {
   ExecuteTest(GetParam());
 }
 
