6cccf1b5721c93a2b52d5a28c88a1b8cb3da6d0e
AlenaK13 <kruglovatm@mail.ru>
2025-12-29 02:31:32 +0300

    clang-tidy1.2

--- tasks/kruglova_a_2d_multistep_par_opt/mpi/include/ops_mpi.hpp
+++ tasks/kruglova_a_2d_multistep_par_opt/mpi/include/ops_mpi.hpp
@@ -20,7 +20,7 @@ class KruglovaA2DMuitMPI : public BaseTask {
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
-  double Solve1DSequential(const std::function<double(double)> &func, double a, double b, double eps, int max_iters);
+  double Solve1DSequential(std::function<double(double)> &func, double a, double b, double eps, int max_iters);
 };
 
 }  // namespace kruglova_a_2d_multistep_par_opt
