0044e6671b545df8c792f6fa59443d69cf795b58
Andrew-Kop <144238394+cuttty@users.noreply.github.com>
2025-12-29 03:52:13 +0300

    fix for tests

--- tasks/frolova_s_mult_int_trapez/mpi/src/ops_mpi.cpp
+++ tasks/frolova_s_mult_int_trapez/mpi/src/ops_mpi.cpp
@@ -30,11 +30,12 @@ void FrolovaSMultIntTrapezMPI::Recursive(std::vector<double> &point, unsigned in
     Recursive(point, definition, divider * (number_of_intervals_[variable] + 1), variable - 1);
   }
 
-  // Защита от деления на 0 и пустых векторов
-  if (variable < limits_.size() && number_of_intervals_[variable] != 0) {
+  if (number_of_intervals_[variable] != 0) {
     point[variable] = limits_[variable].first + static_cast<double>(definition / divider) *
                                                     (limits_[variable].second - limits_[variable].first) /
                                                     number_of_intervals_[variable];
+  } else {
+    point[variable] = limits_[variable].first;
   }
 
   definition = definition % divider;
@@ -52,8 +53,9 @@ std::vector<double> FrolovaSMultIntTrapezMPI::GetPointFromNumber(unsigned int nu
 bool FrolovaSMultIntTrapezMPI::ValidationImpl() {
   int rank = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
   if (rank == 0) {
-    return !GetInput().limits.empty() && GetInput().number_of_intervals.size() == GetInput().limits.size();
+    return !limits_.empty() && limits_.size() == number_of_intervals_.size();
   }
   return true;
 }
@@ -62,8 +64,6 @@ bool FrolovaSMultIntTrapezMPI::PreProcessingImpl() {
   int rank = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   if (rank == 0) {
-    limits_ = GetInput().limits;
-    number_of_intervals_ = GetInput().number_of_intervals;
     result_ = 0.0;
   }
   return true;
@@ -75,6 +75,7 @@ bool FrolovaSMultIntTrapezMPI::RunImpl() {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+  // Синхронизируем входные данные между всеми процессами
   int limits_size = static_cast<int>(limits_.size());
   MPI_Bcast(&limits_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
@@ -92,7 +93,7 @@ bool FrolovaSMultIntTrapezMPI::RunImpl() {
 
   unsigned long long total_points = 1;
   for (unsigned int val : number_of_intervals_) {
-    total_points *= (val + 1);
+    total_points *= (static_cast<unsigned long long>(val) + 1);
   }
 
   unsigned int u_size = static_cast<unsigned int>(size);
@@ -101,19 +102,21 @@ bool FrolovaSMultIntTrapezMPI::RunImpl() {
 
   unsigned int local_count = base_delta + (static_cast<unsigned int>(rank) < remainder ? 1 : 0);
   unsigned int local_start =
-      static_cast<unsigned int>(rank) * base_delta + std::min(static_cast<unsigned int>(rank), remainder);
+      static_cast<unsigned int>(rank) * base_delta + (static_cast<unsigned int>(rank) < remainder ? rank : remainder);
 
   double local_result = 0.0;
   for (unsigned int i = 0; i < local_count; i++) {
     std::vector<double> point = GetPointFromNumber(local_start + i);
-    local_result += CalculationOfCoefficient(point) * GetInput().function(point);
+    local_result += static_cast<double>(CalculationOfCoefficient(point)) * GetInput().function(point);
   }
 
   MPI_Reduce(&local_result, &result_, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
   if (rank == 0) {
     for (unsigned int i = 0; i < limits_.size(); i++) {
-      result_ *= (limits_[i].second - limits_[i].first) / number_of_intervals_[i];
+      if (number_of_intervals_[i] != 0) {
+        result_ *= (limits_[i].second - limits_[i].first) / number_of_intervals_[i];
+      }
     }
     result_ /= std::pow(2, limits_.size());
   }
