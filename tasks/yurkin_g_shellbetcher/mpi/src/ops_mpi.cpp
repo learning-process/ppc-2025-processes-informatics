a761b4eba2ac3205fdce8a5d6daa357192269f84
georghinho <yurkin23egor@yandex.ru>
2025-12-29 22:11:47 +0300

    try 13

--- tasks/yurkin_g_shellbetcher/mpi/src/ops_mpi.cpp
+++ tasks/yurkin_g_shellbetcher/mpi/src/ops_mpi.cpp
@@ -7,7 +7,6 @@
 #include <cstddef>
 #include <cstdint>
 #include <random>
-#include <ranges>
 #include <vector>
 
 #include "yurkin_g_shellbetcher/common/include/common.hpp"
@@ -37,7 +36,7 @@ void ShellSort(std::vector<int> &a) {
 
 void OddEvenBatcherMergeLocal(const std::vector<int> &a, const std::vector<int> &b, std::vector<int> &out) {
   out.resize(a.size() + b.size());
-  std::ranges::merge(a, b, out.begin());
+  std::merge(a.begin(), a.end(), b.begin(), b.end(), out.begin());
   for (int phase = 0; phase < 2; ++phase) {
     auto start = static_cast<std::size_t>(phase);
     for (std::size_t i = start; i + 1 < out.size(); i += 2) {
