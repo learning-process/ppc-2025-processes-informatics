921e4db4fa43df35d602097af335ba6c529df0f5
Offsidetax <dspichek@bk.ru>
2025-12-30 17:26:22 +0300

    Added more functional tests

--- tasks/spichek_d_jacobi/tests/functional/main.cpp
+++ tasks/spichek_d_jacobi/tests/functional/main.cpp
@@ -52,11 +52,41 @@ TEST_P(SpichekDJacobiRunFuncTestsProcesses, JacobiTest) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 2> kTestParam = {
+const std::array<TestType, 7> kTestParam = {
+    // 1. Базовый тест 2x2
     std::make_tuple(InType{{{10.0, -1.0}, {-1.0, 10.0}}, {9.0, 9.0}, 1e-6, 50}, "simple_2x2"),
 
+    // 2. Базовый тест 3x3
     std::make_tuple(InType{{{4.0, 1.0, 1.0}, {1.0, 4.0, 1.0}, {1.0, 1.0, 4.0}}, {6.0, 6.0, 6.0}, 1e-6, 100},
-                    "simple_3x3")};
+                    "simple_3x3"),
+
+    // 3. Минимальный тест 1x1
+    std::make_tuple(InType{{{10.0}}, {20.0}, 1e-6, 10}, "tiny_1x1"),
+
+    // 4. Единичная матрица 3x3
+    std::make_tuple(InType{{{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}}, {1.5, -2.5, 3.0}, 1e-6, 100},
+                    "identity_3x3"),
+
+    // 5. Тест с нулевым решением
+    std::make_tuple(InType{{{5.0, 1.0}, {1.0, 5.0}}, {0.0, 0.0}, 1e-6, 50}, "zero_solution"),
+
+    // 6. Матрица 4x4
+    std::make_tuple(InType{{{5.0, 1.0, 0.0, 0.0}, {1.0, 5.0, 1.0, 0.0}, {0.0, 1.0, 5.0, 1.0}, {0.0, 0.0, 1.0, 5.0}},
+                           {6.0, 7.0, 7.0, 6.0},
+                           1e-6,
+                           100},
+                    "tridiagonal_4x4"),
+
+    // 7. Матрица 5x5
+    std::make_tuple(InType{{{10.0, 1.0, 0.0, 0.0, 0.0},
+                            {1.0, 10.0, 1.0, 0.0, 0.0},
+                            {0.0, 1.0, 10.0, 1.0, 0.0},
+                            {0.0, 0.0, 1.0, 10.0, 1.0},
+                            {0.0, 0.0, 0.0, 1.0, 10.0}},
+                           {11.0, 12.0, 12.0, 12.0, 11.0},
+                           1e-6,
+                           100},
+                    "band_5x5")};
 
 const auto kTestTasksList =
     std::tuple_cat(ppc::util::AddFuncTask<SpichekDJacobiMPI, InType>(kTestParam, PPC_SETTINGS_spichek_d_jacobi),
