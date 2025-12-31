aca9ddd13505360cd4ad63fd01143c4ddf3f0db3
AlenaK13 <kruglovatm@mail.ru>
2025-12-31 13:16:44 +0300

    clang-tidy1.12

--- tasks/kruglova_a_2d_multistep_par_opt/mpi/src/ops_mpi.cpp
+++ tasks/kruglova_a_2d_multistep_par_opt/mpi/src/ops_mpi.cpp
@@ -8,7 +8,6 @@
 #include <cstddef>
 #include <functional>
 #include <limits>
-#include <utility>
 #include <vector>
 
 #include "kruglova_a_2d_multistep_par_opt/common/include/common.hpp"
@@ -37,27 +36,12 @@ struct CharIdx {
   size_t idx;
 };
 
-size_t FindBestIntervalIdx(const std::vector<Trial1D> &trials, double m_scaled) {
-  double max_rate = -std::numeric_limits<double>::infinity();
-  size_t best_idx = 0;
-  for (size_t i = 0; i + 1 < trials.size(); ++i) {
-    const double dx = trials[i + 1].x - trials[i].x;
-    const double dz = trials[i + 1].z - trials[i].z;
-    const double rate = (m_scaled * dx) + ((dz * dz) / (m_scaled * dx)) - (2.0 * (trials[i + 1].z + trials[i].z));
-    if (rate > max_rate) {
-      max_rate = rate;
-      best_idx = i;
-    }
-  }
-  return best_idx;
-}
-
 double CalculateM1D(const std::vector<Trial1D> &trials) {
   double m_max = 0.0;
   for (size_t i = 0; i + 1 < trials.size(); ++i) {
-    const double dx = trials[i + 1].x - trials[i].x;
+    double dx = trials[i + 1].x - trials[i].x;
+    double dz = std::abs(trials[i + 1].z - trials[i].z);
     if (dx > 1e-15) {
-      const double dz = std::abs(trials[i + 1].z - trials[i].z);
       m_max = std::max(m_max, dz / dx);
     }
   }
@@ -73,47 +57,76 @@ double Solve1DStrongin(const std::function<double(double)> &func, double a, doub
   }
 
   for (int iter = 0; iter < max_iters; ++iter) {
-    const double m_val = CalculateM1D(trials);
-    const double m_scaled = (m_val > 0.0) ? (r_param * m_val) : 1.0;
+    double m_val = CalculateM1D(trials);
+    double m_scaled = (m_val > 0.0) ? (r_param * m_val) : 1.0;
 
-    const size_t b_idx = FindBestIntervalIdx(trials, m_scaled);
-    if ((trials[b_idx + 1].x - trials[b_idx].x) < eps) {
+    double max_rate = -std::numeric_limits<double>::infinity();
+    size_t best_idx = 0;
+
+    for (size_t i = 0; i + 1 < trials.size(); ++i) {
+      double dx = trials[i + 1].x - trials[i].x;
+      double dz = trials[i + 1].z - trials[i].z;
+      double rate = (m_scaled * dx) + ((dz * dz) / (m_scaled * dx)) - (2.0 * (trials[i + 1].z + trials[i].z));
+      if (rate > max_rate) {
+        max_rate = rate;
+        best_idx = i;
+      }
+    }
+
+    if ((trials[best_idx + 1].x - trials[best_idx].x) < eps) {
       break;
     }
 
-    const double x_new =
-        (0.5 * (trials[b_idx + 1].x + trials[b_idx].x)) - ((trials[b_idx + 1].z - trials[b_idx].z) / (2.0 * m_scaled));
+    double x_new = (0.5 * (trials[best_idx + 1].x + trials[best_idx].x)) -
+                   ((trials[best_idx + 1].z - trials[best_idx].z) / (2.0 * m_scaled));
 
     Trial1D new_trial{.x = x_new, .z = func(x_new)};
-    auto it = std::lower_bound(trials.begin(), trials.end(), new_trial,
-                               [](const Trial1D &t1, const Trial1D &t2) { return t1.x < t2.x; });
-    trials.insert(it, new_trial);
+
+    size_t pos = 0;
+    while (pos < trials.size() && trials[pos].x < new_trial.x) {
+      ++pos;
+    }
+    trials.insert(trials.begin() + static_cast<std::ptrdiff_t>(pos), new_trial);
   }
 
-  auto best_it =
-      std::min_element(trials.begin(), trials.end(), [](const Trial1D &t1, const Trial1D &t2) { return t1.z < t2.z; });
-  best_x = best_it->x;
-  return best_it->z;
+  size_t best_idx = 0;
+  for (size_t i = 1; i < trials.size(); ++i) {
+    if (trials[i].z < trials[best_idx].z) {
+      best_idx = i;
+    }
+  }
+
+  best_x = trials[best_idx].x;
+  return trials[best_idx].z;
 }
 
 void MasterCalculateIntervals(const std::vector<Trial2D> &trials, std::vector<IntervalData> &selected, int size,
                               double eps, int &stop_flag) {
   double m_max = 0.0;
   for (size_t i = 0; i + 1 < trials.size(); ++i) {
-    const double dx = trials[i + 1].x - trials[i].x;
+    double dx = trials[i + 1].x - trials[i].x;
     m_max = std::max(m_max, std::abs(trials[i + 1].f - trials[i].f) / dx);
   }
 
-  const double m_val = (m_max > 0.0) ? (2.0 * m_max) : 1.0;
+  double m_val = (m_max > 0.0) ? (2.0 * m_max) : 1.0;
   std::vector<CharIdx> rates;
+
   for (size_t i = 0; i + 1 < trials.size(); ++i) {
-    const double dx = trials[i + 1].x - trials[i].x;
-    const double df = trials[i + 1].f - trials[i].f;
-    const double r_val = (m_val * dx) + ((df * df) / (m_val * dx)) - (2.0 * (trials[i + 1].f + trials[i].f));
+    double dx = trials[i + 1].x - trials[i].x;
+    double df = trials[i + 1].f - trials[i].f;
+    double r_val = (m_val * dx) + ((df * df) / (m_val * dx)) - (2.0 * (trials[i + 1].f + trials[i].f));
     rates.push_back({r_val, i});
   }
 
-  std::sort(rates.begin(), rates.end(), [](const CharIdx &a, const CharIdx &b) { return a.r_val > b.r_val; });
+  for (size_t i = 0; i < rates.size(); ++i) {
+    size_t max_idx = i;
+    for (size_t j = i + 1; j < rates.size(); ++j) {
+      if (rates[j].r_val > rates[max_idx].r_val) {
+        max_idx = j;
+      }
+    }
+    std::swap(rates[i], rates[max_idx]);
+  }
 
   if (rates.empty() || (trials[rates[0].idx + 1].x - trials[rates[0].idx].x) < eps) {
     stop_flag = 1;
@@ -121,46 +134,71 @@ void MasterCalculateIntervals(const std::vector<Trial2D> &trials, std::vector<In
   }
 
   for (int i = 0; i < size; ++i) {
-    const size_t s_idx = (static_cast<size_t>(i) < rates.size()) ? rates[static_cast<size_t>(i)].idx : rates[0].idx;
-    selected[static_cast<size_t>(i)] = {
-        .x1 = trials[s_idx].x, .x2 = trials[s_idx + 1].x, .f1 = trials[s_idx].f, .f2 = trials[s_idx + 1].f};
+    size_t s_idx = (static_cast<size_t>(i) < rates.size()) ? rates[static_cast<size_t>(i)].idx : rates[0].idx;
+    selected[static_cast<size_t>(i)] = {trials[s_idx].x, trials[s_idx + 1].x, trials[s_idx].f, trials[s_idx + 1].f};
   }
 }
 
 std::vector<Trial2D> InitTrials(const InType &in, int init_points) {
   std::vector<Trial2D> trials;
+
   for (int i = 0; i < init_points; ++i) {
-    const double t = static_cast<double>(i) / static_cast<double>(init_points - 1);
-    const double x = in.x_min + ((in.x_max - in.x_min) * t);
+    double x = in.x_min + ((in.x_max - in.x_min) * static_cast<double>(i) / static_cast<double>(init_points - 1));
+
     double y_best = 0.0;
-    const double f = Solve1DStrongin([&](double y) { return ObjectiveFunction(x, y); }, in.y_min, in.y_max, in.eps,
-                                     std::max(20, in.max_iters / 20), y_best);
-    trials.push_back({.x = x, .y = y_best, .f = f});
+    double f = Solve1DStrongin([&](double y) { return ObjectiveFunction(x, y); }, in.y_min, in.y_max, in.eps,
+                               std::max(20, in.max_iters / 20), y_best);
+
+    trials.push_back({x, y_best, f});
   }
-  std::sort(trials.begin(), trials.end(), [](const Trial2D &a, const Trial2D &b) { return a.x < b.x; });
+
+  for (size_t i = 0; i < trials.size(); ++i) {
+    size_t min_idx = i;
+    for (size_t j = i + 1; j < trials.size(); ++j) {
+      if (trials[j].x < trials[min_idx].x) {
+        min_idx = j;
+      }
+    }
+    std::swap(trials[i], trials[min_idx]);
+  }
+
   return trials;
 }
 
 Trial2D ComputeLocalTrial(const IntervalData &interval, const InType &in) {
-  const double dx = interval.x2 - interval.x1;
-  double m_loc = std::abs(interval.f2 - interval.f1) / dx;
-  m_loc = (m_loc > 0.0) ? (2.0 * m_loc) : 1.0;
+  double dx = interval.x2 - interval.x1;
+  double m = std::abs(interval.f2 - interval.f1) / dx;
+  m = (m > 0.0) ? (2.0 * m) : 1.0;
 
-  const double x_new = (0.5 * (interval.x1 + interval.x2)) - ((interval.f2 - interval.f1) / (2.0 * m_loc));
+  double x_new = (0.5 * (interval.x1 + interval.x2)) - ((interval.f2 - interval.f1) / (2.0 * m));
+
   double y_res = 0.0;
-  const double f_res = Solve1DStrongin([&](double y) { return ObjectiveFunction(x_new, y); }, in.y_min, in.y_max,
-                                       in.eps, std::max(25, in.max_iters / 20), y_res);
-  return {.x = x_new, .y = y_res, .f = f_res};
+  double f_res = Solve1DStrongin([&](double y) { return ObjectiveFunction(x_new, y); }, in.y_min, in.y_max, in.eps,
+                                 std::max(25, in.max_iters / 20), y_res);
+
+  return {x_new, y_res, f_res};
 }
 
 void UpdateTrialsOnMaster(std::vector<Trial2D> &trials, const std::vector<Trial2D> &new_trials) {
   for (const auto &res : new_trials) {
-    auto it = std::lower_bound(trials.begin(), trials.end(), res,
-                               [](const Trial2D &a, const Trial2D &b) { return a.x < b.x; });
-    if (it == trials.end() || std::abs(it->x - res.x) > 1e-12) {
-      trials.insert(it, res);
+    size_t pos = 0;
+    while (pos < trials.size() && trials[pos].x < res.x) {
+      ++pos;
+    }
+    if (pos == trials.size() || std::abs(trials[pos].x - res.x) > 1e-12) {
+      trials.insert(trials.begin() + static_cast<std::ptrdiff_t>(pos), res);
     }
   }
+}
+
+Trial2D FindBestTrial(const std::vector<Trial2D> &trials) {
+  size_t best_idx = 0;
+  for (size_t i = 1; i < trials.size(); ++i) {
+    if (trials[i].f < trials[best_idx].f) {
+      best_idx = i;
+    }
+  }
+  return trials[best_idx];
 }
 
 }  // namespace
@@ -209,26 +247,25 @@ bool KruglovaA2DMuitMPI::RunImpl() {
     MPI_Scatter(selected_intervals.data(), sizeof(IntervalData), MPI_BYTE, &my_interval, sizeof(IntervalData), MPI_BYTE,
                 0, MPI_COMM_WORLD);
 
-    Trial2D local_t = ComputeLocalTrial(my_interval, in);
-    std::array<double, 3> send_res = {local_t.x, local_t.y, local_t.f};
+    Trial2D local_trial = ComputeLocalTrial(my_interval, in);
+    std::array<double, 3> send_res = {local_trial.x, local_trial.y, local_trial.f};
     std::vector<double> recv_res(static_cast<size_t>(size) * 3);
     MPI_Gather(send_res.data(), 3, MPI_DOUBLE, recv_res.data(), 3, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-      std::vector<Trial2D> next_trials;
+      std::vector<Trial2D> new_trials;
       for (int i = 0; i < size; ++i) {
-        size_t b = static_cast<size_t>(i) * 3;
-        next_trials.push_back({.x = recv_res[b], .y = recv_res[b + 1], .f = recv_res[b + 2]});
+        size_t base = static_cast<size_t>(i) * 3;
+        new_trials.push_back({.x = recv_res[base], .y = recv_res[base + 1], .f = recv_res[base + 2]});
       }
-      UpdateTrialsOnMaster(trials, next_trials);
+      UpdateTrialsOnMaster(trials, new_trials);
     }
   }
 
   std::array<double, 3> final_res = {0.0, 0.0, 0.0};
   if (rank == 0) {
-    auto best_it =
-        std::min_element(trials.begin(), trials.end(), [](const Trial2D &a, const Trial2D &b) { return a.f < b.f; });
-    final_res = {best_it->x, best_it->y, best_it->f};
+    Trial2D best = FindBestTrial(trials);
+    final_res = {best.x, best.y, best.f};
   }
 
   MPI_Bcast(final_res.data(), 3, MPI_DOUBLE, 0, MPI_COMM_WORLD);
