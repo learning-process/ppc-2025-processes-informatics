1a02da39e37b4ac93f90195adc0dd4b0079f1ebf
Korobokkk <bbb754748@gmail.com>
2026-01-01 02:50:53 +0300

    clang-format-_-

--- tasks/kiselev_i_gauss_method_horizontal_tape_scheme/tests/functional/main.cpp
+++ tasks/kiselev_i_gauss_method_horizontal_tape_scheme/tests/functional/main.cpp
@@ -145,11 +145,10 @@ const std::array<TestType, 14> kTestParam = {
                            std::vector<double>{5.0, 6.0, 6.0, 6.0, 6.0, 5.0}, std::size_t{1}},
                     "laplacian_1d_6")};
 
-const auto ktesttaskslist =
-    std::tuple_cat(ppc::util::AddFuncTask<KiselevITestTaskMPI, InType>(
-                       kTestParam, PPC_SETTINGS_kiselev_i_gauss_method_horizontal_tape_scheme),
-                   ppc::util::AddFuncTask<KiselevITestTaskSEQ, InType>(
-                       kTestParam, PPC_SETTINGS_kiselev_i_gauss_method_horizontal_tape_scheme));
+const auto ktesttaskslist = std::tuple_cat(ppc::util::AddFuncTask<KiselevITestTaskMPI, InType>(
+                                               kTestParam, PPC_SETTINGS_kiselev_i_gauss_method_horizontal_tape_scheme),
+                                           ppc::util::AddFuncTask<KiselevITestTaskSEQ, InType>(
+                                               kTestParam, PPC_SETTINGS_kiselev_i_gauss_method_horizontal_tape_scheme));
 
 const auto kGTestValues = ppc::util::ExpandToValues(ktesttaskslist);
 const auto kPerfTestName = KiselevIRunFuncTestsProcesses2::PrintFuncTestName<KiselevIRunFuncTestsProcesses2>;
