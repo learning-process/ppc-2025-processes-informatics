eae9fd1a8cf685d68e2af98f68e81c08e61fd5f0
cuttty <frolova.sofya.05@mail.ru>
2026-01-01 23:14:05 +0300

    fix for perf test

--- tasks/frolova_s_mult_int_trapez/mpi/src/ops_mpi.cpp
+++ tasks/frolova_s_mult_int_trapez/mpi/src/ops_mpi.cpp
@@ -17,7 +17,7 @@ FrolovaSMultIntTrapezMPI::FrolovaSMultIntTrapezMPI(const InType &in)
   std::cout << "[MPI CONSTRUCTOR] Input limits size: " << in.limits.size() << std::endl;
   std::cout << "[MPI CONSTRUCTOR] Input intervals size: " << in.number_of_intervals.size() << std::endl;
   std::cout << "[MPI CONSTRUCTOR] Function is " << (in.function ? "NOT null" : "NULL") << std::endl;
-
+  SetTypeOfTask(GetStaticTypeOfTask());
   GetInput() = in;
 }
 
