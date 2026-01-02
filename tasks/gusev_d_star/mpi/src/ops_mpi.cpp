27921c006f04e28242b5e96921c50ea11d64b2f6
userskins <userskins@gmail.com>
2026-01-02 04:31:37 +0300

    Добавление файлов работы

---
+++ tasks/gusev_d_star/mpi/src/ops_mpi.cpp
@@ -0,0 +1,113 @@
+#include "gusev_d_star/mpi/include/ops_mpi.hpp"
+
+#include <mpi.h>
+#include <algorithm>
+#include <vector>
+
+namespace gusev_d_star {
+
+const int kTagTask = 0;
+const int kTagResult = 1;
+
+GusevDStarMPI::GusevDStarMPI(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = 0;
+}
+
+bool GusevDStarMPI::ValidationImpl() {
+  return (GetInput() > 0);
+}
+
+bool GusevDStarMPI::PreProcessingImpl() {
+  GetOutput() = 2 * GetInput();
+  return true;
+}
+
+bool GusevDStarMPI::RunImpl() {
+  auto input = GetInput();
+  int rank = 0;
+  int size = 0;
+  
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  if (size == 1) {
+    volatile int local_res = 0;
+    for (int i = 0; i < input; i++) {
+        for (int j = 0; j < input; j++) {
+            for (int k = 0; k < input; k++) {
+                local_res += (i + j + k);
+                local_res -= (i + j + k);
+            }
+        }
+    }
+    GetOutput() += local_res;
+    return true;
+  }
+
+  const int kHubRank = 0;
+
+  if (rank == kHubRank) {
+    RunAsMaster(size, input);
+  } else {
+    RunAsWorker(input);
+  }
+
+  return true;
+}
+
+bool GusevDStarMPI::PostProcessingImpl() {
+  GetOutput() -= GetInput();
+  return true;
+}
+
+void GusevDStarMPI::RunAsMaster(int size, int input) {
+    int workers_count = size - 1;
+    int chunk = input / workers_count;
+    int remainder = input % workers_count;
+    int current_start_index = 0;
+
+    for (int dst_rank = 1; dst_rank < size; dst_rank++) {
+        int count = chunk + (dst_rank <= remainder ? 1 : 0);
+        int task_data[2] = {current_start_index, count};
+        
+        MPI_Send(task_data, 2, MPI_INT, dst_rank, kTagTask, MPI_COMM_WORLD);
+        current_start_index += count;
+    }
+
+    for (int src_rank = 1; src_rank < size; src_rank++) {
+        int worker_result = 0;
+        MPI_Status status;
+        
+        MPI_Recv(&worker_result, 1, MPI_INT, src_rank, kTagResult, MPI_COMM_WORLD, &status);
+        GetOutput() += worker_result;
+    }
+}
+
+void GusevDStarMPI::RunAsWorker(int input) {
+    int task_data[2];
+    MPI_Status status;
+    const int kHubRank = 0;
+
+    MPI_Recv(task_data, 2, MPI_INT, kHubRank, kTagTask, MPI_COMM_WORLD, &status);
+    
+    int start_i = task_data[0];
+    int count_i = task_data[1];
+    int end_i = start_i + count_i;
+
+    volatile int local_res = 0;
+    
+    for (int i = start_i; i < end_i; i++) {
+        for (int j = 0; j < input; j++) {
+            for (int k = 0; k < input; k++) {
+                local_res += (i + j + k);
+                local_res -= (i + j + k);
+            }
+        }
+    }
+
+    MPI_Send((void*)&local_res, 1, MPI_INT, kHubRank, kTagResult, MPI_COMM_WORLD);
+}
+
+}  // namespace gusev_d_star
---
+++ tasks/gusev_d_star/seq/src/ops_seq.cpp
@@ -0,0 +1,46 @@
+#include "gusev_d_star/seq/include/ops_seq.hpp"
+
+#include <vector>
+
+namespace gusev_d_star {
+
+GusevDStarSEQ::GusevDStarSEQ(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = 0;
+}
+
+bool GusevDStarSEQ::ValidationImpl() {
+  return (GetInput() > 0);
+}
+
+bool GusevDStarSEQ::PreProcessingImpl() {
+  GetOutput() = 2 * GetInput();
+  return true;
+}
+
+bool GusevDStarSEQ::RunImpl() {
+  auto n = GetInput();
+  if (n == 0) return false;
+
+  volatile int local_res = 0;
+  
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      for (int k = 0; k < n; k++) {
+        local_res += (i + j + k);
+        local_res -= (i + j + k);
+      }
+    }
+  }
+  
+  GetOutput() += local_res;
+  return true;
+}
+
+bool GusevDStarSEQ::PostProcessingImpl() {
+  GetOutput() -= GetInput();
+  return true;
+}
+
+}  // namespace gusev_d_star
