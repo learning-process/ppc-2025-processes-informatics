d6f7e2b4c681003c35121d22f69d10db8e11ba2d
brb484ul <pppphp2800@gmail.com>
2026-01-02 17:45:07 +0300

    fix tests 3.0

--- tasks/khruev_a_global_opt/tests/performance/main.cpp
+++ tasks/khruev_a_global_opt/tests/performance/main.cpp
@@ -15,10 +15,10 @@ class KhruevAGlobalOptPerfTests : public ppc::util::BaseRunPerfTests<InType, Out
 
   void SetUp() override {
     input_data_.func_id = 1;
-    input_data_.ax = -5.0;
-    input_data_.bx = 5.0;
-    input_data_.ay = -5.0;
-    input_data_.by = 5.0;
+    input_data_.ax = -8.0;
+    input_data_.bx = 8.0;
+    input_data_.ay = -8.0;
+    input_data_.by = 8.0;
 
     input_data_.epsilon = 1e-7;
     input_data_.max_iter = 1000;
