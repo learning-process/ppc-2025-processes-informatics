eecd6c51a15ec3fad90694c57848807b266b8ef2
Ksunyy <xenchik01042005@gmail.com>
2026-01-02 14:07:37 +0300

    fix

--- tasks/shvetsova_k_rad_sort_batch_merge/mpi/include/ops_mpi.hpp
+++ tasks/shvetsova_k_rad_sort_batch_merge/mpi/include/ops_mpi.hpp
@@ -13,7 +13,7 @@ class ShvetsovaKRadSortBatchMergeMPI : public BaseTask {
     return ppc::task::TypeOfTask::kMPI;
   }
 
-  explicit ShvetsovaKRadSortBatchMergeMPI(const InType& in);
+  explicit ShvetsovaKRadSortBatchMergeMPI(const InType &in);
 
  private:
   std::vector<double> data_;
@@ -24,15 +24,12 @@ class ShvetsovaKRadSortBatchMergeMPI : public BaseTask {
   bool PostProcessingImpl() override;
 
   // ===== ДОП ФУНКЦИИ =====
-  static void CreateDistribution(int proc_count, int size,
-                                 std::vector<int>& counts,
-                                 std::vector<int>& displs,
+  static void CreateDistribution(int proc_count, int size, std::vector<int> &counts, std::vector<int> &displs,
                                  int rank);
 
-  static void RadixSortLocal(std::vector<double>& vec);
+  static void RadixSortLocal(std::vector<double> &vec);
 
-  static void OddEvenMergeStep(std::vector<double>& local,
-                               int rank, int proc_count);
+  static void OddEvenMergeStep(std::vector<double> &local, int rank, int proc_count);
 };
 
 }  // namespace shvetsova_k_rad_sort_batch_merge
