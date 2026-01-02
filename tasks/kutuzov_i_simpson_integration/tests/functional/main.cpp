e20485d07006de6229981d98ba09d5c95a921c42
K <Akarfire@gmail.com>
2026-01-03 01:33:34 +0300

    [DEV] Removed debugging

--- tasks/kutuzov_i_simpson_integration/tests/functional/main.cpp
+++ tasks/kutuzov_i_simpson_integration/tests/functional/main.cpp
@@ -65,11 +65,7 @@ class KutuzovIRunFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType,
       }
     }
     sum *= step_x * step_y / 9;
-    bool result = (abs(output_data - sum) < 1e-3);
-    if (!result) {
-      std::cout << std::endl << output_data << ", " << sum << ", " << abs(output_data - sum) << std::endl;
-    }
-    return result;
+    return (abs(output_data - sum) < 1e-3);
   }
 
   InType GetTestInputData() final {
