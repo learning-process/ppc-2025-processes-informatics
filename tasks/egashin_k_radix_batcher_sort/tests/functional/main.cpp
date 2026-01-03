eab5650a752575487a21e30472c64a02dc07e248
crn4t <keagshin@mail.ru>
2026-01-03 18:51:16 +0300

    more tests

--- tasks/egashin_k_radix_batcher_sort/tests/functional/main.cpp
+++ tasks/egashin_k_radix_batcher_sort/tests/functional/main.cpp
@@ -58,31 +58,48 @@ class EgashinKRunFuncTestsRadixBatcherSort : public ppc::util::BaseRunFuncTests<
 
 namespace {
 
-// Helper function to generate sorted output from input
 inline OutType SortedOutput(const InType &input) {
   OutType result = input;
   std::ranges::sort(result);
   return result;
 }
 
-// Test cases with unique names
-const std::array<TestType, 15> kTestParam = {
+const std::array<TestType, 30> kTestParam = {
     std::make_tuple(InType{}, OutType{}, "EmptyArray"),
-    std::make_tuple(InType{42.0}, OutType{42.0}, "SingleElement"),
+    std::make_tuple(InType{42.0}, OutType{42.0}, "SinglePositive"),
+    std::make_tuple(InType{-42.0}, OutType{-42.0}, "SingleNegative"),
+    std::make_tuple(InType{0.0}, OutType{0.0}, "SingleZero"),
     std::make_tuple(InType{1.0, 2.0}, OutType{1.0, 2.0}, "TwoElementsSorted"),
     std::make_tuple(InType{2.0, 1.0}, OutType{1.0, 2.0}, "TwoElementsReverse"),
+    std::make_tuple(InType{-2.0, -1.0}, OutType{-2.0, -1.0}, "TwoNegativeSorted"),
+    std::make_tuple(InType{-1.0, -2.0}, OutType{-2.0, -1.0}, "TwoNegativeReverse"),
+    std::make_tuple(InType{3.0, 1.0, 2.0}, OutType{1.0, 2.0, 3.0}, "ThreeElements"),
+    std::make_tuple(InType{-3.0, -1.0, -2.0}, OutType{-3.0, -2.0, -1.0}, "ThreeNegative"),
+    std::make_tuple(InType{5.0, 3.0, 1.0, 4.0, 2.0}, OutType{1.0, 2.0, 3.0, 4.0, 5.0}, "FiveElements"),
+    std::make_tuple(InType{7.0, 3.0, 5.0, 1.0, 6.0, 2.0, 4.0}, OutType{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0},
+                    "SevenElements"),
     std::make_tuple(InType{5.5, 2.2, 8.8, 1.1, 9.9, 3.3}, OutType{1.1, 2.2, 3.3, 5.5, 8.8, 9.9}, "MultiplePositive"),
     std::make_tuple(InType{-1.0, -5.0, -2.0, -8.0, -3.0}, OutType{-8.0, -5.0, -3.0, -2.0, -1.0}, "MultipleNegative"),
     std::make_tuple(InType{3.0, -1.0, 4.0, -5.0, 2.0, -3.0}, OutType{-5.0, -3.0, -1.0, 2.0, 3.0, 4.0}, "MixedSigns"),
     std::make_tuple(InType{0.0, -0.0, 1.0, -1.0}, SortedOutput({0.0, -0.0, 1.0, -1.0}), "WithZeros"),
+    std::make_tuple(InType{-5.0, 0.0, 5.0}, OutType{-5.0, 0.0, 5.0}, "NegZeroPos"),
     std::make_tuple(InType{3.14, 2.72, 1.41, 1.62}, OutType{1.41, 1.62, 2.72, 3.14}, "DecimalNumbers"),
-    std::make_tuple(InType{1e10, 1e5, 1e15, 1e1, 1e8}, OutType{1e1, 1e5, 1e8, 1e10, 1e15}, "LargeNumbers"),
-    std::make_tuple(InType{1e-10, 1e-5, 1e-15, 1e-1, 1e-8}, OutType{1e-15, 1e-10, 1e-8, 1e-5, 1e-1}, "SmallNumbers"),
+    std::make_tuple(InType{1e10, 1e5, 1e15, 1e1, 1e8}, OutType{1e1, 1e5, 1e8, 1e10, 1e15}, "LargePositive"),
+    std::make_tuple(InType{-1e10, -1e5, -1e15, -1e1, -1e8}, OutType{-1e15, -1e10, -1e8, -1e5, -1e1}, "LargeNegative"),
+    std::make_tuple(InType{1e-10, 1e-5, 1e-15, 1e-1, 1e-8}, OutType{1e-15, 1e-10, 1e-8, 1e-5, 1e-1}, "SmallPositive"),
+    std::make_tuple(InType{-1e-10, -1e-5, -1e-15, -1e-1, -1e-8}, OutType{-1e-1, -1e-5, -1e-8, -1e-10, -1e-15},
+                    "SmallNegative"),
     std::make_tuple(InType{5.0, 2.0, 5.0, 2.0, 5.0}, OutType{2.0, 2.0, 5.0, 5.0, 5.0}, "Duplicates"),
+    std::make_tuple(InType{-3.0, -3.0, -3.0}, OutType{-3.0, -3.0, -3.0}, "AllSameNegative"),
     std::make_tuple(InType{1.0, 2.0, 3.0, 4.0, 5.0}, OutType{1.0, 2.0, 3.0, 4.0, 5.0}, "AlreadySorted"),
     std::make_tuple(InType{5.0, 4.0, 3.0, 2.0, 1.0}, OutType{1.0, 2.0, 3.0, 4.0, 5.0}, "ReverseSorted"),
     std::make_tuple(InType{8.0, 4.0, 2.0, 6.0, 1.0, 5.0, 7.0, 3.0}, OutType{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0},
-                    "PowerOf2Size")};
+                    "EightElements"),
+    std::make_tuple(InType{4.0, 2.0, 3.0, 1.0}, OutType{1.0, 2.0, 3.0, 4.0}, "FourElements"),
+    std::make_tuple(InType{-100.0, 50.0, -25.0, 75.0, 0.0, -50.0, 25.0, -75.0, 100.0},
+                    OutType{-100.0, -75.0, -50.0, -25.0, 0.0, 25.0, 50.0, 75.0, 100.0}, "NineElementsMixed"),
+    std::make_tuple(InType{1.0, -1.0, 2.0, -2.0, 3.0, -3.0}, OutType{-3.0, -2.0, -1.0, 1.0, 2.0, 3.0},
+                    "AlternatingSigns")};
 
 TEST_P(EgashinKRunFuncTestsRadixBatcherSort, Sorting) {
   ExecuteTest(GetParam());
